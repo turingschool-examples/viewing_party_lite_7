c5d50fd5f4609b4279993a140a779622386845b9
Chris Crane <chrisjcrane@gmail.com>
2023-03-21 14:29:51 -0600

    add shoulda-matchers

--- app/models/user_party.rb
+++ app/models/user_party.rb
@@ -1,2 +1,3 @@
 class UserParty < ApplicationRecord
+  belongs_to :user
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,11 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
 end
