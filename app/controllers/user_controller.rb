9b9a62b21ddcc9afadfe541ee70beec163eefcc3
Harrison Ryan <hwryan12@gmail.com>
2023-03-21 16:38:24 -0500

    feature: Create route for registering new users and controller actions #new, #create, #show Co-authored-by: C-V-L

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,4 +6,26 @@ class UserController < ApplicationController
 
   def discover
   end
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
 end
