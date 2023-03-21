365691aaf5a9ad76fe1593ee18fa30517b1cee20
Matt Enyeart <menyeart1@gmail.com>
2023-03-20 20:26:48 -0600

    Create migrations for user, viewing party, viewing party user tables

---
+++ db/migrate/20230321014244_create_viewing_parties.rb
@@ -0,0 +1,13 @@
+class CreateViewingParties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :viewing_parties do |t|
+      t.string :date
+      t.time :start_time
+      t.integer :duration
+      t.integer :movie_id
+      t.integer :host_id
+
+      t.timestamps
+    end
+  end
+end
