1da16dc467bf8744dd8f71eff1973bafe7d5a511
Bobby Luly <bobbyluly@gmail.com>
2023-03-21 15:35:59 -0600

    Refactor: change syntax on expects for page to have a link

--- spec/features/landing/landing_spec.rb
+++ spec/features/landing/landing_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Landing', type: :feature do
+RSpec.describe "Landing", type: :feature do
   before(:each) do
     @user_1 = create(:user)
     @user_2 = create(:user)
@@ -10,19 +10,25 @@ RSpec.describe 'Landing', type: :feature do
   end
 
   describe "User Story 4" do
-    it 'shows the landing page with the name of the app and a button to create a new user' do
-      expect(page).to have_content('Viewing Party')
-      expect(page).to have_button('Create New User')
+    it "shows the landing page with the name of the app and a button to create a new user" do
+      expect(page).to have_content("Viewing Party")
+      expect(page).to have_button("Create New User")
     end
 
-    it 'shows the existing users as a link to their dashboard' do
-      within("#user_#{@user_1.id}") { expect(page).to have_link("#{@user_1.name} - #{@user_1.email}"), href: user_path(@user_1.id) }
-      within("#user_#{@user_2.id}") { expect(page).to have_link("#{@user_2.name} - #{@user_2.email}"), href: user_path(@user_2.id) }
-      within("#user_#{@user_3.id}") { expect(page).to have_link("#{@user_3.name} - #{@user_3.email}"), href: user_path(@user_3.id) }
+    it "shows the existing users as a link to their dashboard" do
+      within("#user_#{@user_1.id}") do
+        expect(page).to have_link(("#{@user_1.name} - #{@user_1.email}"), href: user_path(@user_1.id))
+      end
+      within("#user_#{@user_2.id}") do
+        expect(page).to have_link(("#{@user_2.name} - #{@user_2.email}"), href: user_path(@user_2.id))
+      end
+      within("#user_#{@user_3.id}") do
+        expect(page).to have_link(("#{@user_3.name} - #{@user_3.email}"), href: user_path(@user_3.id))
+      end
     end
 
-    it 'has a link to the landing page' do
-      expect(page).to have_link('Home'), href: root_path
+    it "has a link to the landing page" do
+      expect(page).to have_link(("Home"), href: root_path)
     end
   end
 end
