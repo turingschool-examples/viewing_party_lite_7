517f552efb6cb17573d5dc922eb27d0fb437d952
Chris Crane <chrisjcrane@gmail.com>
2023-03-21 14:00:29 -0600

    add final test for user story 1
    <bobbyluly@gmail.com>

--- spec/features/landing/landing_spec.rb
+++ spec/features/landing/landing_spec.rb
@@ -5,21 +5,25 @@ RSpec.describe 'Landing', type: :feature do
     @user_1 = create(:user)
     @user_2 = create(:user)
     @user_3 = create(:user)
+
+    visit root_path
   end
 
   describe "User Story 4" do
     it 'shows the landing page with the name of the app and a button to create a new user' do
-      visit root_path
       expect(page).to have_content('Viewing Party')
       expect(page).to have_button('Create New User')
     end
 
     it 'shows the existing users as a link to their dashboard' do
-      within '#existing_users' do
-        expect(page).to have_content("#{user_1.name} - #{user_1.email}")
-        expect(page).to have_content("#{user_2.name} - #{user_2.email}")
-        expect(page).to have_content("#{user_3.name} - #{user_3.email}")
+      within("#user_#{user_1.id}") { expect(page).to have_link("#{user_1.name} - #{user_1.email}"), href: dashboard_path(user_1.id) }
+      within("#user_#{user_2.id}") { expect(page).to have_link("#{user_2.name} - #{user_2.email}"), href: dashboard_path(user_2.id) }
+      within("#user_#{user_3.id}") { expect(page).to have_link("#{user_3.name} - #{user_3.email}"), href: dashboard_path(user_3.id) }
       end
+    end
+
+    it 'has a link to the landing page' do
+      expect(page).to have_link('Home'), href: root_path
     end
   end
 end
