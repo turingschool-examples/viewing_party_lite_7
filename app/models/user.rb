334249af7e68338cc45c06855a2c935b866532e0
Matt Enyeart <menyeart1@gmail.com>
2023-03-20 20:27:17 -0600

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates_presence_of :name,
+                        :email
+  validates :email, uniqueness: true
+
+  has_many :viewing_party_users
+  has_many :viewing_parties, through: :viewing_party_users
+end
