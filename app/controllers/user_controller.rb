3171857c0e82c7d190bc79814c710b2d7e47b1d2
Conner Van Loan <cdvanloan@gmail.com>
2023-03-21 14:10:41 -0600

    adds viewing party details to user dashboard

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
 class UserController < ApplicationController
   def dashboard
     @user = User.find(params[:id])
+    @viewing_parties = @user.viewing_parties
   end
 end
--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe 'User Dashboard' do
   before :each do
     @user1 = User.create!(name: 'User 1', email: 'user1@email.com')
     @user2 = User.create!(name: 'User 2', email: 'user2@email.com')
+    @viewing_party = ViewingParty.create!(duration: 120, date: '2021-01-01', time: '12:00:00', movie_id: 1)
+
+    ViewingPartyUser.create!(user_id: @user1.id, viewing_party_id: @viewing_party.id, host: true)
+
     visit dashboard_user_path(@user1)
   end
 
@@ -19,7 +23,11 @@ RSpec.describe 'User Dashboard' do
     end
 
     it 'I see a section for viewing parties' do
-      expect(page).to have_content('Viewing Parties')
+      expect(page).to have_content("Viewing Parties")
+      expect(page).to have_content("Duration: #{@viewing_party.duration}")
+      expect(page).to have_content("Date: #{@viewing_party.date}")
+      expect(page).to have_content("Time: #{@viewing_party.time}")
+      expect(page).to have_content("Movie: #{@viewing_party.movie_id}")
     end
   end
 end
