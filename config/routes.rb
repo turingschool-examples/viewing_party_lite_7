a6e01cee1a9f9eff2b197cc07f545950fd3d1e84
KaraJH <karas.jones@yahoo.com>
2023-03-21 13:37:34 -0600

    feat: Adds routes for user path

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root "welcome#index"
+
+  #get "/register", to: "users#new"
+  #post "/register", to :users#create"
+
+  resources :users, only: [:show]
 end
