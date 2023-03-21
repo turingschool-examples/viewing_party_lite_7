8fe7cced0ead5413f70366fab3b9b3686f7ec7da
Andra Helton <andrahelton@gmail.com>
2023-03-20 20:55:32 -0600

    Add teset for user show page

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "User Show Page", type: :feature do
+  let!(:andra) { User.create!(name: "Andra", email: "andra@turing.edu") }
+  let!(:hady) { User.create!(name: "Hady", email: "hady@turing.edu") }
+
+  let!(:halloween) { Party.create!(name: "Halloween Party", host_id: hady.id, party_date: "10/31/2023", party_time: "10:30", duration: 123) }
+  let!(:girls_night) { Party.create!(name: "Girl's Night", host_id: hady.id, party_date: "01/01/2023", party_time: "02:00", duration: 210) }
+  let!(:eighties) { Party.create!(name: "Eighties Themed", host_id: hady.id, party_date: "01/01/2023", party_time: "02:00", duration: 210) }
+
+  let!(:usp1) { UserParty.create!(user_id: andra.id, party_id: halloween.id) }
+  let!(:usp2) { UserParty.create!(user_id: andra.id, party_id: girls_night.id) }
+  let!(:usp3) { UserParty.create!(user_id: andra.id, party_id: eighties.id) }
+  
+  before do
+    visit "/users/#{andra.id}"
+  end
+
+  describe "When I visit '/users/:id'" do
+    it "I see user's name at the top of the page" do
+      expect(page).to have_content("Andra's Info")
+    end
+
+    it "I see a button to Discover Movies" do
+      expect(page).to have_button("Discover Movies")
+    end
+
+    it "I see a section that lists viewing parties" do
+      expect(page).to have_content("Halloween Party")
+      expect(page).to have_content("Girl's Night")
+      expect(page).to have_content("Eighties Themed")
+    end
+  end
+end
