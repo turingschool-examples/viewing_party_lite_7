f4f2c268a25d0734a6defd9380363bca7179f52d
Matt Enyeart <menyeart1@gmail.com>
2023-03-21 17:28:24 -0600

    Add method to applcations controller for flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  add_flash_types :error, :success
+
+  private
+  def error_message(errors)
+    errors.full_messages.join(', ')
+  end
+  
 end
