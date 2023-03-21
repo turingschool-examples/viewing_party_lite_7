e173243fdc223a68f89fae8212199a17882293e8
Meredith Trevino <meredithtrevino3@gmail.com>
2023-03-21 17:17:53 -0600

    user stories #5 and #6

---
+++ spec/features/user_dashboard_page_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+
+RSpec.describe '/users/:id', type: :feature do
+  before do
+    @steve = User.create!(name: "steve", email: "steve@steve.com")
+    visit "/users/#{@steve.id}"
+  end
+
+  describe 'When a user visits their dashboard' do
+    it "shows the 'user dashboard' header" do
+      expect(page).to have_content("#{@steve.name}'s Dashboard")
+    end
+
+    it "has a button to discover movies" do
+      expect(page).to have_link("Discover Movies")
+      expect(page).to have_content("Lists of viewing parties")
+    end
+
+    it "when I click the discover movies button, I am redirected to the discover movies page (/users/:id/discover), where the :id is the user dashboard I was just on" do
+      click_link "Discover Movies"
+      expect(current_path).to eq("/users/#{@steve.id}/discover")
+    end
+  end
+end
