21999615596bc1293664994096daf284ef75cf0f
Andra Helton <andrahelton@gmail.com>
2023-03-20 20:02:30 -0600

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,10 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+
+require 'simplecov'
+SimpleCov.start
+
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
