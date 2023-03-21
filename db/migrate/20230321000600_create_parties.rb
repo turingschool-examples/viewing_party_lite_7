00e2a2b0c8dcdb82a5de00cc1be323d9b28ec122
Meredith Trevino <meredithtrevino3@gmail.com>
2023-03-20 18:12:22 -0600

    Adds migrations and schema

---
+++ db/migrate/20230321000600_create_parties.rb
@@ -0,0 +1,12 @@
+class CreateParties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :parties do |t|
+      t.integer :movie_id
+      t.string :date
+      t.string :time
+      t.integer :duration
+
+      t.timestamps
+    end
+  end
+end
