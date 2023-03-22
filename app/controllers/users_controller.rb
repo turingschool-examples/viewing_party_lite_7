8c3fbff7c5a774e72fe8a6979ffb191b9b9ee810
Elle Majors <hellolovely@ellemajors.com>
2023-03-21 19:45:54 -0600

    add: user story 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def create
+		user = User.create!(user_params)
+		redirect_to user_path(user)
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
 end
---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "User Show Page" do
+  before(:each) do
+    @user_1 = User.create(name: "Bob", email: "bob@myemail.com")
+  end
+
+  it "can see the users name's dashboard at the top of the page" do
+    visit user_path(@user_1)
+
+    expect(page).to have_content("Bob's Dashboard")
+    expect(page).to_not have_content("Sally's Dashboard")
+  end
+
+  it "can see a button to discover movies" do
+    visit user_path(@user_1)
+
+    expect(page).to have_button("Discover Movies")
+  end
+
+  it "list viewing parties" do
+    visit user_path(@user_1)
+    @user_1.parties.create(movie_title: "The Matrix", duration: 120, start_time: "2021-08-01 12:00:00", host: true)
+
+    expect(page).to have_content("Viewing Parties")
+  end
+end    
