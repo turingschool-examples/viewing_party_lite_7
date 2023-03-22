cb3fcfca4279fb087979dd2b90d4ac8a72489942
Caleb Thomas <cjthomas00@comcast.net>
2023-03-21 21:27:44 -0600

    add register route, and create route to users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'welcome#index'
   get "/register", to: "users#new"
 
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :create]  do
     get "/discover", to: "movies#discover"
   end
 end
