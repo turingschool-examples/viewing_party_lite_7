230ae164ce5439379b0bf207d021658af41ff53e
Caleb Thomas <cjthomas00@comcast.net>
2023-03-21 21:24:37 -0600

    test: add validation for email presence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ describe User, type: :model do
 
   describe "validations" do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
   end
 end
