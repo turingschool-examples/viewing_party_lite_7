dbe983ec5aefa82e1fd5114a50ed93afe1d60228
Matt Enyeart <menyeart1@gmail.com>
2023-03-22 17:38:00 -0600

    Create movies facade
    Co-authored-by: Jade
    jadekstewart3@gmail.com
    Matt
    menyeart1@gmail.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   add_flash_types :error, :success
 
   private
+
   def error_message(errors)
-    errors.full_messages.join(', ')
+    errors.full_messages.join(", ")
   end
-  
 end
