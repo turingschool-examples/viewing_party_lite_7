769b258c68169a6f08901c28a833cc303fbb4987
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-03-22 10:00:44 -0600

    feat: add route and have discover movies button take user to user_discover_index_path
    Co-authored-by: Elle Majors <hellolovely@ellemajors.com>
    Co-authored-by: Axel De La Guardia <axeldelaguardia@gmail.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -8,20 +8,23 @@ describe "User Show Page" do
   end
 
   it "can see the users name's dashboard at the top of the page" do
-    visit user_path(@user_1)
-
     expect(page).to have_content("Bob's Dashboard")
     expect(page).to_not have_content("Sally's Dashboard")
   end
 
   it "can see a button to discover movies" do
-    visit user_path(@user_1)
-
     expect(page).to have_button("Discover Movies")
   end
 
   it "list viewing parties" do
     expect(page).to have_content("Viewing Parties")
-		save_and_open_page
   end
-end    
+
+	describe "discover movies" do
+		it "takes user to discover page when the Discover Movies button is clicked" do
+			click_button "Discover Movies"
+
+			expect(current_path).to eq(user_discover_index_path(@user_1))
+		end
+	end
+end
