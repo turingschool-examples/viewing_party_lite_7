05b26758636644c65c903a039ccf1aca249db4e6
Chris Crane <chrisjcrane@gmail.com>
2023-03-22 11:35:51 -0600

    Refactored tests for movie service
    Co-authored-by: Bobby Luly <bobbyluly@gmail.com>

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,13 +2,27 @@ require 'rails_helper'
 
 describe MovieService do
   describe "#top_rated_movies" do
-    it "returns the top 20 rated moves" do
-      top_rated_movies = MovieService.new.top_rated_movies
+    before(:each) do
+      @top_rated_movies = MovieService.new.top_rated_movies
+    end
+    
+    it "returns all expected attributes and data types" do
+      expect(@top_rated_movies).to be_a(Array)
 
-      expect(top_rated_movies).to be_a(Array)
-      expect(top_rated_movies.count).to eq(20)
-      expect(top_rated_movies.first[:title]).to eq("The Godfather")
-      expect(top_rated_movies.first[:vote_average]).to eq(8.7)
+      expect(@top_rated_movies.first).to have_key(:id)
+      expect(@top_rated_movies.first[:id]).to be_a(Integer)
+
+      expect(@top_rated_movies.first).to have_key(:title)
+      expect(@top_rated_movies.first[:title]).to be_a(String)
+
+      expect(@top_rated_movies.first).to have_key(:vote_average)
+      expect(@top_rated_movies.first[:vote_average]).to be_a(Float)
+    end
+
+    it "returns the top 20 rated moves" do
+      expect(@top_rated_movies.count).to eq(20)
+      expect(@top_rated_movies.first[:title]).to eq("The Godfather")
+      expect(@top_rated_movies.first[:vote_average]).to eq(8.7)
     end
   end
 end
