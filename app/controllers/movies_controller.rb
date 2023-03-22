e18a67befe9eb1a03650a8f196ce36a334a99925
Matt Enyeart <menyeart1@gmail.com>
2023-03-22 17:42:27 -0600

    Add index action to movies controller utilizing movie objects Co-authored-by: Jade jadekstewart3@gmail.com Matt menyeart1@gmail.com

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,10 @@
-# require './facade/top_movies_facade.rb'
 class MoviesController < ApplicationController
   def index
-    require 'pry'; binding.pry
     @user = User.find(params[:user_id])
-    if params[:q] == "keyword"
-      @keyword_movies
-    else
-      @top_rated_movies = TopMoviesFacade.top_rated_info
-    end
+    @movies = if params[:q] == "keyword"
+                MoviesFacade.searched_movies(params[:title])
+              else
+                MoviesFacade.top_rated_movies
+              end
   end
 end
