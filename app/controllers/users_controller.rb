bd631bee9d081019723b0409588965bd1ec6517b
Caleb Thomas <cjthomas00@comcast.net>
2023-03-21 21:27:00 -0600

    feat: add view to register a new user. add #new and #create to user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    user = User.new(user_attributes)
+    if user.valid?
+      user.save
+      redirect_to user_path(user)
+    else
+      redirect_to register_path
+      flash[:error] = user.errors.full_messages
+    end
+  end
+
+  private
+  def user_attributes
+    params.permit(:name, :email)
+  end
 end
