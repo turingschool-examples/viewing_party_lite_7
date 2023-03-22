abe8a3dec514166ab791745a5693a7c0e8c935f7
Caleb Thomas <cjthomas00@comcast.net>
2023-03-21 18:56:44 -0600

    test: create all tests for welcome page.

---
+++ spec/features/welcome_page/welcome_page_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe "Welcome Page" do
+  before :each do
+    @user = create(:user)
+    visit "/"
+  end
+  
+  describe "When a user visits the root path they should be on the landing page ('/') which includes:" do
+    it "Title of Application" do 
+      expect(page).to have_content("Viewing Party Lite")
+    end
+
+    it "Has a Button to Create a New User" do
+      expect(page).to have_button("Create New User")
+    end
+
+    it "has a List of Existing Users" do
+      expect(page).to have_content("Existing Users")
+      within(".existing_users") do
+        expect(page).to have_link("#{@user.name}")
+        expect(page).to have_no_link("Home Page")
+      end
+    end
+
+    it "has a link to go back to the welcome page" do
+      within(".links") do
+        expect(page).to have_link("Home Page")
+        expect(page).to have_no_link("#{@user.name}")
+      end
+    end
+  end
+end
