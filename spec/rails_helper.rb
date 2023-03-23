465df70084aaa2855047efc85d5375dd7e2ff0e7
Harrison Ryan <hwryan12@gmail.com>
2023-03-22 19:50:34 -0500

    CO-AUTHORED_BY: Conner V <cdvanloan@gmail.com> tests: Add test for discover/index

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,3 +73,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('api_key') { ENV["api_key"] }
+  config.default_cassette_options = { record: :new_episodes }
+end
