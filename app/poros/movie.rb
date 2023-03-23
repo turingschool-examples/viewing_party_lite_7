89a8851f885d8f6902f300a8532f896a9632a7d1
Matt Enyeart <menyeart1@gmail.com>
2023-03-23 16:08:57 -0600

    Add instance variables to movie poro

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -2,6 +2,12 @@ class Movie
   attr_reader :title,
               :summary,
               :vote_average,
+              :movie_id,
+              :runtime,
+              :genres,
+              :cast,
+              :total_reivews_count,
+              :review_info,
               :movie_id
 
   def initialize(info)
@@ -9,5 +15,10 @@ class Movie
     @title = info[:title]
     @summary = info[:overview]
     @vote_average = info[:vote_average]
+    @runtime = info[:runtime]
+    @genres = info[:genres]
+    @cast = info[:cast]
+    @total_reviews_count = info[:total_reviews]
+    @review_info = info[:review_info]
   end
 end
