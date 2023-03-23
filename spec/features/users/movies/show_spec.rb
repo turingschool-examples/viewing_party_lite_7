7682baa84aa4afa879b5b39b7ca87bc6dc00642b
Chris Crane <chrisjcrane@gmail.com>
2023-03-23 15:50:37 -0600

    add outline for test for movie show page
    Co-authored-by: Bobby Luly <bobbyluly@gmail.com>

---
+++ spec/features/users/movies/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'User Story 10' do
+  before(:each) do
+    @stan = create(:user, id: 1)
+    visit user_discover_index_path(@stan)
+  end
+
+  describe "User Story 8 - Movie Show Page" do
+    it "shows the movie information" do
+      VCR.use_cassette(:movie_by_id_info, serialize_with: :json) do
+        fill_in :search, with: "bat"
+        click_button "Find Movies"
+
+        expect(current_path).to eq("/users/#{@stan.id}/movies")
+      end
+    end
+  end
+end
