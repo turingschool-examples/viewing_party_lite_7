2b4d30b676cbe42ff08505942f0c722c63118a3b
Matt Enyeart <menyeart1@gmail.com>
2023-03-24 08:10:30 -0600

    Change vcr configuration to allow multiple tapes for one test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,6 +72,7 @@ Shoulda::Matchers.configure do |config|
 end
 
 VCR.configure do |config|
+  config.default_cassette_options = { record: :new_episodes }
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data("DONT_LOOK_AT_MY_JUNK") { ENV["movie_token"] }
