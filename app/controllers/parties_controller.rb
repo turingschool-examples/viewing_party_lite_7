7c76e0864d5ff23cf439c17a266daf3cc2f88ab5
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-03-24 16:17:00 -0600

    remove: unused code

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,11 +1,4 @@
 class PartiesController < ApplicationController
-  def index
-    @parties = Party.all
-  end
-
-  def show
-    @party = Party.find(params[:id])
-  end
 
 	def new
 		@party = Party.new
