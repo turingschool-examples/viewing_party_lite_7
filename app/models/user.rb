07bd9557cd6c8e2e73ce36b92cf9d7d7dfcf95e7
KaraJH <karas.jones@yahoo.com>
2023-03-24 14:11:14 -0600

    feat: Active record on models to get info for the users viewing parties view

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :email
   validates_presence_of :name
+
+  def movie_ids 
+    viewing_parties.pluck('movie_id')
+  end
+
+  def viewing_parties_invited_to 
+    viewing_parties.joins(:viewing_party_users).where('viewing_party_users.is_host = false').distinct
+  end
+
+  def viewing_parties_as_host
+    viewing_parties.joins(:viewing_party_users).where('viewing_party_users.is_host = true').distinct
+  end
 end
--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -2,5 +2,13 @@ class ViewingParty < ApplicationRecord
   has_many :viewing_party_users 
   has_many :users, through: :viewing_party_users
 
-  validates_presence_of :duration_of_party, :start_time, :when
+  validates_presence_of :duration_of_party, :start_time, :when, :movie_id
+
+  def party_host 
+    users.joins(:viewing_party_users).where('viewing_party_users.is_host = true').distinct.first
+  end
+
+  def users_invited 
+    users.joins(:viewing_party_users).where('viewing_party_users.is_host = false').distinct
+  end
 end
