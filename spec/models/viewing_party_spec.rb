20cb635996e998aa0aa8530c73e05c45df0dc37c
Adam Bailey <116456652+aj-bailey@users.noreply.github.com>
2023-03-24 16:40:33 -0600

    Model refactor (#20)
    * feat: update view css styling
    
    Co-authored-by: James Taylor <jrtaylor0614@gmail.com>
    
    * update bundler
    
    * test: merge spec data from other branch
    
    Co-authored-by: James Taylor <jrtaylor0614@gmail.com>
    
    ---------
    
    Co-authored-by: James Taylor <jrtaylor0614@gmail.com>

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -5,11 +5,35 @@ RSpec.describe ViewingParty do
     it { should have_many :viewing_party_users }
     it { should have_many(:users).through(:viewing_party_users) }
   end
-
+  #-------- unable to test validations due to custom validation --------#
+  # describe 'validations' do
+  #   it { should validate_presence_of :party_date }
+  #   it { should validate_presence_of :party_time }
+  # end
+  #---------------------------------------------------------------------#
+  
   describe '#instance methods' do
-    describe '#host' do
-      it 'returns the host of the viewing party' do
+    before :each do
+      @adam = User.create!( name: 'adam', email: 'adam@example.com')
+      @james = User.create!( name: 'james', email: 'james@example.com')
+      @mike = User.create!( name: 'mike', email: 'mike@example.com')
+      
+      @fight_club = ViewingParty.create!( duration_minutes: 139, party_date: "2123-12-28", party_time: "1:39", host_id: @james.id, movie_id: 550 )
 
+      ViewingPartyUser.create!(user_id: @james.id, viewing_party_id: @fight_club.id)
+      ViewingPartyUser.create!(user_id: @mike.id, viewing_party_id: @fight_club.id)
+      ViewingPartyUser.create!(user_id: @adam.id, viewing_party_id: @fight_club.id)
+    end
+
+    describe '#host', :vcr do
+      it 'returns the host of the viewing party' do
+        expect(@fight_club.host).to eq(@james)
+      end
+    end
+
+    describe "#guest", :vcr do
+      it 'returns the guest of the viewing party' do
+        expect(@fight_club.guests).to eq([@adam, @mike])
       end
     end
   end
