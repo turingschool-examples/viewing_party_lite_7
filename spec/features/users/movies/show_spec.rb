80688c00d4f9a9fcd35066118bb33ea3e10c54d9
Melony Franchini <meltravelz@gmail.com>
2023-03-24 11:13:42 -0600

    Add all feat test & view for movie show page:

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe "/users/:user_id/movies/:movie_id" do
   describe "as a user, when I visit a movie's show page" do 
     before :each do
       @picard = User.create!(name: "Jean-Luc Picard", email: "captain@uss-enterprise.com")
-    end
 
-    it "displays the movie information" do
       movie_response = File.read("spec/fixtures/moviedb/space_odyssey.json")
       stub_request(:get, "https://api.themoviedb.org/3/movie/62?api_key=#{ENV["TMDB_API_KEY"]}")
       .to_return(status: 200, body: movie_response, headers: {})
@@ -21,26 +19,44 @@ RSpec.describe "/users/:user_id/movies/:movie_id" do
       film = MoviedbFacade.new(movie_id: 62)
 
       visit "/users/#{@picard.id}/movies/62"
+    end
 
+    it "displays the movie information" do
       expect(page).to have_content("2001: A Space Odyssey")
+
       expect(page).to have_button("Discover Page")
       expect(page).to have_button("Create Viewing Party for 2001: A Space Odyssey")
       expect(page).to have_content("Vote: 8.083")
-      expect(page).to have_content("Runtime: 149(MINUTES -> HOURS MIN)")
-      expect(page).to have_content("Genre: Science Fiction, Mystery, Adventure")
+      expect(page).to have_content("Runtime: 149 minutes")
+      expect(page).to have_content("Genre:")
+      expect(page).to have_content("Science Fiction")
+      expect(page).to have_content("Mystery")
+      expect(page).to have_content("Adventure")
 
       expect(page).to have_content("Summary")
-      expect(page).to have_content("Humanity finds a mysterious object buried beneath the lunar surface and 
-        sets off to find its origins with the help of HAL 9000, the world's most advanced super computer.")
+      expect(page).to have_content("Humanity finds a mysterious object buried beneath the lunar surface and sets off to find its origins with the help of HAL 9000, the world's most advanced super computer.")
 
       expect(page).to have_content("Cast")
+      # Not sure if I need to test for all 10 actors? 
       expect(page).to have_content("Keir Dullea as Dr. David Bowman")
       expect(page).to have_content("Frank Miller as Mission Controller")
 
       expect(page).to have_content("7 Reviews")
-      expect(page).to have_content("markuspm: There are many great predictions hinting to future (it is from 1968 
-        - can you believe it?) innovations throughout the movie. I might not have found all them because I keep 
-        falling asleep while watching it but I will keep trying to find them all.")
+      # This text is very long, so decided to only test for part of it:
+        # expect(page).to have_content("markuspm: There are many great predictions hinting to future (it is from 1968 
+        #   - can you believe it?) innovations throughout the movie. I might not have found all them because I keep 
+        #   falling asleep while watching it but I will keep trying to find them all.")
+      expect(page).to have_content("markuspm: There are many great predictions hinting to future")
+    end
+
+    it "when I click the discover button, I'm redirected to '/users/:id/discover' page" do
+      click_button("Discover Page")
+      expect(current_path).to eq("/users/#{@picard.id}/discover")
+    end
+
+    it "when I click the Viewing Party button, I'm redirected to '/users/:id/movies/:id/viewing-party/new' page" do
+      click_button("Create Viewing Party for 2001: A Space Odyssey")
+      expect(current_path).to eq("/users/#{@picard.id}/movies/62/viewing_party/new")
     end
   end
 end
