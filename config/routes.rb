31e0c8255b69c9d5ee6587db99fa3389bdb73d7a
Matt Enyeart <menyeart1@gmail.com>
2023-03-24 15:36:28 -0600

    Add viewing party users route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   resources :users, only: %i[show create] do
     resources :discover, only: [:index]
     resources :movies, only: %i[index show] do
-      resources :viewing_party, only: [:new]
+      resources :viewing_party, only: [:new, :create]
+      resources :viewing_party_user, only: [:create] 
     end
   end
 
