01a4d9bc0149e843fc29502fe0492bd2d05f4ee0
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-03-24 09:44:02 -0600

    Feat: create new party page

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -9,13 +9,32 @@ class PartiesController < ApplicationController
 
 	def new
 		@party = Party.new
+		@movie = movie_params
+		@users = User.all
+		@movie_id = params[:movie_id]
+	end
+
+	def create
+		user = User.find(params[:user_id])
+		party = Party.new(party_params)
+		UserParty.create(user: user, party: party, host: user)
+		params[:party][:users].each do |user_id|
+			next if user_id.empty?
+			UserParty.create(user_id: user_id.to_i, party: party, host: user)
+		end
+		party.save
+		redirect_to user_path(user)
 	end
 
   private
 
-  def party_params
-    params.require(:party).permit(:name, :description, :location, :date, :time)
+  def movie_params
+    params.permit(:title, :runtime, :movie_id)
   end
+
+	def party_params
+		params.require(:party).permit(:duration, :date, :time, :movie_id)
+	end
 end
 
 
