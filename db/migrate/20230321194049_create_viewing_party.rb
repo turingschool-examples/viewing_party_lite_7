12fbd49c6df49c3261818f461e4b047bbdf6ae05
Caleb Thomas <cjthomas00@comcast.net>
2023-03-24 11:25:18 -0600

    change date to party_date

--- db/migrate/20230321194049_create_viewing_party.rb
+++ db/migrate/20230321194049_create_viewing_party.rb
@@ -2,8 +2,9 @@ class CreateViewingParty < ActiveRecord::Migration[7.0]
   def change
     create_table :viewing_parties do |t|
       t.integer :duration
-      t.date :date
+      t.date :party_date
       t.time :start_time
+      t.integer :movie_id
       t.timestamps
     end
   end
