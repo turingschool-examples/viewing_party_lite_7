3d3227efbbd240d592288fda8e2f5b9cf12e78f4
Max MacGillivray <mary.macgillivray@gmail.com>
2023-03-24 09:34:14 -0700

    Created additional testing for flash messages in user creation

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -3,21 +3,18 @@ require 'rails_helper'
 RSpec.describe "user registration page" do
   before :each do
     User.delete_all
+    
+    visit register_path
   end
   
   describe "initial testing" do
     it "exists and has a registration form" do
-      visit register_path
-      
       expect(page).to have_field("Name")
       expect(page).to have_field("Email")
       expect(page).to have_button("Create New User")
     end
     
     it "takes input to create a new user" do
-      
-      visit register_path
-      
       fill_in('Name', with: 'Lightning McQueen')
       fill_in('Email', with: 'Kachow@cars.net')
       click_on("Create New User")
@@ -29,14 +26,39 @@ RSpec.describe "user registration page" do
     it "won't create a user with an already used email" do
       user = User.create(name: "Lightning McQueen", email: "kachow@cars.net")
       
-      visit register_path
-      
       fill_in('Name', with: 'Chick Hicks')
       fill_in('Email', with: 'kachow@cars.net')
       click_on("Create New User")
 
       expect(current_path).to eq(register_path)
       expect(page).to have_content("Email has already been taken")
+    end
+    
+    describe "won't create a user with blank fields" do
+      it "won't create a user with two blank fields" do
+        click_on("Create New User")
+        
+        expect(current_path).to eq(register_path)
+        expect(page).to have_content("Name can't be blank and Email can't be blank")
+      end
+      
+      it "won't create a user without a name" do
+        fill_in('Name', with: "Jeff Goldblum")
+        click_on("Create New User")
+        
+        expect(current_path).to eq(register_path)
+        expect(page).to have_content("Email can't be blank")
+        expect(page).to_not have_content("Name can't be blank")
+      end
+      
+      it "won't create a user without an email" do
+        fill_in('Email', with: "JurassicSnark@jp.com")
+        click_on("Create New User")
+        
+        expect(current_path).to eq(register_path)
+        expect(page).to have_content("Name can't be blank")
+        expect(page).to_not have_content("Email can't be blank")
+      end
     end
   end
 end
