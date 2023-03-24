b8ebd034ef4518734cabfa6deb4e38c78cad7a08
Matt Enyeart <menyeart1@gmail.com>
2023-03-24 08:08:42 -0600

    Create viewing party new route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show create] do
     resources :discover, only: [:index]
-    resources :movies, only: [:index, :show] do
+    resources :movies, only: %i[index show] do
       resources :viewing_party, only: [:new]
     end
   end
