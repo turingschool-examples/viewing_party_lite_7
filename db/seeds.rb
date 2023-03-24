3b43dbb1002fc965a5ab9990bcbb28321b0aeeaf
Isaac Alter <i_alter@yahoo.com>
2023-03-24 15:35:41 -0600

    Adds refactor: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,24 @@
 
 @user_1 = User.create!(name: "Joe Smith", email: "joey_smithy@yahooey.com")
 @user_2 = User.create!(name: "Sam Smith", email: "sam_smithy@yahooey.com")
+@user_3 = User.create!(name: "Isaac Smith", email: "isaac_smithy@yahooey.com")
+    @user_4 = User.create!(name: "Kara Smith", email: "kara_smithy@yahooey.com")
 
-@viewing_party_1 = ViewingParty.create!(duration_of_party: 300, when: "2023-12-25", start_time: "10:10")
-@viewing_party_2 = ViewingParty.create!(duration_of_party: 280, when: "2439-10-31", start_time: "11:48")
+    @viewing_party_1 = ViewingParty.create!(duration_of_party: 300, when: "2023-12-25", start_time: "10:10 AM", movie_id: 238 )
+    @viewing_party_2 = ViewingParty.create!(duration_of_party: 280, when: "2439-10-31", start_time: "11:48 AM", movie_id: 278)
+    @viewing_party_3 = ViewingParty.create!(duration_of_party: 400, when: "2455-7-31", start_time: "12:25 PM", movie_id: 155)
+    @viewing_party_4 = ViewingParty.create!(duration_of_party: 450, when: "2467-10-31", start_time: "10:30 PM", movie_id: 497)
+    @viewing_party_5 = ViewingParty.create!(duration_of_party: 425, when: "2439-9-25", start_time: "9:00 PM", movie_id: 13)
 
-@user_1.viewing_party_users.create!(viewing_party: @viewing_party_1, is_host: false)
-@user_1.viewing_party_users.create!(viewing_party: @viewing_party_2, is_host: true)
+    @user_1.viewing_party_users.create!(viewing_party: @viewing_party_1, is_host: false)
+    @user_1.viewing_party_users.create!(viewing_party: @viewing_party_2, is_host: true)
+    @user_1.viewing_party_users.create!(viewing_party: @viewing_party_3, is_host: false)
+    @user_1.viewing_party_users.create!(viewing_party: @viewing_party_4, is_host: true)
+
+    @user_2.viewing_party_users.create!(viewing_party: @viewing_party_1, is_host: true)
+    @user_2.viewing_party_users.create!(viewing_party: @viewing_party_3, is_host: true)
+    @user_2.viewing_party_users.create!(viewing_party: @viewing_party_2, is_host: false)
+
+    @user_3.viewing_party_users.create!(viewing_party: @viewing_party_1, is_host: false)
+
+    @user_4.viewing_party_users.create!(viewing_party: @viewing_party_1, is_host: false)
