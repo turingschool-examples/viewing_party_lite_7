82cfebf62c1b0c22488d643ec99a1c21c8d768e0
Isaac Alter <i_alter@yahoo.com>
2023-03-23 20:37:42 -0600

    Add feat: nested route for the viewing party new

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   get "/users/:id/movies", to: "users#movie_results"
   get "/users/:user_id/movies/:movie_id", to: "users#movie_details"
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :movies, only: [:show] do
+      resources :viewing_parties, :path => "viewing_party", only: [:new, :create], controller: "viewing_parties" 
+    end
+  end
 end
