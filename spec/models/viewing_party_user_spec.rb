603c14da76fc39142f8e5889823cb61bbeb94375
Harrison Ryan <hwryan12@gmail.com>
2023-03-23 21:14:34 -0500

    remove unused test

--- spec/models/viewing_party_user_spec.rb
+++ spec/models/viewing_party_user_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe ViewingPartyUser, type: :model do
   it { should belong_to :user }
   it { should belong_to :viewing_party }
-  it { should validate_presence_of :host }
   
 end
