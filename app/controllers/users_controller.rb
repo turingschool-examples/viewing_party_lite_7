40059cc89d86c53b4434121e54767c8bb8f93d0e
Meredith Trevino <meredithtrevino3@gmail.com>
2023-04-03 17:03:49 -0600

    Adds user story 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.valid?
-      @user.save
-      redirect_to "/users/#{@user.id}"
+    user = User.new(user_params)
+    session[:user_id] = user.id
+    if user.save
+      session[:user_id] = user.id
       flash[:notice] = "User was successfully created"
+      redirect_to "/users/#{user.id}"
     else 
-      flash[:alert] = @user.errors.full_messages.join(", ")
+      flash[:alert] = user.errors.full_messages.join(", ")
       redirect_to "/register"
     end
   end
@@ -27,6 +28,7 @@ class UsersController < ApplicationController
   def login_user
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
       redirect_to "/users/#{user.id}"
     else flash[:error] = "Invalid Credentials"
--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -20,5 +20,28 @@ RSpec.describe 'Login Page', type: :feature do
     click_on 'Login'
 
     expect(current_path).to eq("/users/#{@steve.id}")
+    expect(page).to have_content("Welcome, steve!")
+  end
+
+  describe 'sad paths for user login' do
+   it 'should be redirected to login page if I wrong password' do
+      fill_in :email, with: 'steve@steve.com'
+      fill_in :password, with: 'wrongpassword'
+      fill_in :password_confirmation, with: 'wrongpassword'
+
+      click_on 'Login'
+      expect(page).to have_content("Invalid Credentials")
+      expect(current_path).to eq('/login')
+   end
+
+  #  it 'should be redirected to login page if I wrong email' do
+  #   fill_in :email, with: 'wrong@wrong.com'
+  #   fill_in :password, with: 'steve123'
+  #   fill_in :password_confirmation, with: 'steve123'
+
+  #   click_on 'Login'
+  #   expect(page).to have_content("Invalid Credentials")
+  #   expect(current_path).to eq('/login')
+  #  end
   end
 end
