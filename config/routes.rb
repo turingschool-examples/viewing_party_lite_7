3b4d416851ff0b5f223afce730d3cd6f8322d376
KaraJH <karas.jones@yahoo.com>
2023-04-03 13:42:53 -0700

    feat: Routes for register and login

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get "/register", to: "users#new"
-  post "/register", to: "users#create"
+  get "/login", to: "users#login_form"
+  post "/login", to: "users#login"
+  # post "/register", to: "users#create"
 
   get "/users/:id/discover", to: "users#discover"
   get "/users/:id/movies", to: "users#movie_results"
   get "/users/:user_id/movies/:movie_id", to: "users#movie_details"
 
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :create] do
     resources :movies, only: [:show] do
       resources :viewing_parties, :path => "viewing_party", only: [:new, :create], controller: "viewing_parties" 
     end
