816e39ed17c62af067fa70b9f745d4b5cc84f4fa
Matt Enyeart <menyeart1@gmail.com>
2023-04-03 16:06:03 -0600

    Add additional tests for users model password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
+    it { should validate_presence_of(:password_digest)}
+    it { should have_secure_password}
   end
   describe "relationships" do
     it { should have_many(:viewing_party_users) }
