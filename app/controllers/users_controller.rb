8c8b32690eaf983b26ec4cbac792ac29a4d7bcc1
Matt Enyeart <menyeart1@gmail.com>
2023-04-03 16:01:26 -0600

    Add login form and login user methods, update user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,15 @@ class UsersController < ApplicationController
   end
 
   def new
-    # @new_user = User.new
+    @new_user = User.new
   end
 
   def create
     @new_user = User.new(user_attributes)
-    if @new_user.save
+    if params[:password] != params[:password_confirmation] 
+      redirect_to "/register"
+      flash[:error] = "Passwords do not match"
+    elsif @new_user.save
       redirect_to "/users/#{@new_user.id}"
     else
       redirect_to "/register"
@@ -18,9 +21,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def login_form
+  end
+
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_to user_path(user.id)
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :login_form
+    end
+  end
+
   private
 
   def user_attributes
-    params.permit(:name, :email)
+    params.permit(:name, :email, :password)
   end
 end
