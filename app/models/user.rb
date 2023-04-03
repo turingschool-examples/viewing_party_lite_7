a6fb63b2b44ee0fd5752d776690be4b5eb183308
KaraJH <karas.jones@yahoo.com>
2023-04-03 13:43:45 -0700

    feat: User model with password and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_many :viewing_parties, through: :viewing_party_users
 
   validates_uniqueness_of :email
-  validates_presence_of :name
+  validates :password, confirmation: true
+  validates_presence_of :name, :email, :password, :password_confirmation
+
+  has_secure_password
+
 
   def movie_ids 
     viewing_parties.pluck('movie_id')
