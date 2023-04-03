ddbf780881e8fb80a62913483e9cf9873763e180
David Marino <davidjmarino8@gmail.com>
2023-04-03 17:35:10 -0400

    update testing

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe "'/register'", type: :feature do
     end
 
     it "doesn't create a new user if email has already been used" do
-      User.create(name: "Bob", email: "bob123@example.com", password: "password")
+      bob = User.create(name: "Bob", email: "bob123@example.com", password: "password")
 
       fill_in :name, with: "John"
       fill_in :email, with: "bob123@example.com"
@@ -38,6 +38,22 @@ RSpec.describe "'/register'", type: :feature do
 
       click_button("Create New User")
       expect(page).to have_content("Email already exists")
+    end
+
+    it "should not display the users password" do
+      expect(page).to have_content("Viewing Party")
+      expect(page).to have_content("Register a New User")
+      expect(page).to have_field(:name)
+      expect(page).to have_field(:email)
+      expect(page).to have_button("Create New User")
+
+      fill_in :name, with: "David"
+      fill_in :email, with: "David@example.com"
+      fill_in :password, with: "password"
+      fill_in :password_confirmation, with: "password"
+      click_button("Create New User")
+
+      expect(User.last.password_digest).to_not eq("password")
     end
   end
 end
