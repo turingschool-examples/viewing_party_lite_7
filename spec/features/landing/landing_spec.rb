f987c684905afdf2ee16123ad452f2b0a8509e34
Chris Crane <chrisjcrane@gmail.com>
2023-04-04 11:30:57 -0600

    move user login tests into its own spec file

--- spec/features/landing/landing_spec.rb
+++ spec/features/landing/landing_spec.rb
@@ -34,32 +34,4 @@ RSpec.describe "Landing", type: :feature do
       expect(page).to have_link("Home", href: root_path)
     end
   end
-
-  describe "User Story 3 - Authentication" do
-    it "has a login link that takes me to the login page" do
-      
-      expect(page).to have_link("Log In")
-      click_link "Log In"
-      
-      expect(current_path).to eq(login_path)
-
-      fill_in :email, with: @user_1.email
-      fill_in :password, with: @user_1.password
-      click_button "Log In"
-
-      expect(current_path).to eq(user_path(@user_1.id))
-    end
-  end
-
-  # User Story 4 - Authentication
-  it "will not log in a user with incorrect credentials" do
-    click_link "Log In"
-    fill_in :email, with: @user_1.email
-    fill_in :password, with: "wrong_password"
-
-    click_button "Log In"
-
-    expect(current_path).to eq(login_path)
-    expect(page).to have_content("Invalid Credentials")
-  end
 end
