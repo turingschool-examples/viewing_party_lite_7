04f8fbc78c174a5455fbb308c618792e7792e05a
Andrew Bowman <andrewtbowman@gmail.com>
2023-05-09 12:28:17 -0400

    routes: landing page; user new, create, dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+
+  resources :users, only: [:new, :create]
+
+  get '/users/:id/dashboard', to: 'users#dashboard', as: 'user_dashboard'
 end
