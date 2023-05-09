b66112351c77ed84b1ac61bf42dc5e98ad6af2bf
Andrew Bowman <andrewtbowman@gmail.com>
2023-05-09 12:13:25 -0400

    test: user factory, home page, helper files

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Home page", type: :feature do
+  let!(:users) { create_list(:user, 3) }
+
+  it "home page" do
+    visit root_path
+
+    expect(page).to have_content "Viewing Party"
+
+    expect(page).to have_button "Create a New User"
+
+    users.each do |user|
+      expect(page).to have_link user.name, href: user_dashboard_path(user)
+    end
+  end
+end
