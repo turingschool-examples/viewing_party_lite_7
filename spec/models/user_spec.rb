41f9cdd29be5615078caa2073201301bf6379f65
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-05-09 08:59:37 -0400

    add validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe User, type: :model do
     it { should have_many(:viewing_party_users) }
     it { should have_many(:viewing_parties).through(:viewing_party_users) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:name) }
+  end
 end
--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe ViewingParty, type: :model do
     it { should have_many(:viewing_party_users) }
     it { should have_many(:users).through(:viewing_party_users) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:time) }
+    it { should validate_numericality_of(:duration).only_integer }
+  end
 end
