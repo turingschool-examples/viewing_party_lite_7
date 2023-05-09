04a3fac00f380da5954918bb396c8b0226e0fbec
Katie Lonsdale <katie952@gmail.com>
2023-05-09 16:07:33 -0600

    test: add tests for button to create new user and list of existing users

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -7,21 +7,40 @@
 
 require 'rails_helper'
 
-RSpec.describe 'landing page' do
-  describe 'as a user, when I visit the landing page' do
-    it 'displays the title of the application' do
-      visit '/'
+RSpec.describe "landing page" do
+  describe "as a user, when I visit the landing page" do
+    before(:each) do
+      visit root_path
+      @user_1 = User.create!(name: "Katie", email: "email_address@gmail.com")
+      @user_2 = User.create!(name: "Steve", email: "email_address_2@gmail.com")
+      @user_3 = User.create!(name: "Stacey", email: "email_address_3@gmail.com")
+      @users = [@user_1, @user_2, @user_3]
+    end
+    it "displays the title of the application" do
       within("#title") do
         expect(page).to have_content("Viewing Party")
       end
     end
-    xit 'has a button to create a new user' do
+    it "has a button to create a new user" do
+      within("#new-user") do
+        expect(page).to have_button("Create New User")
+        # click_button("Create New User")
+      end
+
+      # expect(current_path).to eq("/register")
+    end
+    it "has a list of existing users which links to the user dashboard" do
+      within("#users") do
+        @users.each do |user|
+          expect(page).to have_link("#{user.email}")
+          click_link("#{user.email}")
+          expect(current_path).to eq(user_path(user.id))
+          visit root_path
+        end
+      end
 
     end
-    xit 'has a list of existing users which links to the user dashboard' do
-
-    end
-    xit 'has a link to go back to the landing page' do
+    xit "has a link to go back to the landing page" do
 
     end
   end
