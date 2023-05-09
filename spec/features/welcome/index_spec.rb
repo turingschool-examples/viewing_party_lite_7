612996ab09949c46a60ce8ffc33545da35eb0cec
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-09 16:06:24 -0400

    test: add tests for links in landing page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -22,5 +22,21 @@ RSpec.describe "Landing page", type: :feature do
         expect(page).to have_link(@user3.name)
       end
     end
+
+    it 'When I click on the register button I see the Home link on that page and it takes me back to the root' do
+      click_button "Register"
+      expect(current_path).to eq("/register")
+      expect(page).to have_link("Home")
+      click_link "Home"
+      expect(current_path).to eq(root_path)
+    end
+    
+    it 'When I click on a users name I see the Home link on the page and it takes me back to the root' do
+      click_link "#{@user1.name}"
+      expect(current_path).to eq(user_path(@user1))
+      expect(page).to have_link("Home")
+      click_link "Home"
+      expect(current_path).to eq(root_path)
+    end
   end
 end
