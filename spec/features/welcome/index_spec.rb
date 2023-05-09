f80d07616f4e1d2683f984a0f7d20ca75154d9bf
Andrew Bingham <andrewgbingham@gmail.com>
2023-05-09 16:45:58 -0600

    test: create feature tests for user new and show

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,38 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome Page', type: :feature do
-  before (:each) do
-    @user_1 = User.create!(name: "John Doe", email: "johndoe@yahoo.com" )
-    @user_2 = User.create!(name: "Alex Smith", email: "Asmith@yahoo.com" )
-    @user_3 = User.create!(name: "Elvis Presley", email: "kingofrock@yahoo.com" )
+  before(:each) do
+    @user_1 = User.create!(name: 'John Doe', email: 'johndoe@yahoo.com')
+    @user_2 = User.create!(name: 'Alex Smith', email: 'Asmith@yahoo.com')
+    @user_3 = User.create!(name: 'Elvis Presley', email: 'kingofrock@yahoo.com')
     visit root_path
   end
+
   describe 'Landing Pages #3' do
-    it "exists" do
+    it 'exists' do
       expect(current_path).to eq(root_path)
     end
-    it "displays title of application" do
-      within("#header") do
-        expect(page).to have_content("Viewing Party")
+
+    it 'displays title of application' do
+      within('#header') do
+        expect(page).to have_content('Viewing Party')
       end
     end
-    it "displays button to create a new user" do
-      within("#button") do
-        expect(page).to have_button("Create a New User")
+
+    it 'displays button to create a new user' do
+      within('#button') do
+        expect(page).to have_button('Create a New User')
       end
     end
-    it "displays a list of existing users" do
-      within("#existing-users") do
-        expect(page).to have_content("Existing Users")
+
+    it 'displays a list of existing users' do
+      within('#existing-users') do
+        expect(page).to have_content('Existing Users')
         expect(page).to have_content(@user_1.email)
         expect(page).to have_content(@user_2.email)
         expect(page).to have_content(@user_3.email)
       end
     end
-    it "displays a link to go back to landing page" do
-      within("#header") do
-        expect(page).to have_link("Home")
-        click_link("Home")
+
+    it 'displays a link to go back to landing page' do
+      within('#header') do
+        expect(page).to have_link('Home')
+        click_link('Home')
         expect(current_path).to eq(root_path)
       end
     end
