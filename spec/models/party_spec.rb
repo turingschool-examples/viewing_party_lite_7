1ad3b4262b36b8ae97b58f09b7dfb2eb130e9f99
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-05-09 15:11:00 -0600

    add migrations, models, and model testing

---
+++ spec/models/party_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Party do
+  describe "relationships" do
+    it {should have_many(:user_parties)}
+    it {should have_many(:users).through( :user_parties)}
+  end
+
+  describe "validations" do
+    it { should validate_numericality_of :duration }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :time }
+    it { should validate_presence_of :host }
+  end
+end
