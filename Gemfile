77b28bcd47847fdaf2a61baeea80318085aad935
Sean Cowans <zedesia@yahoo.com>
2023-05-08 19:58:34 -0600

    feat: Create landing page and user

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.1.1"
+ruby "3.1.3"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
@@ -51,6 +51,15 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "pry"
+  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'launchy'
+  gem 'orderly'
+  gem 'shoulda-matchers'
+  gem 'net-http'
+  gem 'jquery-rails'
+  gem 'turbolinks'
 end
 
 group :development do
---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "welcome index" do
+  before :each do
+    @user_1 = User.create!(name: "JoJo", email: "JoJo@hotmail.com")
+    @user_2 = User.create!(name: "JaJa", email: "JaJa@hotmail.com")
+    visit "/"
+  end
+
+  it 'has application title' do
+    expect(page).to have_content("Viewing Party")
+  end
+
+  it 'has button to create new user' do
+    expect(page).to have_button("Create New User")
+  end
+
+  it 'has list of existing users' do
+    expect(page).to have_content("Users")
+    within '#users' do
+      expect(page).to have_link(@user_1.name)
+      expect(page).to have_link(@user_2.name)
+    end
+  end
+
+  it 'has link to go back to home page' do
+    within '#nav-container' do
+      expect(page).to have_link("Home")
+    end
+  end
+end
