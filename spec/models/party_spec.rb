5b052806085b73fe673828972e0fef7c852fcafc
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-05-09 16:10:28 -0600

    register user page with tests

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Party do
   describe "relationships" do
-    it {should have_many(:user_parties)}
-    it {should have_many(:users).through( :user_parties)}
+    it { should have_many(:user_parties) }
+    it { should have_many(:users).through(:user_parties) }
   end
 
   describe "validations" do
