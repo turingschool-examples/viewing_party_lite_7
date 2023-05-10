15bdaeedca855660348443ce79b65a71b78ddd2b
Katie Lonsdale <katie952@gmail.com>
2023-05-10 14:00:09 -0600

    refactor: add secure token

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -7,7 +7,7 @@ class MovieService
 
   def get_url(url)
     response = Faraday.new(url: url).get do |faraday|
-      faraday.headers["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Zjk3MDk2OWQyYWE5NDJmOTA2YmI0MGNjMWY2MmUyMCIsInN1YiI6IjY0NTliY2RlYWUzODQzMDExYjUxODU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z7oDek4ow3wESMafRwa0Hlte66LPdTmTGBfsWsFC6C0'
+      faraday.headers["Authorization"] = ENV['MOVIEDB_AUTHORIZATION_TOKEN']
     end
     JSON.parse(response.body, symbolize_names: true)
   end
