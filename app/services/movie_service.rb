cfe5279403a7c63416d96ec9f02ba582fee14da1
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-10 10:21:07 -0600

    Create: movie_service

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,25 +1,22 @@
-require 'faraday'
-require 'figaro'
-
 class MovieService
   def search(key_word)
-    get_url("/search/movie?query=#{key_word}")
+    get_url("/3/search/movie?query=#{key_word}")
   end
 
   def top_rated
-    get_url("/movie/top_rated")
+    get_url("/3/movie/top_rated")
   end
 
+  private
   def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
-
+  
   def conn
-    Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
-      faraday.headers["api_key"] = ENV["moviedb_api_key"]
+    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = ENV['moviedb_api_key']
+      faraday.params["language"] = "en-US"
     end
   end
 end
-
-require 'pry'; binding.pry
