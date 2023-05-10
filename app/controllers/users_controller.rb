c0ab00389d0473077c0542fe73f76be37c63b8bc
Crow Rising <miscbyrd@gmail.com>
2023-05-09 18:18:12 -0600

    feat: add action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new 
+    
+  end
+  
+  def create
+    #reroute to dashboard page with user/id show page
+  end
 end
