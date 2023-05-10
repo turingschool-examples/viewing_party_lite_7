8ac59b9332375966870bdd7b0aea079ea8f2bbc6
Thomas Hawley <thawley22@turing.edu>
2023-05-10 16:06:34 -0600

    refactor: Add private method create_movies to clean up class methods

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,16 +1,17 @@
 class MovieFacade
-  
-  def self.top_rated_movies
-    movies = TmbdService.top_rated_movies[:results]
 
-    movies.map do |movie_data|
-      Movie.new(movie_data)
-    end
+  def self.top_rated_movies
+    movies = TmbdService.top_rated_movies
+    create_movies(movies)
   end
 
   def self.search_movies(search)
     movies = TmbdService.search_movies(search)
+    create_movies(movies)
+  end
 
+  private
+  def self.create_movies(movies)
     movies[:results].map do |movie_data|
       Movie.new(movie_data)
     end
