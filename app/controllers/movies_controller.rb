c821c76a90e19c12425dd515f518fe22093eba7b
Thomas Hawley <thawley22@turing.edu>
2023-05-10 17:14:43 -0600

    feat: Add logic to the index action and create show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,14 @@
 class MoviesController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
     if params[:q].present?
-      @movies
+      @movies = MovieFacade.search_movies(params[:q])
     else
-      @movies
+      @movies = MovieFacade.top_rated_movies
     end
+  end
+
+  def show
+    require 'pry'; binding.pry
   end
 end
