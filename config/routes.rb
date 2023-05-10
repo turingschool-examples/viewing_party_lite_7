5a5540c5d758042adc4d102820b3c6819bd981f4
Andrew Bowman <andrewtbowman@gmail.com>
2023-05-10 17:12:43 -0400

    routes for discover and movies pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    get 'discover', on: :member
+    get 'movies', on: :member
+  end
 
   # get '/users/:id/dashboard', to: 'users#dashboard', as: 'user_dashboard'
 end
