8ddf55f6e1d60b5485aef0ec0fd832947051a1af
Katie Lonsdale <katie952@gmail.com>
2023-05-10 13:26:10 -0600

    add routes for movies search action

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
 
   root "application#welcome"
 
-  get '/register', to: 'users#new'
-  get '/users/:user_id', to: 'users#dashboard'
-  get "/users/:user_id/discover", to: 'users#discover'
+  get "/register", to: "users#new"
+  get "/users/:user_id", to: "users#dashboard"
+  get "/users/:user_id/discover", to: "movies#discover"
+  get "/users/:user_id/movies", to: "movies#index"
+  get "/movies/search", to: "movies#search"
 
   resources :users, only: [:create]
 end
