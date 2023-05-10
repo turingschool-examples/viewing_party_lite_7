4330f6427b426fac6969dd52541018ad0459ad6b
Crow Rising <miscbyrd@gmail.com>
2023-05-10 16:34:29 -0600

    test: add to test

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe MovieFacade do
 
     expect(movie.title).to eq('The Godfather')
     expect(movie.vote_average).to eq(8.713)
+    expect(movie.overview).to eq("Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.")
   end
 
   it 'creates top rated movies poros', :vcr do
