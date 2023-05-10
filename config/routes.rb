0c506f4439c2ec9bb634b44670b1ac92317e553e
Thomas Hawley <thawley22@turing.edu>
2023-05-10 17:15:45 -0600

    feat: Add route to movies#show

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :show] do
     get 'discover', to: 'users/discover#index'
-    resources :movies, only:[:index]
+    resources :movies, only:[:index, :show]
   end
 end
