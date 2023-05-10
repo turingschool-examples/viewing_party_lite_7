b58e02f10da75688d41041f307f0687352e3ff26
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-10 11:39:13 -0600

    Create: movie facade and movie poro

---
+++ app/facades/movie_facade.rb
@@ -0,0 +1,22 @@
+class MovieFacade
+  def search(key_word)
+    results = service.search(key_word)[:results]
+
+    results.map do |result|
+      Movie.new(result)
+    end
+  end
+
+  def top_rated
+    results = service.top_rated[:results]
+
+    results.map do |result|
+      Movie.new(result)
+    end
+  end
+
+  private
+  def service
+    MovieService.new
+  end
+end
