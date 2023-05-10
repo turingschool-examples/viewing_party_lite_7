5ac6d0129be9e758c43d69cfadf029e7e27e259a
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-10 10:33:17 -0600

    Make create & new actions, routes for them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(strong_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Please fill in all fields, email must be unique"
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  
+  def strong_params
+    params.require(:user).permit(:email, :name)
+  end
 end
