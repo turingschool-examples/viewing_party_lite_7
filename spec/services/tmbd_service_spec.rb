1b317d4149241a3c924ab4fdfe86b1fecde68d9f
Thomas Hawley <thawley22@turing.edu>
2023-05-10 14:31:34 -0600

    test: Add tmbd service tests

---
+++ spec/services/tmbd_service_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe TmbdService do
+  describe 'Establish connection' do
+    it 'can retreive a list of the top 20 movies', :vcr do
+      top_movies = TmbdService.top_rated_movies
+
+      expect(top_movies).to be_a(Hash)
+      expect(top_movies[:results]).to be_an(Array)
+      expect(top_movies[:results].first[:title]).to be_a(String)
+      expect(top_movies[:results].count).to eq(20)
+    end
+
+    it 'can retrieve a list of search results' do
+      VCR.use_cassette('search_movies_tremors') do
+        search_movies = TmbdService.search_movies('Tremors')
+
+        expect(search_movies).to be_a(Hash)
+        expect(search_movies[:results]).to be_an(Array)
+        expect(search_movies[:results].first[:title]).to be_a(String)
+        expect(search_movies[:results].first[:title]).to eq('Tremors')
+      end
+    end
+  end
+end
