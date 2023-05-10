91f14c43b1ac919b32d290c063e89179c03b18f0
Katie Lonsdale <katie952@gmail.com>
2023-05-10 13:27:41 -0600

    feat: add movie facade

---
+++ app/facades/movie_facade.rb
@@ -0,0 +1,16 @@
+class MovieFacade
+  def initialize
+  end
+
+  def find_movies(query)
+    service = MovieService.new
+    json = service.movies(query)[:results]
+
+    movies = []
+    json.each_with_index do |info, i|
+      break if i > 19
+      movies << Movie.new(info)
+    end
+    movies
+  end
+end
