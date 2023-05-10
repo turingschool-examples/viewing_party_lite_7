36acfed3979dade2455c2ecd810dd5b7f5d7cff5
Crow Rising <miscbyrd@gmail.com>
2023-05-09 18:55:34 -0600

    add post route for regieter user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/register', controller: 'users', to: 'users#new'
-
+  post '/register', controller: 'users', to: 'users#create'
+  
   resources :users
   resources :user_viewing_parties
   resources :viewing_parties
