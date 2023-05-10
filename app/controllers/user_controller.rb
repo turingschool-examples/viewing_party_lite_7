183007787eeb45b40b3645f1a943aaec30a64661
Logan Cole <emtlogancole@gmail.com>
2023-05-10 13:37:33 -0500

    user create route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,21 @@ class UserController < ApplicationController
   def new
 
   end
+
+  def method_name
+    
+  end
   
+  def create
+    user = User.create(user_params)
+      if user.save
+        redirect_to user_path(user)
+        flash[:notice] = "Welcome #{user.name}!"
+      else
+        flash[:notice] = "Registration Failed"
+        render :new
+      end
+  end
 
   private
   def user_params
