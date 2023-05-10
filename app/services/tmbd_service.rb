e1d333f571bd242d6d8adc07f16d69397724ce34
Thomas Hawley <thawley22@turing.edu>
2023-05-10 15:54:29 -0600

    refactor:Clean up methods to conform to SRP

--- app/services/tmbd_service.rb
+++ app/services/tmbd_service.rb
@@ -1,19 +1,21 @@
 class TmbdService
+  
+  def self.top_rated_movies
+    get_url('/3/movie/top_rated')
+  end
+  
+  def self.search_movies(search)
+    get_url("/3/search/movie?query=#{search}")
+  end 
+
   def self.conn
     Faraday.new(url: 'https://api.themoviedb.org') do |f|
       f.params['api_key'] = ENV['tmbd_key']
     end
   end
 
-    def self.top_rated_movies
-      response = conn.get('/3/movie/top_rated')
-
-      JSON.parse(response.body, symbolize_names: true)
-    end
-
-    def self.search_movies(search)
-      response = conn.get("/3/search/movie?query=#{search}")
-
-      JSON.parse(response.body, symbolize_names: true)
-    end
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
