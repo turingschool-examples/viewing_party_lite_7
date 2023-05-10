3b0aad601acdf0dfea50da8be15eea5b7ffd24f5
Andrew Bowman <andrewtbowman@gmail.com>
2023-05-10 18:46:23 -0400

    feat: moviedbservice for api calls via faraday

---
+++ app/services/movie_db_service.rb
@@ -0,0 +1,20 @@
+class MovieDbService
+  def self.conn
+    Faraday.new(url: 'https://api.themoviedb.org') do |req|
+      req.headers['Authorization'] = "Bearer #{ENV['TMDB_V4_TOKEN']}"
+      req.headers['Content-Type'] = 'application/json;charset=utf-8'
+    end
+  end
+
+  def self.search_movies(query)
+    response = conn.get("/3/search/movie") do |req|
+      req.params['query'] = query
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.movie_details(id)
+    response = conn.get("/3/movie/#{id}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
