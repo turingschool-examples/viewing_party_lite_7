27fc164ab74110252025f3387e289946f7f8c03e
Andrew Bingham <119075417+andrew-bingham1@users.noreply.github.com>
2023-05-09 19:02:02 -0600

    Update users_controller.rb
    remove a pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @viewing_parties = ViewingParty.find_viewing_parties(@user)
-    # require 'pry'; binding.pry
   end
 
 end
