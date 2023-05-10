84f58f8d07847f86cc0523b16c837d0e17260c67
Logan Cole <emtlogancole@gmail.com>
2023-05-10 11:52:36 -0500

    add user seed to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+class Seed
+  def self.start
+    seed = Seed.new
+    seed.generate_users
+  end
+ 
+ 
+  def generate_users
+    50.times do |i|
+      user = User.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email
+        )
+      puts "User #{i}: #{user.name} - #{user.email} created!"
+    end
+  end
+ end
+ 
+ 
+ Seed.start
+ 
