8db89994b660f8731d487d7c79cf9a47d10431d2
Crow Rising <miscbyrd@gmail.com>
2023-05-09 18:54:26 -0600

    feat: add create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,21 @@ class UsersController < ApplicationController
   end
 
   def new 
-    
+
   end
   
   def create
-    #reroute to dashboard page with user/id show page
+    @new_user = User.new(user_params)
+    if @new_user.save
+      redirect_to user_path(@new_user)
+    else
+      flash.notice = "Oops, please try again your email is not unique."
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:name, :email)
   end
 end
