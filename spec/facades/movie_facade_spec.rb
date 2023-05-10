b4188b4d432af2550d758f709c8ccf0a5b7988e1
Thomas Hawley <thawley22@turing.edu>
2023-05-10 16:00:47 -0600

    test: Add test for search movie method of the movie facade

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MovieFacade do
-  describe 'Top movies creation' do
+  describe 'Top movies object creation' do
     it 'returns an array of the top movies in movie objects' do
       VCR.use_cassette('TmbdService/Establish_connection/can_retreive_a_list_of_the_top_20_movies') do
 
@@ -9,6 +9,19 @@ RSpec.describe MovieFacade do
         expect(top_movies).to be_an(Array)
         expect(top_movies.first).to be_a(Movie)
         expect(top_movies.count).to eq(20)
+      end
+    end
+  end
+
+  describe 'Search movie object creation' do
+    it 'returns an array of movies searched with a specific keyword' do
+
+      VCR.use_cassette('search_movies_tremors') do
+        
+        search_movies = MovieFacade.search_movies('Tremors')
+        expect(search_movies).to be_an(Array)
+        expect(search_movies.first).to be_a(Movie)
+        expect(search_movies.count).to eq(20)
       end
     end
   end
