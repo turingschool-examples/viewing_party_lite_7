5700730780c1c0ed3451cee2f2a24efb4e539538
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-10 17:30:34 -0400

    feat: added functionality for the movies results page

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,20 +1,20 @@
 class MovieService
-  def conn
+  def self.conn
     Faraday.new(url: "https://api.themoviedb.org/3") do |faraday|
       faraday.params["api_key"] = ENV["MOVIE_API_KEY"]
     end
   end
 
-  def get_url(url)
+  def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def top_rated_movies
-    get_url("movie/top_rated")
+  def self.top_rated_movies
+    get_url("https://api.themoviedb.org/3/movie/top_rated")
   end
 
-  def movie_search(query)
-    get_url("search/movie?query=#{query}")
+  def self.movie_search(query)
+    get_url("https://api.themoviedb.org/3/search/movie?query=#{query}")
   end
 end
