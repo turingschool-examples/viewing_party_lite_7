df5ee97e218af2d8e0fba2054c367eb11b2ff1c8
Katie Lonsdale <katie952@gmail.com>
2023-05-10 13:44:30 -0600

    remove search action path

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get "/users/:user_id", to: "users#dashboard"
   get "/users/:user_id/discover", to: "movies#discover"
   get "/users/:user_id/movies", to: "movies#index"
-  get "/movies/search", to: "movies#search"
 
   resources :users, only: [:create]
 end
