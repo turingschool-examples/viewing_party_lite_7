495869a65a6e412c2f7bfe2389853c28d03192e0
Katie Lonsdale <katie952@gmail.com>
2023-05-11 17:27:28 -0600

    fix: remove extra spaces

--- spec/poros/review_spec.rb
+++ spec/poros/review_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Review do
     it "exists and has attributes" do
       expect(@review).to be_a Review
       expect(@review.author).to eq("Goddard")
-      expect(@review.content).to eq("Pretty awesome movie.  It shows what one crazy person can convince other crazy people to do.  Everyone needs something to believe in.  I recommend Jesus Christ, but they want Tyler Durden.")
+      expect(@review.content).to eq("Pretty awesome movie. It shows what one crazy person can convince other crazy people to do. Everyone needs something to believe in. I recommend Jesus Christ, but they want Tyler Durden.")
     end
   end
 end
