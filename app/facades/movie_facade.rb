d746f21b80bf79a741e15dcb710b492ae20aeeba
Thomas Hawley <thawley22@turing.edu>
2023-05-10 19:13:38 -0600

    feat: Update facade spec and facade to include finding movie details by id

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -10,10 +10,27 @@ class MovieFacade
     create_movies(movies)
   end
 
+  def self.find_movie(id)
+    movie = TmbdService.find_movie(id)
+    create_movie(movie)
+  end
+
   private
   def self.create_movies(movies)
     movies[:results].map do |movie_data|
       Movie.new(movie_data)
     end
+  end
+
+  def self.create_movie(movie)
+    movie_data = {
+      id: movie[:id],
+      title: movie[:title],
+      vote_average: movie[:vote_average],
+      runtime: movie[:runtime],
+      genres: movie[:genres].map {|genre| genre[:name]},
+      summary: movie[:overview]
+    }
+    Movie.new(movie_data)
   end
 end
--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -15,13 +15,29 @@ RSpec.describe MovieFacade do
 
   describe 'Search movie object creation' do
     it 'returns an array of movies searched with a specific keyword' do
-
       VCR.use_cassette('search_movies_tremors') do
         
         search_movies = MovieFacade.search_movies('Tremors')
         expect(search_movies).to be_an(Array)
         expect(search_movies.first).to be_a(Movie)
         expect(search_movies.count).to eq(20)
+      end
+    end
+
+    describe 'Find movie by id' do
+      it 'returns a single movie object' do
+        VCR.use_cassette('find_movie_by_id_550') do
+          movie = MovieFacade.find_movie(550)
+          
+          expect(movie.id).to be_a(Integer)
+          expect(movie.title).to be_a(String)
+          expect(movie.vote_average).to be_a(Float)
+          expect(movie.runtime).to be_a(Integer)
+          expect(movie.genres).to be_a(Array)
+          expect(movie.genres.first).to be_a(String)
+          expect(movie.summary).to be_a(String)
+
+        end
       end
     end
   end
