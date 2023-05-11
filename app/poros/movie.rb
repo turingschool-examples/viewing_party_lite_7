156883e77b2e342257a951432ae21d8c34ce4b46
Thomas Hawley <thawley22@turing.edu>
2023-05-10 19:17:59 -0600

    feat: Update movie poro and spec to include needed attributes

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,9 +1,12 @@
 class Movie
-  attr_reader :id, :title, :vote_average
+  attr_reader :id, :title, :vote_average, :runtime, :genres, :summary
   
   def initialize(data)
     @id = data[:id]
     @title = data[:title]
     @vote_average = data[:vote_average]
+    @runtime = data[:runtime]
+    @genres = data[:genres]
+    @summary = data[:summary]
   end
 end
--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  it 'exists' do
-    data = {id: 1, title: 'Tremors', vote_average: 9.7}
+  it 'exists and has attributes' do
+    data = {
+            id: 1, 
+            title: 'Tremors', 
+            vote_average: 9.7,
+            runtime: 160,
+            genres: ['drama', 'thriller'],
+            summary: 'A really really good movie'
+          }
     movie = Movie.new(data)
 
     expect(movie).to be_a(Movie)
     expect(movie.id).to be_an(Integer)
     expect(movie.title).to be_a(String)
     expect(movie.vote_average).to be_a(Float)
+    expect(movie.runtime).to be_a(Integer)
+    expect(movie.genres).to be_a(Array)
+    expect(movie.summary).to be_a(String)
 
     expect(movie.id).to eq(1)
     expect(movie.title).to eq('Tremors')
     expect(movie.vote_average).to eq(9.7)
-
+    expect(movie.runtime).to eq(160)
+    expect(movie.genres).to eq(['drama', 'thriller'])
+    expect(movie.summary).to eq('A really really good movie')
   end
 end
