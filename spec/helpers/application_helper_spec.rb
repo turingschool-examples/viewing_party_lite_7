e1d06fdc930b0ddee90c84964a3465e74e11ce9d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-11 14:36:23 -0400

    merge: push files from solo spike to be combined with Hawley Solo Spike for refactoring

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper do
+  describe 'helper methods' do
+    describe '#format_runtime(runtime)' do
+      it 'formats a runtime in minutes to a string with hours and minutes' do
+        expect(format_runtime(100)).to eq("1h 40m")
+      end
+    end
+
+    describe '#format_genres(genres)' do
+      it 'formats an array of genre strings to a single string seperated by commas' do
+        genres = ['Comedy', 'Drama', 'Horror']
+        expect(format_genres(genres)).to eq("Comedy, Drama, Horror")
+      end
+    end
+
+    describe '#count_reviews(reviews)' do
+      it 'counts the number of reviews hashes in a single reviews array' do
+        reviews = [{a: "a"}, {b: "b"}, {c: "c"}]
+        expect(count_reviews(reviews)).to eq(3)
+      end
+    end
+  end
 end
