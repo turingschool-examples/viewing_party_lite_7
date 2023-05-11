0ea8612be2d88dbce02a873f4e43034b1b331af6
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-11 13:31:39 -0600

    add webmock and vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -100,3 +100,10 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data("<MOVIES_API_READ_ACCESS_TOKEN>") { ENV["MOVIES_API_READ_ACCESS_TOKEN"]}
+  config.default_cassette_options = { re_record_interval: 7.days } # this will re-record the cassette every 7 days
+  config.configure_rspec_metadata!
+end
