ce225d4d9f9beeebdd4cfbfd6d1801c59edcbd23
Katie Lonsdale <katie952@gmail.com>
2023-05-11 11:58:57 -0600

    test: test top_rated_movies method

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -20,4 +20,12 @@ RSpec.describe MovieFacade do
       expect(results).to all be true
     end
   end
+  describe "top_rated_movies" do
+    it "returns a list of the top rated movies", :vcr do
+      movies = @mf.top_rated_movies
+      expect(movies).to all be_a Movie
+      expect(movies.count <= 20).to be true
+      expect(movies.first.title).to eq("The Godfather")
+    end
+  end
 end
