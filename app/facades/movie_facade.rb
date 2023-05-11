bac31b419bbf377c59c32b3d6ad2102cb0f98162
Thomas Hawley <thawley22@turing.edu>
2023-05-10 21:40:21 -0600

    update: Add methods to retrieve cast and reviews as helper methods, add #all_movie_data method to combine movie, cast and review calls along with tests

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -10,27 +10,50 @@ class MovieFacade
     create_movies(movies)
   end
 
-  def self.find_movie(id)
-    movie = TmbdService.find_movie(id)
-    create_movie(movie)
+  def self.all_movie_data(id)
+    movie = find_movie(id)
+    reviews = find_reviews(id)[:results]
+    cast = find_cast(id)[:cast][0..9]
+    combine_data(movie, reviews, cast)
   end
 
+  
   private
+  def self.find_movie(id)
+    movie = TmbdService.find_movie(id)
+    # create_movie(movie)
+  end
+
+  def self.find_reviews(id)
+    reviews = TmbdService.find_reviews(id)
+  end
+
+  def self.find_cast(id)
+    cast = TmbdService.find_cast(id)
+  end
+  
   def self.create_movies(movies)
     movies[:results].map do |movie_data|
       Movie.new(movie_data)
     end
   end
 
-  def self.create_movie(movie)
+  def self.create_movie(data)
+    Movie.new(data)
+  end
+
+  def self.combine_data(movie, reviews, cast)
     movie_data = {
       id: movie[:id],
       title: movie[:title],
       vote_average: movie[:vote_average],
       runtime: movie[:runtime],
       genres: movie[:genres].map {|genre| genre[:name]},
-      summary: movie[:overview]
+      summary: movie[:overview],
+      poster: 'https://image.tmdb.org/t/p/original' + movie[:poster_path],
+      reviews: reviews,
+      cast: cast
     }
-    Movie.new(movie_data)
+    create_movie(movie_data)
   end
 end
--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -24,11 +24,12 @@ RSpec.describe MovieFacade do
       end
     end
 
-    describe 'Find movie by id' do
-      it 'returns a single movie object' do
-        VCR.use_cassette('find_movie_by_id_550') do
-          movie = MovieFacade.find_movie(550)
-          
+    describe '#all_movie_data' do
+      it 'returns an object that stores all movie data needed for show page' do
+        VCR.use_cassette('all_movie_data_by_id_550') do
+          movie = MovieFacade.all_movie_data(550)
+
+          expect(movie).to be_a(Movie)
           expect(movie.id).to be_a(Integer)
           expect(movie.title).to be_a(String)
           expect(movie.vote_average).to be_a(Float)
@@ -36,7 +37,20 @@ RSpec.describe MovieFacade do
           expect(movie.genres).to be_a(Array)
           expect(movie.genres.first).to be_a(String)
           expect(movie.summary).to be_a(String)
+          expect(movie.poster).to be_a(String)
+          expect(movie.reviews).to be_an(Array)
 
+          movie.reviews.each do |review|
+            expect(review[:author]).to be_a(String)
+            expect(review[:content]).to be_a(String)
+          end
+
+          movie.cast.each do |cast|
+            expect(cast[:name]).to be_a(String)
+            expect(cast[:character]).to be_a(String)
+          end
+
+          expect(movie.cast.count).to eq(10)
         end
       end
     end
