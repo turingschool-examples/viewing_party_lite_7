b996af869bbf0347154d4799fa260515160802de
Katie Lonsdale <katie952@gmail.com>
2023-05-11 17:30:49 -0600

    add test data

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -217,3 +217,20 @@ def test_movie_details
     "vote_count": 26372
   }
 end
+
+def test_review
+  @data = {
+    "author": "Goddard",
+    "author_details": {
+        "name": "",
+        "username": "Goddard",
+        "avatar_path": "/https://secure.gravatar.com/avatar/f248ec34f953bc62cafcbdd81fddd6b6.jpg",
+        "rating": nil
+    },
+    "content": "Pretty awesome movie. It shows what one crazy person can convince other crazy people to do. Everyone needs something to believe in. I recommend Jesus Christ, but they want Tyler Durden.",
+    "created_at": "2018-06-09T17:51:53.359Z",
+    "id": "5b1c13b9c3a36848f2026384",
+    "updated_at": "2021-06-23T15:58:09.421Z",
+    "url": "https://www.themoviedb.org/review/5b1c13b9c3a36848f2026384"
+  } 
+end
