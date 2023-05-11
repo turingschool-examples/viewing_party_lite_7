f74537c3fa4839e0683e3c38685da55fa96d73e5
Katie Lonsdale <katie952@gmail.com>
2023-05-10 18:23:18 -0600

    feat: add vote average attribute

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,9 +1,10 @@
 # so movies from API can be converted to POROs
 class Movie
-  attr_reader :title, :id
+  attr_reader :title, :id, :vote_average
 
   def initialize(data)
     @title = data[:original_title]
     @id = data[:id]
+    @vote_average = data[:vote_average]
   end
 end
