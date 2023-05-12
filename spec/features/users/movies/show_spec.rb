1946afe76f9d5661c9a7a71391fe9e397fe304de
Crow Rising <miscbyrd@gmail.com>
2023-05-12 10:20:41 -0600

    test: add test for user story 10

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -5,22 +5,17 @@ RSpec.describe 'user story 10' do
     before(:each) do
       @user1 = User.create(name: 'Bob', email: 'bob@email.com')
       @user2 = User.create(name: 'Sally', email: 'sally@email.com')
-    end 
+    end
 
     it 'displays a button to create a viewing party', :vcr do
-      # visit user_movie_path(@user1, 324857)
       visit "/users/#{@user1.id}/movie/324857"
 
       within '#create-party' do
         expect(page).to have_button('Create Viewing Party for Movie')
         click_button 'Create Viewing Party for Movie'
       end
-
-      # expect(current_path).to eq("/users/#{@user1.id}/movie/324857/viewing_parties/new")
       expect(current_path).to eq(new_user_movie_viewing_party_path(@user1, 324_857))
     end
-
-
 
     it 'displays a button to return to the discover page', :vcr do
       visit "/users/#{@user1.id}/movie/324857"
@@ -31,6 +26,70 @@ RSpec.describe 'user story 10' do
       end
 
       expect(current_path).to eq(user_discover_index_path(@user1))
+    end
+
+    it 'displays movie title and vote average', :vcr do
+      visit "/users/#{@user1.id}/movie/324857"
+
+      within "#movie-details" do
+        expect(page).to have_content("Spider-Man: Into the Spider-Verse")
+        expect(page).to have_content(8.4)
+      end
+    end
+
+    it 'displays movie runtime in hours and minutes', :vcr do
+      visit "/users/#{@user1.id}/movie/324857"
+
+      within "#movie-details" do
+        expect(page).to have_content("1h 57min")
+      end
+    end
+
+    it 'displays movie genre', :vcr do
+      visit "/users/#{@user1.id}/movie/324857"
+
+      within "#movie-details" do
+        expect(page).to have_content("Action")
+        expect(page).to have_content("Adventure")
+        expect(page).to have_content("Animation")
+        expect(page).to have_content("Science Fiction")
+      end
+    end
+
+    it 'displays summary of movie', :vcr do
+      visit "/users/#{@user1.id}/movie/324857"
+save_and_open_page
+      within "#movie-details" do
+        expect(page).to have_content("Miles Morales is juggling his life between being a high school student and being a spider-man. When Wilson \"Kingpin\" Fisk uses a super collider, others from across the Spider-Verse are transported to this dimension.")
+      end
+    end
+
+    xit 'displays first 10 cast members', :vcr do
+      visit "/users/#{@user1.id}/movie/324857"
+
+      within "#movie-details" do
+        expect(page).to have_content()
+        expect(page).to have_content()
+      end
+    end
+
+    xit 'displays count of total reviews', :vcr do
+      visit new_user_movie_viewing_party_path(@user1, 324_857)
+
+      within "#movie-details" do
+        expect(page).to have_content()
+      end
+    end
+
+    xit 'displays each reviews author and information', :vcr do
+      visit new_user_movie_viewing_party_path(@user1, 324_857)
+      
+      within "#movie-details" do
+        expect(page).to have_content()
+        expect(page).to have_content()
+        expect(page).to have_content()
+        expect(page).to have_content()
+      end
     end
   end
 end
