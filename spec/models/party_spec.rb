cb907f655ee6db8b703fdaa44c32f4240de644a8
Katie Lonsdale <katie952@gmail.com>
2023-05-12 10:49:45 -0600

    test: add guests test

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Party do
     it { should validate_presence_of :date }
     it { should validate_presence_of :time }
   end
+
+  describe "instance methods" do
+    before(:each) do
+      test_data
+    end
+    describe "guests" do
+      it "returns list of users who are not hosting party" do
+        expect(@party_1.guests).to eq([@user_2, @user_3])
+        expect(@party_2.guests).to eq([@user_4, @user_5])
+      end
+    end
+  end
 end
