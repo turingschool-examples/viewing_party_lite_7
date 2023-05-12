7e9f805543bcbef2bb4636b0570f61ba8f2d9dcd
Grace Joh <graceehjoh@gmail.com>
2023-05-12 10:37:40 -0500

    fix: Party factory date

--- spec/factories/party.rb
+++ spec/factories/party.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :party do
     start_time { Faker::Time.between(from: Time.now - 240, to: Time.now + 240) }
-    date { Faker::Time.between(from: Date.now - 10, to: Date.now + 10) }
+    date { Faker::Time.between(from: Date.new(2023, 5, 1), to: Date.new(2023, 5, -1)) }
     duration { Faker::Number.between(from: 90, to: 240) }
     movie_id { Faker::Number.between(from: 100, to: 1000) }
   end
