634636203b65368373ae7dcd71b098993444afb9
Andrew Bowman <andrewtbowman@gmail.com>
2023-05-12 17:27:14 -0400

    refactors for readability and simplicity

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,34 +1,35 @@
 class Movie
-  attr_reader :id, :title, :vote_average, :poster_path, :runtime, :genres, :summary, :cast_members, :review_count, :reviews
+  attr_reader :id, :title, :vote_average, 
+              :poster_path, :runtime, :genres, :summary, 
+              :cast_members, :review_count, :reviews
 
-  def initialize(data, credits = {}, reviews = [])
-    @id = data[:id]
-    @title = data[:title]
-    @vote_average = data[:vote_average]
-    @poster_path = data[:poster_path]
+  def initialize(info, credits = {}, reviews = [])
+    @id = info[:id]
+    @title = info[:title]
+    @vote_average = info[:vote_average]
+    @poster_path = info[:poster_path]
 
-    if data[:genres]
-      @runtime = data[:runtime]
-      @genres =  data[:genres].map { |g| g[:name] }
-      @summary = data[:overview]
+    if info[:genres]
+      @runtime = info[:runtime]
+      @genres =  info[:genres].map { |g| g[:name] }
+      @summary = info[:overview]
       @cast_members = get_cast(credits) 
       @review_count = reviews.size  
-      @reviews = format_reviews(reviews)
+      @reviews = get_reviews(reviews)
     end
   end
 
   private
 
   def get_cast(credits)
-    credits.dig(:cast)&.take(10)&.map { |c| "#{c[:name]} as #{c[:character]}" } 
+    credits[:cast].first(10).map do |credit|
+       "#{credit[:name]} as #{credits[:character]}"
+    end
   end
 
-  def format_reviews(reviews)
-    reviews.map do |review|
-      {
-        author: review[:author],
-        url: review[:url]
-      }
+  def get_reviews(reviews)
+    reviews.map do |review|    
+      { author: review[:author], url: review[:url] }
     end
   end
 end
--- app/services/movie_db_service.rb
+++ app/services/movie_db_service.rb
@@ -31,7 +31,6 @@ class MovieDbService
   def conn
     Faraday.new(url: 'https://api.themoviedb.org') do |req|
       req.headers['Authorization'] = "Bearer #{ENV['TMDB_V4_TOKEN']}"
-      req.headers['Content-Type'] = 'application/json;charset=utf-8'
     end
   end
 end
