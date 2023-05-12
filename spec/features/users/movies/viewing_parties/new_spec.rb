ff6dbf408c79facfd199a46e104bcc6534f2a9e5
Grace Joh <graceehjoh@gmail.com>
2023-05-12 17:15:59 -0500

    Test: new Viewing Party page

---
+++ spec/features/users/movies/viewing_parties/new_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+RSpec.describe 'New Viewing Party Page', type: :feature do
+  before(:each) do
+    User.delete_all
+
+    @user1 = create(:user)
+    @user2 = create(:user)
+    @user3 = create(:user)
+
+    @movie = MovieFacade.new(550).find_movie
+
+    visit new_user_movie_viewing_party_path(@user1, @movie.id)
+  end
+
+  describe 'new viewing party form', :vcr do
+    it 'displays a subheader' do
+      expect(page).to have_content("Create a Movie Party for #{@movie.title}")
+    end
+
+    it 'displays a Discover Page button' do
+      expect(page).to have_button('Discover Page')
+
+      click_button 'Discover Page'
+
+      expect(current_path).to eq(user_discover_index_path(@user1))
+    end
+
+    it 'displays a form to create a new viewing party' do
+      expect(page).to have_content('Viewing Party Details')
+      expect(page).to have_content("Movie Title: #{@movie.title}")
+
+      expect(page).to have_content('Duration of party')
+      expect(page).to have_field(:duration)
+
+      expect(page).to have_content('Day')
+      expect(page).to have_field(:date)
+
+      expect(page).to have_content('Start time')
+      expect(page).to have_field(:start_time)
+
+      expect(page).to have_content('Invite Other Users')
+      expect(page).to have_unchecked_field("user_#{@user2.id}")
+      expect(page).to have_content(@user2.user_name)
+      expect(page).to have_unchecked_field("user_#{@user3.id}")
+      expect(page).to have_content(@user3.user_name)
+
+      expect(page).to have_button('Create Party')
+    end
+
+    it 'creates Partygoers when the form is submitted' do
+      fill_in :duration, with: 120
+      fill_in :date, with: '2023-11-11'
+      fill_in :start_time, with: '12:00'
+      check @user2.user_name
+      click_button 'Create Party'
+
+      expect(current_path).to eq(user_path(@user1))
+      expect(page).to have_content('Your party has been created!')
+      expect(page).to have_content(@movie.title)
+      expect(page).to have_content('November 11, 2023')
+      expect(page).to have_content('12:00 PM')
+      expect(page).to have_content(@user1.user_name)
+      expect(page).to have_content(@user2.user_name)
+
+      visit user_path(@user2)
+
+      expect(page).to have_content(@movie.title)
+      expect(page).to have_content('November 11, 2023')
+      expect(page).to have_content('12:00 PM')
+      expect(page).to have_content(@user1.user_name)
+      expect(page).to have_content(@user2.user_name)
+    end
+  end
+end
