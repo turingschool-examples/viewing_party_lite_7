c3ca84069b21aa8bcf8aa09165b89c7063a47224
Andrew Bowman <andrewtbowman@gmail.com>
2023-05-11 23:54:16 -0400

    add parties and partygoings tables

---
+++ db/migrate/20230511233529_create_partygoings.rb
@@ -0,0 +1,10 @@
+class CreatePartygoings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :partygoings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :party, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
