3e5ff64af056b31ceee48ee9f7ef68abe3c1311c
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-05-12 10:04:35 -0600

    add movie_id to parties

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,11 +2,10 @@ class PartiesController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @movie = MovieFacade.new.find_movie(params[:movie_id])
-    # require 'pry'; binding.pry
     @users = User.all
   end
 
   def create
-
+    require 'pry'; binding.pry
   end
 end
