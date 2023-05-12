ad8989f85a4ba5a965bc671622655c96699f3020
Katie Lonsdale <katie952@gmail.com>
2023-05-11 18:40:57 -0600

    fix: review content test

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,23 +1,5 @@
 require 'rails_helper'
 
-# When I visit a movie's detail page (/users/:user_id/movies/:movie_id where :id is a valid user id,
-# I should see
-
-#  Button to create a viewing party
-#  Button to return to the Discover Page
-# Details This viewing party button should take the user to the new viewing party page (/users/:user_id/movies/:movie_id/viewing-party/new)
-
-# And I should see the following information about the movie:
-
-#  Movie Title
-#  Vote Average of the movie
-#  Runtime in hours & minutes
-#  Genre(s) associated to movie
-#  Summary description
-#  List the first 10 cast members (characters&actress/actors)
-#  Count of total reviews
-#  Each review's author and information
-
 RSpec.describe "Movie Detail page" do
   before(:each) do
     test_data
@@ -81,7 +63,7 @@ RSpec.describe "Movie Detail page" do
       expect(page).to have_content("#{reviews.count} Reviews")
         reviews.each do |review|
             expect(page).to have_content(review.author)
-            # expect(page).to have_content(review.content)
+            expect(page).to have_content("Pretty awesome movie. It shows what one crazy person can convince other crazy people to do. Everyone needs something to believe in. I recommend Jesus Christ, but they want Tyler Durden.")
         end
       end
     end
