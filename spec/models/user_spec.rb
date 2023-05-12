5b84b8a2682a16c8537b118cfb765c8a0bf8f438
Boston Lowrey <bostonlowrey@gmail.com>
2023-05-12 13:55:56 -0400

    refactor: tests in model user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before(:each) do
+    @user_1 = User.create!(name: "Bob", email: "bobert@bob.com")
+    @user_2 = User.create!(name: "Jeff", email: "jefferey@bob.com")
+    @user_3 = User.create!(name: "Sally", email: "sally@bob.com")
+
+    @viewing_party_1 = ViewingParty.create!(duration: 1, date: Date.current, time: Time.current, movie_id: 5)
+
+    ViewingPartyUser.create!(user_id: @user_1.id, viewing_party_id: @viewing_party_1.id, host: true)
+    ViewingPartyUser.create!(user_id: @user_2.id, viewing_party_id: @viewing_party_1.id, host: false)
+    ViewingPartyUser.create!(user_id: @user_3.id, viewing_party_id: @viewing_party_1.id, host: false)
+  end
   describe 'relationships' do
     it { should have_many(:viewing_party_users) }
     it { should have_many(:viewing_parties).through(:viewing_party_users) }
@@ -14,27 +25,16 @@ RSpec.describe User, type: :model do
 
   describe 'instance methods' do
     it '#other_users' do
-      @user_1 = User.create!(name: "Bob", email: "bobert@bob.com")
-      @user_2 = User.create!(name: "Jeff", email: "jefferey@bob.com")
-      @user_3 = User.create!(name: "Sally", email: "sally@bob.com")
-      
       expect(@user_1.other_users).to eq([@user_2, @user_3])
     end
 
-    it '#hosted_parties & #invited_parties' do
-      @user_1 = User.create!(name: "Bob", email: "bob@bobbington.com")
-      @user_2 = User.create!(name: "Jeff", email: "jeffrey@jeff.com")
-      @user_3 = User.create!(name: "Sally", email: "salamander@bob.com")
-
-      @viewing_party_1 = ViewingParty.create!(duration: 1, date: Date.current, time: Time.current, movie_id: 5)
-      ViewingPartyUser.create!(user_id: @user_1.id, viewing_party_id: @viewing_party_1.id, host: true)
-      ViewingPartyUser.create!(user_id: @user_2.id, viewing_party_id: @viewing_party_1.id, host: false)
-      ViewingPartyUser.create!(user_id: @user_3.id, viewing_party_id: @viewing_party_1.id, host: false)
-
+    it '#hosted_parties' do
       expect(@user_1.hosted_parties).to eq([@viewing_party_1])
       expect(@user_2.hosted_parties).to eq([])
       expect(@user_3.hosted_parties).to eq([])
+    end
 
+    it '#invited_parties' do
       expect(@user_1.invited_parties).to eq([])
       expect(@user_2.invited_parties).to eq([@viewing_party_1])
       expect(@user_3.invited_parties).to eq([@viewing_party_1])
