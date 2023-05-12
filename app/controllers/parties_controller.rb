fd61987b8bb345d21d42d09ad4176079cb89f535
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-05-11 21:00:41 -0600

    partially created view for new viewing party

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,6 +1,12 @@
 class PartiesController < ApplicationController
   def new
     @user = User.find(params[:user_id])
-    @movie = User.find(params[:movie_id])
+    @movie = MovieFacade.new.find_movie(params[:movie_id])
+    # require 'pry'; binding.pry
+    @users = User.all
+  end
+
+  def create
+
   end
 end
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -8,19 +8,23 @@ RSpec.describe "New Viewing party page" do
       @movie_1 = Movie.new(@data)
     end
 
-    it "has a form with all necessary fields" do
+    it "has a form with all necessary fields", :vcr do
       visit new_user_movie_party_path(@user_1, @movie_1.id)
+      save_and_open_page
 
       expect(page).to have_content("Viewing Party Details")
       expect(page).to have_content(@movie_1.title)
-      expect(page).to have_field("Duration of Party", with: 139)
-      expect(page).to have_field("Day")
-      expect(page).to have_field("Start Time")
-      expect(page).to have_selector("input##{user_1.id}[type='radio']")
-      expect(page).to have_selector("input##{user_2.id}[type='radio']")
-      expect(page).to have_selector("input##{user_3.id}[type='radio']")
-      expect(page).to have_selector("input##{user_4.id}[type='radio']")
-      expect(page).to have_selector("input##{user_5.id}[type='radio']")
+      expect(page).to have_field("duration")
+      expect(page).to have_field("[day(1i)]")
+      expect(page).to have_field("[day(2i)]")
+      expect(page).to have_field("[day(3i)]")
+      expect(page).to have_field("[time(4i)]")
+      expect(page).to have_field("[time(5i)]")
+      expect(page).to have_selector("input##{@user_1.id}[type='radio']")
+      expect(page).to have_selector("input##{@user_2.id}[type='radio']")
+      expect(page).to have_selector("input##{@user_3.id}[type='radio']")
+      expect(page).to have_selector("input##{@user_4.id}[type='radio']")
+      expect(page).to have_selector("input##{@user_5.id}[type='radio']")
       expect(page).to have_button("Create Party")
     end
   end
@@ -32,7 +36,9 @@ RSpec.describe "New Viewing party page" do
       @movie_1 = Movie.new(@data)
     end
 
-    it "creates viewing party" do
+    xit "creates viewing party", :vcr do
+      visit new_user_movie_party_path(@user_1, @movie_1.id)
+
       expect(@user_1.parties.count).to eq(0)
       expect(@user_2.parties.count).to eq(1)
       expect(@user_4.parties.count).to eq(0)
