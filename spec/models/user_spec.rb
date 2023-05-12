b9ae9eb5159985d3febd998fc87487ddff048437
Grace Joh <graceehjoh@gmail.com>
2023-05-12 17:15:10 -0500

    Test: User invitees

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
   end
+
+  describe 'instance methods' do
+    describe '#invitees' do
+      it 'returns all users except self' do
+        User.delete_all
+
+        @user1 = create(:user)
+        @user2 = create(:user)
+        @user3 = create(:user)
+
+        expect(@user1.invitees).to eq([@user2, @user3])
+      end
+    end
+  end
 end
