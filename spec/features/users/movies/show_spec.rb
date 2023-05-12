c57ffa020942a0de646255562c4c20557f0f21bc
Thomas Hawley <thawley22@turing.edu>
2023-05-11 23:19:32 -0600

    fix: Update cassettes to pass tests when fixture file is deleted
    Co-authored-by: Caroline Peri <77258069+cariperi@users.noreply.github.com>

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -25,48 +25,52 @@ RSpec.describe '/users/:id/movies/:id' do
   end
 
   describe 'When I visit a movies details page' do
-    VCR.use_cassette('test_individual_movie', :allow_playback_repeats => true) do
     it 'should have a button that links to a page to create a new viewing party' do
-      expect(page).to have_button('Create Viewing Party')
-
-      click_button 'Create Viewing Party'
-
-      expect(current_path).to eq(new_user_movie_viewing_party_path(@user1, @movie.id))
-    end
-
-    it 'should have a button that links to the users discover page' do
-      expect(page).to have_button('Discover Page')
-
-      click_button 'Discover Page'
-
-      expect(current_path).to eq(user_discover_path(@user1))
-    end
-
-    it 'should have information about the specific movie' do
-      expect(page).to have_content(@selected_movie.title)
-      expect(page).to have_content("Vote Average: #{@selected_movie.vote_average}")
-      expect(page).to have_content("Runtime: 2h 55m")
-
-      @selected_movie.genres.each do |genre|
-        expect(page).to have_content(genre)
-      end
-
-      expect(page).to have_content(@selected_movie.summary)
-
-      expect(page).to have_css('.cast', maximum: 10)
-
-      @cast.each do |cast_member|
-        expect(page).to have_content(cast_member.character)
-        expect(page).to have_content(cast_member.name)
-      end
-
-      expect(page).to have_content("#{@reviews.count} Reviews")
-
-      @reviews.each do |review|
-        expect(page).to have_content(review.author)
-        # expect(page).to have_content(review.content)
+      VCR.use_cassette('movie_details', :allow_playback_repeats => true) do
+        expect(page).to have_button('Create Viewing Party')
+        
+        click_button 'Create Viewing Party'
+        
+        expect(current_path).to eq(new_user_movie_viewing_party_path(@user1, @movie.id))
       end
     end
-    end
+
+      it 'should have a button that links to the users discover page' do
+        VCR.use_cassette('movie_details', :allow_playback_repeats => true) do
+          expect(page).to have_button('Discover Page')
+          
+          click_button 'Discover Page'
+          
+          expect(current_path).to eq(user_discover_path(@user1))
+        end
+      end
+
+      it 'should have information about the specific movie' do
+        VCR.use_cassette('movie_details', :allow_playback_repeats => true) do
+          expect(page).to have_content(@selected_movie.title)
+          expect(page).to have_content("Vote Average: #{@selected_movie.vote_average}")
+          expect(page).to have_content("Runtime: 2h 55m")
+          
+          @selected_movie.genres.each do |genre|
+            expect(page).to have_content(genre)
+          end
+          
+          expect(page).to have_content(@selected_movie.summary)
+          
+          expect(page).to have_css('.cast', maximum: 10)
+          
+          @cast.each do |cast_member|
+            expect(page).to have_content(cast_member.character)
+            expect(page).to have_content(cast_member.name)
+          end
+          
+          expect(page).to have_content("#{@reviews.count} Reviews")
+          
+          @reviews.each do |review|
+            expect(page).to have_content(review.author)
+            # expect(page).to have_content(review.content)
+          end
+        end
+      end
   end
 end
