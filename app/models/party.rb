99c6c9f92862e3bab1831eee41dc7dd39dfa8085
Katie Lonsdale <katie952@gmail.com>
2023-05-12 10:49:53 -0600

    feat: add guests methodd

--- app/models/party.rb
+++ app/models/party.rb
@@ -4,7 +4,10 @@ class Party < ApplicationRecord
   validates_presence_of :date
   validates_presence_of :time
   
-
   has_many :user_parties
   has_many :users, through: :user_parties
+
+  def guests
+    users.joins(:user_parties).where("user_parties.is_host=false").distinct
+  end
 end
