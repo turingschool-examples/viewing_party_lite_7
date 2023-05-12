ae68253a6c1a17b9945ff7d8e4bd31f3e956560e
Katie Lonsdale <katie952@gmail.com>
2023-05-12 15:55:33 -0600

    fix: prevent bug if no participants were chosen

--- app/controllers/user_parties_controller.rb
+++ app/controllers/user_parties_controller.rb
@@ -2,8 +2,10 @@ class UserPartiesController < ApplicationController
   def create
     @party = Party.find(params[:party])
     @party.user_parties.create(user_id: params[:party_host], is_host: true)
-    params[:participants].each do |participant|
+    if params[:participants].count >= 1
+      params[:participants].each do |participant|
       @party.user_parties.create(user_id: participant)
+      end
     end
 
     redirect_to "/users/#{params[:party_host]}"
