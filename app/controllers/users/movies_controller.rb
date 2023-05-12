c0aa7b41aa050358dce70376c8c9ca47134b29c5
Grace Joh <graceehjoh@gmail.com>
2023-05-11 20:29:12 -0500

    refactor: User Movies controller index action

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,14 +1,18 @@
 class Users::MoviesController < ApplicationController
+  before_action :find_user, only: [:index]
+
   def index
     if params[:q].empty?
       flash[:alert] = 'Please enter a movie'
       redirect_to user_discover_index_path(params[:user_id])
-    elsif params[:q] == 'top%20rated'
-      @user = User.find(params[:user_id])
-      @movies = MovieFacade.new.top_rated
     else
-      @user = User.find(params[:user_id])
-      @movies = MovieFacade.new.search(params[:q])
+      @movies = MovieFacade.new.get_movies(params[:q])
     end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
   end
 end
