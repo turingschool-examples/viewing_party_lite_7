22bebb896d7158d1a1ce8c4603cd7d14566e806e
Angel Byun <abyun22@turing.edu>
2023-05-22 17:20:09 -0600

    Create validations for password_digest and password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   validates_presence_of :name,
-                        :email
+                        :email,
+                        :password_digest
+  validates_presence_of :password, confirmation: {case_sensitive: true}
   validates_uniqueness_of :email
 
   has_many :party_users
   has_many :parties, through: :party_users
 
-  def name_and_email
-    "#{name} (#{email})"
-  end
+  has_secure_password
+
+  # def name_and_email_
+  #   "#{name} (#{email})"
+  # end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password_digest }
+    it { should have_secure_password}
+
+    it 'validates the validations' do
+      user = User.create(name: "angelbyun", email: "abyun22@turing.edu", password: "12345", password_confirmation: "12345")
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq("12345")
+    end
   end
 
   describe 'relationships' do
@@ -14,7 +22,7 @@ RSpec.describe User, type: :model do
 
   describe 'instance methods' do
     describe '#name_and_email' do
-      it 'returns a string with the user name and email' do
+      xit 'returns a string with the user name and email' do
         user = User.create!(name: 'Billy Bob Thornton', email: 'billybob@turing.edu')
         expect(user.name_and_email).to eq('Billy Bob Thornton (billybob@turing.edu)')
       end
