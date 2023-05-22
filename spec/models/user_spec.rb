75e89115f2c6195ff63e49b2907cd67ebebf6807
Grace Joh <graceehjoh@gmail.com>
2023-05-22 12:48:43 -0500

    Fix: User user_name and password test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,12 +11,13 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password_digest) }
-    it { should have_secure_password }
   end
 
-  describe 'secure password' do
+  describe 'has_secure_password' do
+    it { should have_secure_password }
+
     it 'should not store the password' do
-      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      user = User.create(user_name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
 
       expect(user).to_not have_attribute(:password)
       expect(user.password_digest).to_not eq('password123')
