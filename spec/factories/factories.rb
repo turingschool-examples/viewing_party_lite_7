146f49ccaf55ea46304e51d7d1e0f5dc96b7a7f0
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-22 17:06:59 -0600

    update factory bot for tests

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    password_confirmation { password }
   end
 
   factory :viewing_party do
