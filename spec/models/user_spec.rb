c34df6b5cae957b2c8ed6470c25366cd29236fd4
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-22 17:06:23 -0600

    add validations and test them

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,19 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password }
+    it 'should validate password security' do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
+    it 'should validate password confirmation' do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'notpassword123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+      expect(user.save).to eq(false)
+    end
   end
 
   describe 'instance methods' do
