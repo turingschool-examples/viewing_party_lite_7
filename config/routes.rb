3d8eb30aa3e6fe09ada4b94bb785e99bd06ff77d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-23 16:02:25 -0400

    refactor: update routes to use sessions REST actions for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'welcome#index'
 
   get 'register', to: 'users#new', as: :new_user
-  get 'login', to: 'users#login_form', as: :login_form
-  post 'login', to: 'users#login_user', as: :login_user
 
-  get 'logout', to: 'users#logout', as: :logout
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :users, only: [:create, :show] do
     get 'discover', to: 'users/discover#index'
