6fe7e1773e2898f6b9a74348f99452c607f82a1a
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-23 14:55:26 -0600

    add new route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   get '/login', to: 'users#login_form'
   post '/login', to: 'users#login_user'
+  delete '/logout', to: 'users#logout_session'
   resources :users, only: %i[create]
   resources :users, only: [:show], as: 'user_dashboard' do
     resources :discover, only: [:index]
