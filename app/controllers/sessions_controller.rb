877eb7feb455e333e5d2ebb12c23df92407c6d9c
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-23 16:02:07 -0400

    refactor: move logic for login from users controller to sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.email}!"
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Sorry, your credentials are not valid."
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
