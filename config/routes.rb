cf4e8a7505bd3425b351a3022d76e41181dd04e9
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-24 17:42:31 -0400

    feat: update routes to change user/:id to /dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'register', to: 'users#new', as: :new_user
+  get 'dashboard', to: 'users#show', as: :user
 
   resources :sessions, only: [:new, :create, :destroy]
 
-  resources :users, only: [:create, :show] do
+  resources :users, only: [:create] do
     get 'discover', to: 'users/discover#index'
     resources :movies, only: [:index, :show], controller: 'users/movies' do
       resources :viewing_party, only: [:new, :create], controller: 'users/movies/viewing_parties'
