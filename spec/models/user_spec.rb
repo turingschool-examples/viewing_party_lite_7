4b328c2fbd7cec84aee73a78cb5022d234692f70
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-04 14:09:02 -0600

    remove data from user_spec file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,4 @@ RSpec.describe User, type: :model do
   describe 'validations' do 
     it {should validate_uniqueness_of :email }
   end
-
-  before(:each) do 
-    user = create(:user)
-    viewing_party = create(:viewing_party)
-    viewing_party_user = create(:viewing_party_user, user: user, viewing_party: viewing_party)
-    require 'pry'; binding.pry
-  end
 end
