1ca439ac0fe8926c2d7f06c69fc402ac9a4a6053
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-04 16:04:33 -0600

    add migrations for users, viewing party and join table configure rubocop gem create models for user, viewing party, and user viewing parties

---
+++ db/migrate/20230704204600_create_viewing_parties.rb
@@ -0,0 +1,11 @@
+class CreateViewingParties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :viewing_parties do |t|
+      t.date :day
+      t.time :start_time
+      t.integer :movie_id
+
+      t.timestamps
+    end
+  end
+end
