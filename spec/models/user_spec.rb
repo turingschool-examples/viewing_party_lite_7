8e6f4e576362d70c87a9852e06ae73ec904c003d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-04 14:05:54 -0600

    set up factory bot and faker

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,15 @@ RSpec.describe User, type: :model do
     it { should have_many(:viewing_party_users) }
     it { should have_many(:viewing_parties).through(:viewing_party_users) }
   end
-
+  
   describe 'validations' do 
     it {should validate_uniqueness_of :email }
+  end
+
+  before(:each) do 
+    user = create(:user)
+    viewing_party = create(:viewing_party)
+    viewing_party_user = create(:viewing_party_user, user: user, viewing_party: viewing_party)
+    require 'pry'; binding.pry
   end
 end
