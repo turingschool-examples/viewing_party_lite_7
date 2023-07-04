4f5833493a554b12e60cfdc242c77a008800a79f
Amy Spears <amspears007@gmail.com>
2023-07-04 17:08:17 -0600

    create home spec and tests

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "Home Page" do
+  describe "Home page content" do
+   before(:each) do
+    @user1 = User.create!(name: "KD", email: "kd@gmail.com")
+    @user2 = User.create!(name: "Amy", email: "amers@gmail.com")
+    @user3 = User.create!(name: "Jess", email: "jess@gmail.com")
+    visit "/"
+  end
+
+    it "I see the Title of the Application and a Button to Create New User" do
+      expect(page).to have_content("Viewing Party Lite 7")
+    end
+
+    it "I see a List of Existing Users which links to the users dashboard"
+    
+    it "I see Link to go back to the landing page (this link will be present at the top of all pages)" 
+  end
+end
