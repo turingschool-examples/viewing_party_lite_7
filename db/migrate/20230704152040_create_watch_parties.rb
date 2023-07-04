de7b0d0f7e0f981b07ceafeef097f12114c57f01
Jesse Sorman <jessesorman@gmail.com>
2023-07-04 11:32:22 -0400

    add schema tables

---
+++ db/migrate/20230704152040_create_watch_parties.rb
@@ -0,0 +1,11 @@
+class CreateWatchParties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :watch_parties do |t|
+      t.string :date
+      t.integer :duration
+      t.string :start_time
+
+      t.timestamps
+    end
+  end
+end
