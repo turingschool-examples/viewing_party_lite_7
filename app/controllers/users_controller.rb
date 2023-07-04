273ae116139583c6af864aaa2dd042433ea12457
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-04 15:06:35 -0600

    initialize users controller add new and create methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def create
+    # User.create!(user_params)
+    # redirect_to(user_path)
+  end
+
+  def new; end
+  # private
+
+  # def user_params
+  #   params.permit(:name, :email, :password)
+  # end
+end
