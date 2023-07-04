4ad697b0c4591d631ab0b996c908b05ddc5f0e76
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-04 14:41:29 -0600

    test user registration

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper' 
+
+RSpec.describe '/register', type: :feature do 
+  describe 'new user creation'
+    before(:each) do 
+      visit "/register"
+    end
+
+    it 'has a form to register' do 
+      expect(page).to have_field(:name)
+      expect(page).to have_field(:email)
+      expect(page).to have_button('Register')
+    end
+
+    it 'form can be filled out' do 
+      fill_in(:name, with: 'John Jacob Jingleheimer Schmidt')
+      fill_in(:email, with: 'jjjs@gmail.com')
+      click_button 'Register' 
+      expect(current_path).to eq(user_path)
+    end
+    
+    it 'if email validation fails, try again' do 
+      user_1 = create(:user)
+      fill_in(:name, with: 'John Jacob Jingleheimer Schmidt')
+      fill_in(:email, with: user_1.email)
+      expect(page).to have_content('Email already exists. Choose another.')
+    end
+end
+
+# When a user visits the '/register' path they should see a form to register.
+
+# The form should include:
+
+#  Name
+#  Email (must be unique)
+#  Register Button
+# Once the user registers they should be taken to a dashboard page '/users/:id', where :id is the id for the user that was just created.
