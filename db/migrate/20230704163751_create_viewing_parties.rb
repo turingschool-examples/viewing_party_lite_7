e7d51d75f8e863b1c24d6262292b27adc36cb710
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-04 10:59:09 -0600

    create database.
    Co-authored-by: Kailey
    Co-authored-by: Nick

---
+++ db/migrate/20230704163751_create_viewing_parties.rb
@@ -0,0 +1,13 @@
+class CreateViewingParties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :viewing_parties do |t|
+      t.datetime :date
+      t.datetime :time
+      t.integer :host_id
+      t.integer :duration
+      t.integer :movie_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230704163905_create_viewing_party_users.rb
@@ -0,0 +1,10 @@
+class CreateViewingPartyUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :viewing_party_users do |t|
+      t.references :users, null: false, foreign_key: true
+      t.references :viewing_party, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
