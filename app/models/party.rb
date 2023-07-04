f6fa6566f331f3509f48500891354eb3f54dffc3
Steven Stackhouse <stackmm@gmail.com>
2023-07-04 13:54:33 -0600

    add party validations and tests

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,10 @@
 class Party < ApplicationRecord
   has_many :party_users
   has_many :users, through: :party_users
+
+  validates_presence_of :date,
+                        :start_time,
+                        :duration,
+                        :movie_id, 
+                        :host_id                 
 end
