134300830039f5f99f7f5b755acd93787d5dff5c
Amy Spears <amspears007@gmail.com>
2023-07-04 17:31:56 -0600

    Add test for button and button_to on view page with method action

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -11,9 +11,13 @@ RSpec.describe "Home Page" do
 
     it "I see the Title of the Application and a Button to Create New User" do
       expect(page).to have_content("Viewing Party Lite 7")
+      click_button("Create New User")
+      expect(current_path).to eq(new_user_path)
     end
 
-    it "I see a List of Existing Users which links to the users dashboard"
+    it "I see a List of Existing Users which links to the users dashboard" do
+        
+    end
     
     it "I see Link to go back to the landing page (this link will be present at the top of all pages)" 
   end
