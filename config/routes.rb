5e12ebecdbee987be8e955f3bc06c282d56a2ea9
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-04 15:05:41 -0600

    correct route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/register", to: 'users#create', as: 'new_user'
+  get "/register", to: 'users#new', as: 'new_user'
   resources :users, only: [:create] 
   get "/dashboard", to: 'users#create', as: 'user'
 end
