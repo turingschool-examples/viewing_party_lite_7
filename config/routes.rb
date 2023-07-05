692c611c9d09fe74e92d826e1bf16f82da85b7f6
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-05 14:49:10 -0600

    ad routes for register#index & users# show,create.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "welcome#index"
+
+  resources :register, only: [:index]
+  resources :users, only: [:show, :create]
+
 end
