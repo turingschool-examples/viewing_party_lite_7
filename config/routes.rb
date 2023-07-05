95d271d32b403dce79510af88120c7976b8576d5
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-07-05 14:24:45 -0600

    Feat: Landing page factory setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
   resources :viewing_users
   resources :viewing_parties
   resources :movies
->>>>>>> 0e64f93bd232c0f3dacd51cd1bfbf79707b942ff
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "/", to: "articles#index"
 end
