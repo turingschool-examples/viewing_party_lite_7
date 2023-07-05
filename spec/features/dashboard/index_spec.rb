c1b06639fb4cdd4bd7960f67de4b5f50f74212b6
Steven Stackhouse <stackmm@gmail.com>
2023-07-04 22:30:52 -0600

    add index method

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -15,15 +15,15 @@ RSpec.describe 'Landing Page' do
 
   end
 
-  it 'displays a button to create a new user' do
+  xit 'displays a button to create a new user' do
 
   end
 
-  it 'displays a list of existing users which links to the users dashboard' do
+  xit 'displays a list of existing users which links to the users dashboard' do
 
   end
 
-  it 'displays a link to go back to the landing page' do
-    
+  xit 'displays a link to go back to the landing page' do
+
   end
 end
