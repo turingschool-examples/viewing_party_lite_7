c5ce5af34f3ecd5df0cc9975e0163074eb10d1c9
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-05 13:57:59 -0600

    add routes for new register form and creating new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'dashboard#index'
+
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+
+  resources :users, only: [:show]
 end
 
