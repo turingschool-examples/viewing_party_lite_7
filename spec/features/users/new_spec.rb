76631e1684885c3d70fe1206dbd63ddf9e395e38
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-05 15:43:00 -0600

    add user validations

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -15,5 +15,16 @@ RSpec.describe "New User Form", type: :feature do
       expect(page).to have_content("wolfie@gmail.com")
       expect(page).to have_content("User successfully registered.")
     end
+    
+    it "using valid data" do 
+      fill_in "Name", with: "Wolfie"
+      fill_in "Email", with: "wolfie@gmail.com"
+      click_button "Save" 
+      
+      expect(current_path).to eq("/users/#{User.all.last.id}")
+      expect(page).to have_content("Wolfie")
+      expect(page).to have_content("wolfie@gmail.com")
+      expect(page).to have_content("User successfully registered.")
+    end
   end
 end
