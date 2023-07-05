2f5ee9dea70a8f304c69949b845b9ec41fcccaa5
Boston Lowrey <bostonlowrey@gmail.com>
2023-07-05 17:17:09 -0500

    feat: add routes and users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "welcome#index"
+  get "/register", to: "users#new"
+
+  resources :users, only: [:create]
 end
