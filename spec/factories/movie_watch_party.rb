a584c4651b30a9ec515c13005b1d08c8509ac82f
Jesse Thomas <jgthomas3@gmail.com>
2023-07-05 14:01:13 -0600

    Refactor: adjust column names

--- spec/factories/movie_watch_party.rb
+++ spec/factories/movie_watch_party.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :movie_watch_party do
-    watch_parties_id { rand(1..100) }
-    movies_id { rand(1..100) }
-    users_id { rand(1..100) }
+    watch_party_id { rand(1..100) }
+    movie_id { rand(1..100) }
+    user_id { rand(1..100) }
     user_status { rand(0..1) }
   end
 end
