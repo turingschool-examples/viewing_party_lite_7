3d2cafaec218af862c9f1be32f59aeb288cc553a
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-05 14:02:53 -0600

    add test for  new user

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper" 
+
+RSpec.describe "New User Form", type: :feature do 
+  before(:each) do 
+    visit "/register" 
+  end
+  describe "create a new user" do 
+    it "using valid data" do 
+      fill_in "Name", with "Wolfie"
+      fill_in "Email", with "wolfie@gmail.com"
+      click_button "Submit" 
+
+      expect(current_path).to eq("/users/#{User.all.last.id}")
+      expect(page).to have_content("Wolfie")
+      expect(page).to have_content("wolfie@gmail.com")
+    end
+  end
+end
