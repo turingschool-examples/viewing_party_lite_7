8ddb8f759cd1d08b5e2b0decd854d791c0e4404c
Steven Stackhouse <stackmm@gmail.com>
2023-07-04 22:39:32 -0600

    finish tests

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -8,22 +8,38 @@ require 'rails_helper'
 
 RSpec.describe 'Landing Page' do
   before(:each) do
+    @user_1 = User.create!(name: 'John Smith', email: 'jsmith@aol.com')
+    @user_2 = User.create!(name: 'Jane Doe', email: 'jdoe@gmail.com')
+    @user_3 = User.create!(name: 'Michael Jackson', email: 'mjack@gmail.com')
+
     visit root_path
   end
 
   it 'displays the title of the application' do
-
+    within('#title') do
+      expect(page).to have_content('Viewing Party')
+    end
   end
 
   xit 'displays a button to create a new user' do
-
+    within('#create-user') do
+      expect(page).to have_button('Create User')
+    end
   end
 
   xit 'displays a list of existing users which links to the users dashboard' do
-
+    within('#existing-users') do
+      expect(page).to have_content('Existing Users')
+      expect(page).to have_link(@user_1.name)
+      expect(page).to have_link(@user_2.name)
+      expect(page).to have_link(@user_3.name)
+    end
   end
 
   xit 'displays a link to go back to the landing page' do
-
+    expect(page).to have_link('Home')
+    
+    click_link('Home')
+    expect(current_path).to eq(root_path)
   end
 end
