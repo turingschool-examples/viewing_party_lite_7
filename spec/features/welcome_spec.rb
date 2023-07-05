234c9617770ce1aafce7b1aa1159909bb8e42b26
Jimmy Jurado <jurado140@gmail.com>
2023-07-05 16:17:43 -0400

    Creates spec test for US1/welcome page. Missing route and controllers.

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe '/', type: :feature do
+  before(:each) do
+    @user1 = User.create!(name: "Sarah", email: "Sarah@gmail.com")
+    @user2 = User.create!(name: "Jimmy", email: "Jimmy@gmail.com")
+    @user3 = User.create!(name: "Alex", email: "Alex@gmail.com")
+    @user4 = User.create!(name: "John", email: "John@gmail.com")
+    visit "/"
+  end
+  describe "User visits root path" do
+    it "should display title of application" do
+      expect(page).to have_content("Viewing Party")
+    end
+
+    it "should display button to create a new user" do
+      expect(page).to have_button("Create New User")
+    end
+
+    it "should display existing users with links to the users dashboard" do
+      expect(page).to have_content("#{@user1.name}")
+      expect(page).to have_content("#{@user2.name}")
+      expect(page).to have_content("#{@user3.name}")
+      expect(page).to have_content("#{@user4.name}")
+    end
+
+    it "should have a nav home page link" do
+      expect(page).to have_link("Home")
+    end
+  end
+end
