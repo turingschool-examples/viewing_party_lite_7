4f662c38d8415793f91b6095ae50e409cf1768c7
Jesse Thomas <jgthomas3@gmail.com>
2023-07-05 14:01:46 -0600

    Refactor: Adjust associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  belongs_to :movie_watch_party, optional: true
+  has_many :movie_watch_parties
+
+  has_many :movies, through: :movie_watch_parties
+  has_many :watch_parties, through: :movie_watch_parties
 
   validates :name, presence: true
   validates :email, uniqueness: true
