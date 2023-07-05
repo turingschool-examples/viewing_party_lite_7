a962f041d8ea38c1ffdb0b454f6984368f166c1d
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-05 14:14:59 -0600

    add create conditional action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new
-    if save
+  def new; end
+
+  def create
+    @user = User.find(params[:id])
+    if @user.save
       redirect_to user_path
+    else
+      redirect_to new_user_path
+      flash[:alert] = "Error: #{error_message(merchant.errors)}"
     end
   end
-
-  def create; end
 end
