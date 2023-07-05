52747b1126debe9b743e1e9da2d9efb0dec726e7
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-05 13:56:14 -0600

    add new, create, show actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "/users/#{@user.id}"
+    else
+      redirect_to '/register'
+    
+    end
+  end
+  
+  def show
+    
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
+
+end
