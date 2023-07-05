5c3c153a3c206a966a98e3b37cf15aad113eed29
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-05 14:53:34 -0600

    Feat: create users controller along with #show and #create actions.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def create 
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user.id)
+    else
+      flash[:error] = "'Name' and 'Email' fields are required, please try again."
+      redirect_to (register_index_path)
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
+end
