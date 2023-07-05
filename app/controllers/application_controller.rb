d32859fbce7ceced00918eedfa5e69d1d4538767
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-07-05 17:24:05 -0600

    Feat: Application landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def landing
+    @users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :viewing_users
   resources :viewing_parties
   resources :movies
-  resources :users
+  resources :users, only: [:show] 
+  #   resources :dashboard, only: [:index]
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "/", to: "articles#index"
+  get "/", to: "application#landing", controller: [:user]
 end
