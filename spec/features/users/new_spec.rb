504b34b3e8b838949a311998b4fc1ae3afbd9c85
Boston Lowrey <bostonlowrey@gmail.com>
2023-07-05 17:18:22 -0500

    test: for home page link

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe "user registration page", type: :feature do
     @user3 = User.create!(name: "boston", email: "boston@example.com")
     visit "/register"
   end
-  
+
   describe "when I visit the user registration page" do
+    it 'I see a link to return to the landing page' do
+      expect(page).to have_link("Home")
+      click_link "Home"
+      expect(current_path).to eq("/")
+    end
+
     it "I see a for to register a user" do
       expect(page).to have_content("New User Registration")
       within("#user-registration") do
