e17a0ec45e947b9ac19daf5bfbbcd385c447e237
Lauren Golden <llgolden@gmail.com>
2023-07-06 14:34:22 -0600

    Saving before merge with main

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def index
     @user = User.find(params[:id])
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/users/:id", to: "users#index"
-  post "/users/:id/discover", to: 
+  post "/users/:id/discover", to: "users#create"
 end
