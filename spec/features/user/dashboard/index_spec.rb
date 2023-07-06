45cba181b2bbef12f28910873d1440119af0cead
Javen Wilson <javenb022@gmail.com>
2023-07-06 17:24:17 -0400

    refactor for rubocop
    Co-authored-by
    Logan Wilson <loganwilson777@gmail.com>
    Co-authored-by: Lauren Golden <llgolden@gmail.com>

--- spec/features/user/dashboard/index_spec.rb
+++ spec/features/user/dashboard/index_spec.rb
@@ -4,11 +4,11 @@ describe "User Dashboard Page" do
   before :each do
     @user_1 = User.create!(name: "Simon", email: "simon@gmail.com")
 
-    visit "/users/#{@user1.id}"
+    visit "/users/#{@user_1.id}"
   end
 
   it "should display the dashboard content" do
-    expect(page).to have_content("#{@user1.name}'s Dashboard")
+    expect(page).to have_content("#{@user_1.name}'s Dashboard")
     expect(page).to have_button("Discover Movies")
     expect(page).to have_content("Viewing Parties")
   end
@@ -16,6 +16,6 @@ describe "User Dashboard Page" do
   it "should have a button to the user's discover page" do
     expect(page).to have_button("Discover Movies")
     click_button "Discover Movies"
-    expect(page).to have_current_path("/users/#{@user1.id}/discover")
+    expect(page).to have_current_path("/users/#{@user_1.id}/discover")
   end
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -24,14 +24,14 @@ RSpec.describe "/", type: :feature do
     it "I see a list of existing users which links to the their dashboard" do
       expect(page).to have_content("Existing Users")
 
-      within "#user_#{@user1.id}" do
-        expect(page).to have_content(@user1.email)
+      within "#user_#{@user_1.id}" do
+        expect(page).to have_content(@user_1.email)
       end
-      within "#user_#{@user2.id}" do
-        expect(page).to have_content(@user2.email)
+      within "#user_#{@user_2.id}" do
+        expect(page).to have_content(@user_2.email)
       end
-      within "#user_#{@user3.id}" do
-        expect(page).to have_content(@user3.email)
+      within "#user_#{@user_3.id}" do
+        expect(page).to have_content(@user_3.email)
       end
     end
 
