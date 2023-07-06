4d47c1ee22b36d173db9288ce518be1b404a4935
Sarah Garlock <garlock.sarah10@gmail.com>
2023-07-05 19:47:47 -0600

    set up render

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Users.destroy_all
+Parties.destroy_all
+UserParties.destroy_all
+
+
 @user1 = User.create!(name: 'Sarah', email: 'Sarah@gmail.com')
 @user2 = User.create!(name: 'Jimmy', email: 'Jimmy@gmail.com')
 @user3 = User.create!(name: 'Alex', email: 'Alex@gmail.com')
