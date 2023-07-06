cee7c812edf23217432e644313104c3a04c3aae7
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-05 20:05:15 -0400

    add global flash messages to application.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,3 @@
-# app/helpers/application_helper.rb
+# # app/helpers/application_helper.rb
 module ApplicationHelper
-  def flash_message
-    flash.map do |type, message|
-      content_tag :div, message, class: "alert #{flash_class(type)}"
-    end.join.html_safe
-  end
-
-  def flash_class(type)
-    case type.to_sym
-    when :error
-      'alert-error'
-    end
-  end
 end
