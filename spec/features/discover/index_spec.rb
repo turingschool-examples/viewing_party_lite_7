a22ec45aa462a00eb802a74be3b773b8696033ad
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-06 11:52:37 -0600

    Test: Enhance tests for discover index

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -12,11 +12,13 @@ RSpec.describe "user's discover page", type: :feature do
       before(:each) do
         visit user_discover_index_path(@user1.id)
       end
-      
+
       it "displays a link to the home page, 'Viewing Party', and 'Discover Movies' at the top of the page" do
         expect(page).to have_link("Home")
         expect(page).to have_content("Viewing Party")
-        expect(page).to have_content("Discover Movies")
+        within("#title") do
+          expect(page).to have_content("Discover Movies")
+        end
       end
 
       it "displays a button to 'Discover Top Rated Movies'" do
@@ -24,8 +26,10 @@ RSpec.describe "user's discover page", type: :feature do
       end
 
       it "displays a search field to 'Find Movies'" do
-        expect(page).to have_field("Find Movies")
-        expect(page).to have_button("Find Movies")
+        within("#movie-search") do
+          expect(page).to have_field("Movie Title:")
+          expect(page).to have_button("Find Movies")
+        end
       end
     end
   end
