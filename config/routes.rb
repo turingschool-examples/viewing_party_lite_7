3271ff9eec3c24e0241ef1997b681d60651fe4ea
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-06 14:15:50 -0400

    add route controller and view for discover controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get "/register", to: "users#new"
-  resources :users, only: [:show, :create]
+
+  resources :users, only: [:show, :create] do
+    resources :discover, only: [:index]
+  end
 end
