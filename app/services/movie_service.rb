4e14648dfe3e750a2e0c167ad46c2253af591eb4
Steven Stackhouse <stackmm@gmail.com>
2023-07-06 15:37:14 -0600

    add search movies by id and find_cast functionality to movie service

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -19,4 +19,12 @@ class MovieService
   def search_movies(query)
     get_url("/3/search/movie?query=#{query}&include_adult=false&language=en-US&page=1")
   end
+
+  def search_movies_by_id(movie_id)
+    get_url("/3/movie/#{movie_id}?language=en-US")
+  end
+
+  def find_cast(movie_id)
+    get_url("/3/movie/#{movie_id}/credits?language=en-US")
+  end
 end
