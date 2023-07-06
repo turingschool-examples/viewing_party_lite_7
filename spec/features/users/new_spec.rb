aa06c3b96e515650f95835a14e9b8f109ad70857
Jimmy Jurado <jurado140@gmail.com>
2023-07-06 10:38:12 -0400

    Adds user model to form view, updates user controller with private params method, adds flash method to application html file. All tests passing.

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -25,5 +25,22 @@ RSpec.describe "/register", type: :feature do
       created_user = User.last
       expect(current_path).to eq(user_path(created_user))
     end
+
+    it "displays flash error if email is not unique" do
+      fill_in 'Name', with: 'Jonah Hill'
+      fill_in 'Email Address', with: 'Jhill@gmail.com'
+      click_button 'Register'
+
+      created_user = User.last
+      expect(current_path).to eq(user_path(created_user))
+
+      visit register_path
+
+      fill_in 'Name', with: 'James Hill'
+      fill_in 'Email Address', with: 'Jhill@gmail.com'
+      click_button 'Register'
+
+      expect(page).to have_content("Email Address Must Be Unique")
+    end
   end
 end
