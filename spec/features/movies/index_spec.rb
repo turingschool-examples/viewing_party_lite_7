599d46d1fa9128610e11fa4075d49f4f0ccffdaf
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-06 11:50:47 -0600

    add button to discover page from movies results page

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe 'movies results page' do
   end
 
   describe 'visiting movies results page' do 
-    it 'top movies button goes to results page'
+    before(:each) do 
+      visit user_discover_index_path(@user_1)
+    end
+
+    it 'top movies button goes to results page' do 
+      click_on 'Discover Top Rated Movies'
+      expect(current_path).to eq(user_movies_path(@user_1))
+    end
 
     it 'movies results page lists top movies if top movies button clicked'
 
@@ -14,12 +21,20 @@ RSpec.describe 'movies results page' do
   end
 
   describe 'movies results page content' do 
+    before(:each) do 
+      visit user_movies_path(@user_1)
+    end
+
     it 'title of movie is link to movie details page'
 
     it 'vote average of movie is listed below movie title'
 
     it 'only lists 20 results on page' 
 
-    it 'has a button to return to Discover page' 
+    it 'has a button to return to Discover page' do 
+      expect(page).to have_button('Discover Page')
+      click_button 'Discover Page'
+      expect(current_path).to eq(user_discover_index_path(@user_1))
+    end
   end
 end
