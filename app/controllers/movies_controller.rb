614962bb15b920936512f8a0122c51dad4e1783c
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-06 18:32:22 -0400

    add connection to api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,27 @@
 class MoviesController < ApplicationController
   before_action :get_user
-  
+
   def index
+    conn = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
+      faraday.params["api_key"] = ENV["TMDB_API_KEY"]
+    end
+
+    if params[:q] == "top%20rated"
+      response = conn.get("movie/top_rated")
+    else
+      search_word = params[:q]
+      response = conn.get("search/movie?query=#{search_word}")
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @movies = json[:results]
+  end
+
+  def show
   end
 
   private
+
   def get_user
     @user = User.find(params[:user_id])
   end
