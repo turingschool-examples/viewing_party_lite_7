11e1af5c8bd75130c5040db5d3e10b6073fbd2ee
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-06 10:05:37 -0600

    remove user story comment from spec file

--- spec/features/discover/index_spec.rb
+++ spec/features/discover/index_spec.rb
@@ -53,12 +53,3 @@ RSpec.describe 'Discover Movies Page', type: :feature do
     end
   end
 end
-
-# As an user,
-# When I visit the '/users/:id/discover' path, where :id, is the id of a valid user,
-# I should see
-
-#  Button to Discover Top Rated Movies
-#  A text field to enter keyword(s) to search by movie title
-#  A Button to Search by Movie Title
-# Details When the user clicks on the Top Rated Movies OR the search button, they should be taken to the movies results page (more details of this on the Movies Results Page issue.
