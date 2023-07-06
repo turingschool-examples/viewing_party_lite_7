71a44352cfc71166695da6ac288c8e48af01d901
Steven Stackhouse <stackmm@gmail.com>
2023-07-06 13:50:12 -0600

    create movie service with top_rated_movies method

---
+++ app/services/movie_service.rb
@@ -0,0 +1,18 @@
+require 'faraday'
+
+class MovieService
+  def conn 
+    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = ENV["MOVIEDB_API_KEY"]
+    end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def top_rated_movies
+    get_url("/3/movie/top_rated?language=en-US&page=1")
+  end
+end
