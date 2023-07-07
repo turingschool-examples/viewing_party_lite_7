b2a898946096a48633e3c396953664026c9c8218
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-07 16:50:16 -0600

    add expectations for form fields

--- spec/features/users/viewing_party/new_spec.rb
+++ spec/features/users/viewing_party/new_spec.rb
@@ -7,19 +7,8 @@ RSpec.describe "User's Viewing Party page" do
     @user3 = User.create!(name: 'Jimmy johnson', email: 'jj33@aol.com')
     @user4 = User.create!(name: 'Todd Guy', email: 'tg@gmail.com')
     
-    @party1 = Party.create!(date: '2021-07-04', start_time: '17:00:00 UTC' , duration: 120, movie_id: 1, host_id: @user1.id)
-    @party2 = Party.create!(date: '2021-12-09', start_time: '08:00:00 UTC' , duration: 150, movie_id: 2, host_id: @user2.id)
-    
-    @party_users1 = PartyUser.create!(user_id: @user1.id, party_id: @party1.id)
-    @party_users2 = PartyUser.create!(user_id: @user3.id, party_id: @party1.id)
-    @party_users3 = PartyUser.create!(user_id: @user2.id, party_id: @party1.id)
-    @party_users4 = PartyUser.create!(user_id: @user2.id, party_id: @party2.id)
-    @party_users5 = PartyUser.create!(user_id: @user4.id, party_id: @party2.id)
-    
     visit  user_viewing_party_path(@user1, 238)
   end
- 
-  
   # Duration of Party with a default value of movie runtime in minutes; a viewing party should NOT be created if set to a value less than the duration of the movie
   # When: field to select date
   # Start Time: field to select time
@@ -35,15 +24,16 @@ RSpec.describe "User's Viewing Party page" do
   end
 
   it 'has a field for party duration that defaults to movie runtime' do
-    
+    save_and_open_page
+    expect(page).to have_field('duration', with: "2h 55m") ##change to minutes 
   end
 
   it 'has a field for party date' do
-    
+    expect(page).to have_field('date')
   end
   
   it 'has a field for party start time' do
-    
+    expect(page).to have_field('start_time')
   end
   
   xit 'has checkboxes for existing users' do
