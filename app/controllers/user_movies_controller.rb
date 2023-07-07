9ea40c14bc2263612edc37d0e6e96886946586e6
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-06 20:15:52 -0600

    add divcover movies link to movie results page

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,6 +1,6 @@
 class UserMoviesController < ApplicationController
   
   def index
-
+    @user = User.find(params[:user_id])
   end
 end
---
+++ spec/features/user_movies/index_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper" 
+
+RSpec.describe "Movie Results Page" do 
+  before(:each) do 
+    @user1 = User.create!(name: "Wolfie", email: "wolfie@gmail.com")
+  end
+  describe "search results" do 
+    it "displays search results for a keyword/title search" do 
+      visit "/users/#{@user1.id}/discover"
+      fill_in "keyword", with: "Jaws"
+      click_button "Search"
+
+      expect(current_path).to eq("/users/#{@user1.id}/movies")
+      
+      # stub this below
+      # within("#results") do 
+      #   expect(page).to have_content()
+      # end
+    end
+    
+    it "has a button to return to the discover page" do 
+      visit "/users/#{@user1.id}/movies"
+      save_and_open_page
+      click_button "Discover Movies" 
+      expect(current_path).to eq("/users/#{@user1.id}/discover")
+    end
+  end
+end
