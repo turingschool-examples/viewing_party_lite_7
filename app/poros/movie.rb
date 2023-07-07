3a801561927eb742fb9f9a4511d903e70ee45ead
Sarah Garlock <garlock.sarah10@gmail.com>
2023-07-07 13:55:11 -0600

    add movie poros

---
+++ app/poros/movie.rb
@@ -0,0 +1,11 @@
+class Movie 
+  attr_reader :id,
+              :title,
+              :vote_average
+
+  def initialize(movie_data)
+    @id = movie_data[:id]
+    @title = movie_data[:title]
+    @vote_average = movie_data[:vote_average]
+  end
+end
