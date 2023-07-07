0d1ff8214cdad7c331d92336968a471cf624ca47
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-07 13:42:07 -0600

    Feat: #get_url and test

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -7,4 +7,9 @@ class MovieService
       faraday.headers['Authorization'] = ENV['TMDB_AUTH']
     end
   end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -17,7 +17,19 @@ RSpec.describe MovieService do
       expect(response).to be_a Faraday::Connection
     end
 
-    it '#get_url'
+    it '#get_url' do 
+      response = @ms.get_url('movie/top_rated')
+      expect(response).to be_a Hash
+      expect(response[:results]).to be_a Array
+
+      movie_data = response[:results].first
+
+      expect(movie_data).to have_key(:title)
+      expect(movie_data[:title]).to be_a String
+
+      expect(movie_data).to have_key(:vote_average)
+      expect(movie_data[:vote_average]).to be_a Float
+    end
 
     it '#top_movies' 
 
