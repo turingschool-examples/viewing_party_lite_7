005f836cb943b2af9085bdc458d60c599e910cc6
Jeff Nelson <jpnelson85@gmail.com>
2023-07-07 16:21:58 -0500

    finish movie results page user story

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe MovieService do
   describe "search" do
-    it "returns search results by movie title" do
+    it "returns search results by movie title", :vcr do
       movie_service = MovieService.new
 
       princess_movies = movie_service.search("Princess")[:results]
