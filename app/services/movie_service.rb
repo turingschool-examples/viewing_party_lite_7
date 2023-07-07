f512595456a1bf87cd35618d6550fd73a63ae251
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-07 13:33:00 -0600

    feat: movie_service #conn

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,3 +1,10 @@
 class MovieService 
   
+
+
+  def conn 
+    Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
+      faraday.headers['Authorization'] = ENV['TMDB_AUTH']
+    end
+  end
 end
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.describe MovieService do 
   before(:each) do 
-    @mds = MovieService.new
+    @ms = MovieService.new
   end
 
   describe 'initialize' do 
     it 'exists' do 
-      expect(@mds).to be_a MovieService
+      expect(@ms).to be_a MovieService
     end
   end
 
-  describe 'class methods' do 
-    it '#conn' 
+  describe 'class methods', :vcr do 
+    it '#conn' do 
+      response = @ms.conn
+      expect(response).to be_a Faraday::Connection
+    end
 
     it '#get_url'
 
