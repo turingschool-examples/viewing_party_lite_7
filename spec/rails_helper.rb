8040334064e5c141ba30d48b35b7b02c522e6ed6
Sarah Garlock <garlock.sarah10@gmail.com>
2023-07-07 14:26:30 -0600

    add webmock and vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,7 +71,9 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-# VCR.configure do |config|
-#   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-#   config.hook_into :webmock
-# end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<MOVIE_DB_API_KEY>') { ENV['MOVIE_DB_API_KEY']}
+  config.configure_rspec_metadata!
+end
