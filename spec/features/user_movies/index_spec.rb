e7174fea498b1281b8249bf6830204e03e4b9e32
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-06 20:38:57 -0600

    install figaro

--- spec/features/user_movies/index_spec.rb
+++ spec/features/user_movies/index_spec.rb
@@ -20,9 +20,13 @@ RSpec.describe "Movie Results Page" do
     
     it "has a button to return to the discover page" do 
       visit "/users/#{@user1.id}/movies"
-      save_and_open_page
       click_button "Discover Movies" 
       expect(current_path).to eq("/users/#{@user1.id}/discover")
     end
   end
 end
+
+curl --request GET \
+     --url 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc' \
+     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmM2VjOWMzOGI0YzQ2OWE1OGExZDBiMzgzMjBmNGVjOSIsInN1YiI6IjY0YTYxYzI1MDdmYWEyMDBlNDBiNTFkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qqbkVYkBsvGZgSIHWwzsMaIF8W9d9zoGGt6CRlE9WM4' \
+     --header 'accept: application/json'
