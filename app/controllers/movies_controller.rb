a32f430b0770452cda311103d4bcb3cdb1257669
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-07 19:59:37 -0400

    add connection to movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,18 @@ class MoviesController < ApplicationController
     @facade = search_keyword? ? MovieFacade.new(params[:keyword]) : MovieFacade.new
   end
 
-  def show; end
+  def show
+    movie_id = params[:id]
+
+    conn = Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
+      faraday.params["api_key"] = ENV["TMDB_API_KEY"]
+    end
+
+    response = conn.get("movie/#{movie_id}")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @movie = Movie.new(json)
+  end
 
   private
 
