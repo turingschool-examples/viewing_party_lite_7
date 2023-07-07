527cfa1df67cc4d72333ee8163454bfb033a68ef
Boston Lowrey <bostonlowrey@gmail.com>
2023-07-07 14:30:18 -0500

    feat: add routes and update movies controller alsoo updated movies index page and discover index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :show] do
     resources :discover, only: [:index]
-    resources :movies, only: [:index]
+    resources :movies, only: [:index, :show]
   end
 end
