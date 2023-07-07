283b9e38d8b19cec52f72692d5437caa11ced537
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-07 17:16:02 -0600

    add users for view

--- app/controllers/users/viewing_party_controller.rb
+++ app/controllers/users/viewing_party_controller.rb
@@ -2,7 +2,10 @@ class Users::ViewingPartyController < ApplicationController
   def new
     @movie = MovieFacade.new(params).search
     @user = User.find(params[:user_id])
+    @users = User.where.not(id: @user.id)
   end
+
+  
 
   
 end
