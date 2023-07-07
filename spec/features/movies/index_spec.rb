d5ecf018ca81c775ec0c094163252a86e5340b30
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-06 19:43:07 -0600

    add discover movies page tests

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,13 +1,23 @@
 require "rails_helper" 
 
 RSpec.describe "Discover Movies Page", type: :feature do 
+  before(:each) do 
+    @user1 = User.create!(name: "Wolfie", email: "wolfie@gmail.com")
+    visit "/users/#{@user1.id}/discover"
+  end
+  
   describe "discovery links that route to users/:user_id/movies" do 
     it "button to discover top rated movies" do 
+      click_button "Discover Top Rated Movies"
 
+      expect(current_path).to eq("/users/#{@user1.id}/movies")
     end
 
     it "a text field to search by title with search button" do 
+      fill_in "Search by Title", with: "Jaws"
+      click_button "Search"
 
+      expect(current_path).to eq("/users/#{@user1.id}/movies")
     end
   end
 end
