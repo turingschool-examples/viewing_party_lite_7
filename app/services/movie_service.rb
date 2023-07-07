8b398baac9c625da02e915e44c5e4d3829026315
Boston Lowrey <bostonlowrey@gmail.com>
2023-07-07 14:30:56 -0500

    feat: add requests to movie_service

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,12 +1,23 @@
 class MovieService
+
+  private
+
   def self.conn
     Faraday.new(url: "https://api.themoviedb.org/3/") do |faraday|
       faraday.params["api_key"] = ENV["MOVIE_API_KEY"]
     end
   end
 
-  def get_url(url)
+  def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.top_rated_movies
+    get_url("https://api.themoviedb.org/3/movie/top_rated")
+  end
+
+  def self.movie_search(query)
+    get_url("https://api.themoviedb.org/3/search/movie?query=#{query}")
   end
 end
