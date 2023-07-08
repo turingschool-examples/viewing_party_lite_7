4bbce12c7eef17cc5c27f78378dcbba83ac862a4
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-08 12:55:26 -0600

    add specs for review and credit poros.

---
+++ spec/poros/review_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe Movie do
+  before(:each) do
+    test_movie_reviews
+    test_movie_review_info
+    @reviews = Review.new(@movie_reviews_data)
+  end
+
+  it "exists and has attributes" do
+    expect(@reviews).to be_a(Review)
+    expect(@reviews.all_reviews).to be_an(Array)
+    expect(@reviews.all_reviews[0]).to be_a(String)
+    expect(@reviews.review_count).to be_an(Integer)
+  end
+
+  it "#get_review_info(review_details)" do
+    expect(@reviews.get_review_info(@movie_review_info_data)).to be_an(Array)
+    expect(@reviews.get_review_info(@movie_review_info_data)[0]).to be_a(String)
+  end
+end
