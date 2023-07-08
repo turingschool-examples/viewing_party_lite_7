78e7401f048a8db53f5ba1f1fe6dc739fc71b15d
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-08 08:02:53 -0600

    test: more movie attributes

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe Movie do
     before(:each) do 
       @movie = Movie.new({
         title: 'Soul',
-        vote_average: '9.75', 
-        id: '3'})
+        vote_average: 9.75, 
+        id: 3, 
+        run_time: 120,
+        genres: ['family', 'animation'], 
+        summary: 'this is a summary'
+        })
     end
 
     it 'exists' do 
@@ -15,8 +19,11 @@ RSpec.describe Movie do
 
     it 'has attributes' do 
       expect(@movie.title).to eq('Soul')
-      expect(@movie.vote_average).to eq('9.75')
-      expect(@movie.id).to eq('3')
+      expect(@movie.vote_average).to eq(9.75)
+      expect(@movie.id).to eq(3)
+      expect(@movie.run_time).to eq(120)
+      expect(@movie.genres).to eq(['family', 'animation'])
+      expect(@movie.summary).to eq('this is a summary')
     end
   end
 end
