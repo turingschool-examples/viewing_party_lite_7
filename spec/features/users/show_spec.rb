d33e74df6d09375f6a99a3336d4ee6dea72b3b10
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-07 20:45:19 -0600

    delete tests from viewing party spec and put them in show_spec

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -6,15 +6,6 @@ RSpec.describe 'User Dashboard page' do
     @user2 = User.create!(name: 'Emily Jones', email: '343ftl.com')
     @user3 = User.create!(name: 'Jimmy johnson', email: 'jj33@aol.com')
     @user4 = User.create!(name: 'Todd Guy', email: 'tg@gmail.com')
-    
-    @party1 = Party.create!(date: '2021-07-04', start_time: '17:00:00 UTC' , duration: 120, movie_id: 1, host_id: @user1.id)
-    @party2 = Party.create!(date: '2021-12-09', start_time: '08:00:00 UTC' , duration: 150, movie_id: 2, host_id: @user2.id)
-    
-    @party_users1 = PartyUser.create!(user_id: @user1.id, party_id: @party1.id)
-    @party_users2 = PartyUser.create!(user_id: @user3.id, party_id: @party1.id)
-    @party_users3 = PartyUser.create!(user_id: @user2.id, party_id: @party1.id)
-    @party_users4 = PartyUser.create!(user_id: @user2.id, party_id: @party2.id)
-    @party_users5 = PartyUser.create!(user_id: @user4.id, party_id: @party2.id)
   end
   
   it 'displays the users Dashboard' do
@@ -44,5 +35,47 @@ RSpec.describe 'User Dashboard page' do
 
     click_button('Discover Movies')
     expect(current_path).to eq("/users/#{@user1.id}/discover")
+  end
+
+    # Duration of Party with a default value of movie runtime in minutes; a viewing party should NOT be created if set to a value less than the duration of the movie
+  # When: field to select date
+  # Start Time: field to select time
+  # Checkboxes next to each existing user in the system
+  # Button to create a party
+  # Details When the party is created, the user should be redirected back to the dashboard where the new event is shown. The event should also be listed on any other user's dashbaords that were also invited to the party.
+  it 'displays new event on users dashboard' do 
+    visit  user_viewing_party_path(@user1, 238)
+
+    fill_in 'duration', with: 210
+    fill_in 'date', with: "2024-01-01"
+    fill_in 'start_time', with: "07:00"
+    check("selected_users[]", option: @user2.id)
+    check("selected_users[]", option: @user3.id)
+    click_button('Create Party')
+
+    expect(current_path).to eq(user_path(@user1))
+    expect(page).to have_content("The Godfather")
+    expect(page).to have_content(@user1.name)
+    expect(page).to have_content(@user2.name)
+    expect(page).to have_content(@user3.name)
+    expect(page).to_not have_content(@user4.name)
+  end
+  
+  it 'displays events on other invited users dashboards' do
+    visit user_viewing_party_path(@user1, 238)
+    
+    fill_in 'duration', with: 210
+    fill_in 'date', with: "2024-01-01"
+    fill_in 'start_time', with: "07:00"
+    check("selected_users[]", option: @user2.id)
+    check("selected_users[]", option: @user3.id)
+    click_button('Create Party')
+
+    visit user_path(@user2)
+    expect(page).to have_content("The Godfather")
+    expect(page).to have_content(@user1.name)
+    expect(page).to have_content(@user2.name)
+    expect(page).to have_content(@user3.name)
+    expect(page).to_not have_content(@user4.name)
   end
 end
--- spec/features/users/viewing_party/new_spec.rb
+++ spec/features/users/viewing_party/new_spec.rb
@@ -9,12 +9,7 @@ RSpec.describe "User's Viewing Party page" do
     
     visit  user_viewing_party_path(@user1, 238)
   end
-  # Duration of Party with a default value of movie runtime in minutes; a viewing party should NOT be created if set to a value less than the duration of the movie
-  # When: field to select date
-  # Start Time: field to select time
-  # Checkboxes next to each existing user in the system
-  # Button to create a party
-  # Details When the party is created, the user should be redirected back to the dashboard where the new event is shown. The event should also be listed on any other user's dashbaords that were also invited to the party.
+
   it 'displays the movie title' do
     expect(page).to have_content('The Godfather')
   end
@@ -57,23 +52,37 @@ RSpec.describe "User's Viewing Party page" do
     expect(current_path).to eq(user_path(@user1))
   end
   
-  it 'displays new event on users dashboard' do 
-    
-  end
-  
-  xit 'displays events on other invited users dashboards' do
-    
-  end
-  
   xit 'does not create a party if duration is less than the movie runtime' do
+    fill_in 'duration', with: 10
+    fill_in 'date', with: "2024-01-01"
+    fill_in 'start_time', with: "07:00"
+    check("selected_users[]", option: @user2.id)
+    check("selected_users[]", option: @user3.id)
+    click_button('Create Party')
+
+    expect(current_path).to eq(user_viewing_party_path(@user1, 238))
+    expect(page).to have_content("Party duration cannot be less than runtime")
+  end
   
+  it 'does not create a party if any field is empty' do
+    fill_in 'duration', with: ""
+    fill_in 'date', with: "2024-01-01"
+    fill_in 'start_time', with: "07:00"
+    check("selected_users[]", option: @user2.id)
+    check("selected_users[]", option: @user3.id)
+    click_button('Create Party')
+
+    expect(current_path).to eq(user_viewing_party_path(@user1, 238))
+    expect(page).to have_content("Duration can't be blank")
   end
+  
+  it 'does not create a party if no user is selected' do
+    fill_in 'duration', with: 300
+    fill_in 'date', with: "2024-01-01"
+    fill_in 'start_time', with: "07:00"
+    click_button('Create Party')
 
-  xit 'does not create a party if any field is empty' do
-
-  end
-
-  xit 'does not create a party if no user is selected' do
-
+    expect(current_path).to eq(user_viewing_party_path(@user1, 238))
+    expect(page).to have_content("Please select at least one user")
   end
 end
