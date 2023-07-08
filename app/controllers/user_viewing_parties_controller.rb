37710d86cc9ad052f1b004113c5271eb88f0bc38
K.D. Hubbard <hubbard.kd@gmail.com>
2023-07-08 11:54:09 -0600

    wip new viewing party form

--- app/controllers/user_viewing_parties_controller.rb
+++ app/controllers/user_viewing_parties_controller.rb
@@ -1,5 +1,7 @@
 class UserViewingPartiesController < ApplicationController 
   def new
-    @movie = MovieSearch.new.full_movie_details_by_id(params[:id])
+    @movie = MovieSearch.new.full_movie_details_by_id(params[:movie_id])
+    @viewing_party = ViewingParty.new
+    @user = User.find(params[:user_id])
   end
 end
