ab8a67d36ac346c5487ac8c9b37bc12682548d48
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-07 22:54:34 -0600

    Feat: Add a bunch of methods to MovieFacade

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,5 +1,6 @@
 class MovieFacade
-  attr_reader :user_id
+  attr_reader :movie_id,
+              :user_id
 
   def initialize(params)
     @movie_id = params[:id]
@@ -40,7 +41,63 @@ class MovieFacade
     movie_details.title
   end
 
+  def movie_rating
+    movie_details.rating
+  end
+
+  def movie_runtime
+    total_minutes = movie_details.runtime
+
+    hours = total_minutes / 60
+    minutes = total_minutes % 60
+    "#{hours}hr #{minutes}min"
+  end
+
+  def movie_genre
+    movie_details.genres.map { |genre| genre[:name] }.join(", ")
+  end
+
+  def movie_summary
+    movie_details.summary
+  end
+
+  def first_ten_cast
+    cast_collector = {}
+
+    MovieService.cast_by_id(@movie_id)[:cast][0..9].each do |cast|
+      cast_collector[cast[:name]] = cast[:character]
+    end
+
+    cast_collector
+  end
+
+  def movie_cast
+    first_ten_cast.map do |name, character|
+      "#{name} as #{character}"
+    end
+  end
+
+  def all_reviews
+    review_collector = {}
+
+    MovieService.reviews_by_id(@movie_id)[:results].map do |review|
+      review_collector[review[:author]] = review[:content]
+    end
+
+    review_collector
+  end
+
+  def movie_reviews
+    all_reviews.map do |author, content|
+      "#{content} - #{author}"
+    end
+  end
+
+  def number_of_reviews
+    all_reviews.count
+  end
+
   def find_by_id(movie_id)
-    results = MovieService.find_by_id(movie_id)
+    MovieService.find_by_id(movie_id)
   end
 end
