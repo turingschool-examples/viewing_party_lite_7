d810f50ae41aaebdd15ef10166a140560794f379
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-07-08 13:15:56 -0600

    Test: Movie Details Page Viewing Party

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -5,10 +5,20 @@ RSpec.describe "Movies Show Page" do
     before :each do
       @user1 = FactoryBot.create(:user)
       json_response = File.read('spec/fixtures/sandlot_id_search.json')
-      response1 = stub_request(:get, "https://api.themoviedb.org/3/movie/11528").
+      response1 = stub_request(:get, "https://api.themoviedb.org/3/search/movie/11528").
         to_return(status: 200, body: json_response)
       movie = JSON.parse(response1.response.body, symbolize_names: true)
       @movie_id = movie[:id]  
+
+      json_response2 = File.read('spec/fixtures/sandlot_cast.json')
+      response2 = stub_request(:get, "https://api.themoviedb.org/3/movie/11528/credits").
+        to_return(status: 200, body: json_response2)
+      @movie_cast = JSON.parse(response2.response.body, symbolize_names: true)
+
+      json_response3 = File.read('spec/fixtures/sandlot_reviews.json')
+      response3 = stub_request(:get, "https://api.themoviedb.org/3/movie/447365/reviews").
+        to_return(status: 200, body: json_response3)
+      @movie_review = JSON.parse(response3.response.body, symbolize_names: true)
     end
     
     it "return to discover page button" do
@@ -22,7 +32,7 @@ RSpec.describe "Movies Show Page" do
       visit user_movie_path(@user1.id, @movie_id)
       expect(page).to have_button "Create Viewing Party"
       click_button "Create Viewing Party"
-      expect(current_path).to eq new_user_movie_viewing_party_path(@user1.id, @movie[:id])
+      expect(current_path).to eq new_user_movie_viewing_party_path(@user1.id, @movie_id)
     end
   end
 end
--- spec/poros/popular_movie_spec.rb
+++ spec/poros/popular_movie_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PopularMovie do
   it "exists" do
     attrs = {
       id: 5555,
-      original_title: "Ace Ventura",
+      title: "Ace Ventura",
       vote_average: 10
     }
 
@@ -12,7 +12,7 @@ RSpec.describe PopularMovie do
 
     expect(movie).to be_a PopularMovie
     expect(movie.id).to eq(5555)
-    expect(movie.original_title).to eq("Ace Ventura")
+    expect(movie.title).to eq("Ace Ventura")
     expect(movie.vote_average).to eq(10)
   end
 end
