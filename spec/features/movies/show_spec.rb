94e9b8e142a6cc9c8dce1202b325d564fb75e3de
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-08 13:02:40 -0600

    Feat: Added stubbing for credits and reviews

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -2,13 +2,23 @@ require "rails_helper"
 
 RSpec.describe "Movie Details Page", type: :feature do
   before(:each) do
-    file = File.read("./spec/fixtures/lord_of_the_rings_collection.json")
+    lotr_details = File.read("./spec/fixtures/lord_of_the_rings_fellowship/details.json")
+    lotr_credits = File.read("./spec/fixtures/lord_of_the_rings_fellowship/credits.json")
+    lotr_reviews = File.read("./spec/fixtures/lord_of_the_rings_fellowship/reviews.json")
 
     @user = User.create!(name: "John", email: "john@example.com")
-    @movie = Movie.new(JSON.parse(file, symbolize_names: true))
+    @movie = Movie.new(JSON.parse(lotr_details, symbolize_names: true))
 
+    # This stubs out the API call to the movie details endpoint
     stub_request(:get, "https://api.themoviedb.org/3/movie/120?api_key=#{ENV['MOVIE_API_KEY']}")
-    .to_return(status: 200, body: file)
+    .to_return(status: 200, body: lotr_details)
+
+    # This stubs out the API call to the movie credits endpoint
+    stub_request(:get, "https://api.themoviedb.org/3/movie/120/credits?api_key=#{ENV['MOVIE_API_KEY']}")
+    .to_return(status: 200, body: lotr_credits)
+
+    stub_request(:get, "https://api.themoviedb.org/3/movie/120/reviews?api_key=#{ENV['MOVIE_API_KEY']}")
+    .to_return(status: 200, body: lotr_reviews)
   end
 
   describe "when I visit the movie details page" do
@@ -35,7 +45,7 @@ RSpec.describe "Movie Details Page", type: :feature do
         visit user_movie_path(@user, 120)
 
         expect(page).to have_button("Create Viewing Party for #{@movie.title}")
-        
+
         click_button("Create Viewing Party for #{@movie.title}")
       end
     end
