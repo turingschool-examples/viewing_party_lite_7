97907ef7b7e5ec0c29ac0aca227fc0c26e773f97
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-08 15:39:06 -0400

    move format runtime into application controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # # app/helpers/application_helper.rb
 module ApplicationHelper
+
+  def format_runtime(time)
+    if time
+      hours = time / 60
+      minutes = time % 60
+      "#{hours}hr #{minutes}min"
+    else
+      "no run time"
+    end
+  end
 end
