fde6e54d377cd662e0a6e769c632546d4a3d11b2
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-08 11:00:50 -0600

    add tests for instance methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,44 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
+
+  describe 'instance methods' do
+    describe '#hosted_parties' do
+      it 'list parties that the user is hosting' do
+        @user1 = User.create!(name: 'John Smith', email: 'jsmith@aol.com')
+        @user2 = User.create!(name: 'Emily Jones', email: '343ftl.com')
+        @user3 = User.create!(name: 'Jimmy johnson', email: 'jj33@aol.com')
+        
+        @party1 = Party.create!(date: '2023-01-01', start_time: '08:00', duration: 275, movie_id: 238, host_id: @user1.id)
+        @party2 = Party.create!(date: '2023-01-01', start_time: '08:00', duration: 275, movie_id: 238, host_id: @user2.id)
+        @party_user1 = PartyUser.create!(user_id: @user1.id, party_id: @party1.id)
+        @party_user2 = PartyUser.create!(user_id: @user2.id, party_id: @party1.id)
+        @party_user3 = PartyUser.create!(user_id: @user3.id, party_id: @party1.id)
+        @party_user4 = PartyUser.create!(user_id: @user1.id, party_id: @party2.id)
+        @party_user5 = PartyUser.create!(user_id: @user2.id, party_id: @party2.id)
+
+        expect(@user1.hosted_parties).to eq([@party1])
+        expect(@user2.hosted_parties).to eq([@party2])
+      end
+
+    describe '#invited_to_parties' do
+      it 'list parties the the user is invited to' do
+        @user1 = User.create!(name: 'John Smith', email: 'jsmith@aol.com')
+        @user2 = User.create!(name: 'Emily Jones', email: '343ftl.com')
+        @user3 = User.create!(name: 'Jimmy johnson', email: 'jj33@aol.com')
+        
+        @party1 = Party.create!(date: '2023-01-01', start_time: '08:00', duration: 275, movie_id: 238, host_id: @user1.id)
+        @party2 = Party.create!(date: '2023-01-01', start_time: '08:00', duration: 275, movie_id: 238, host_id: @user2.id)
+        @party_user1 = PartyUser.create!(user_id: @user1.id, party_id: @party1.id)
+        @party_user2 = PartyUser.create!(user_id: @user2.id, party_id: @party1.id)
+        @party_user3 = PartyUser.create!(user_id: @user3.id, party_id: @party1.id)
+        @party_user4 = PartyUser.create!(user_id: @user1.id, party_id: @party2.id)
+        @party_user5 = PartyUser.create!(user_id: @user2.id, party_id: @party2.id)
+
+        expect(@user1.invited_to_parties).to eq([@party2])
+        expect(@user2.invited_to_parties).to eq([@party1])
+        expect(@user3.invited_to_parties).to eq([@party1])
+      end
+    end
+  end
 end
