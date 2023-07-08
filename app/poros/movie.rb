0c5040efed9d53a9497073d6e8d40e4b099f31ac
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-07-08 08:04:22 -0600

    Feat: more attributes

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,11 +1,17 @@
 class Movie 
   attr_reader :title, 
               :vote_average, 
-              :id
+              :id, 
+              :run_time, 
+              :genres, 
+              :summary
   
   def initialize(attributes)
     @title = attributes[:title]
     @vote_average = attributes[:vote_average]
     @id = attributes[:id]
+    @run_time = attributes[:run_time]
+    @genres = attributes[:genres]
+    @summary = attributes[:summary]
   end
 end
