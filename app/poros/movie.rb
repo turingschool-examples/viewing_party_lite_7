c16df4b73b40fd161ee03aade211989362bcbadc
Myles Nottingham <MylesNottingham@gmail.com>
2023-07-07 22:55:50 -0600

    Feat: Round vote average to one decimal

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -9,7 +9,7 @@ class Movie
   def initialize(movie_data)
     @id = movie_data[:id]
     @title = movie_data[:title].to_s
-    @rating = movie_data[:vote_average]
+    @rating = movie_data[:vote_average].round(1)
     @runtime = movie_data[:runtime]
     @genres = movie_data[:genres]
     @summary = movie_data[:overview].to_s
