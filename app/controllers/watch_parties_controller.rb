f62256ee550bf8e3c89d56e39c503b0395c2e129
Jesse Sorman <jessesorman@gmail.com>
2023-07-08 16:51:44 -0400

    refactor new watch party form

--- app/controllers/watch_parties_controller.rb
+++ app/controllers/watch_parties_controller.rb
@@ -8,14 +8,21 @@ class WatchPartiesController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
+    @visitor = User.find(params[:user_id])
     watch_party = @user.watch_parties.create!(watch_parties_params)
+    watch_party_visitors = @visitor.watch_parties.create!(movie_watch_party_params)
     if watch_party.save
       redirect_to "/users/#{@user.id}"
+
     end
   end
 
   private
   def watch_parties_params
     params.permit(:date, :duration, :start_time, :movie_title)
+  end
+
+  def movie_watch_party_params
+    params.permit(:watch_party, :user, user_status: 0)
   end
 end
