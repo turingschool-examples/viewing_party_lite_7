aeffeda89ba6281fa855505f2be27d5b8f4b7d66
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-08 12:29:26 -0600

    add host_name instance method

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -16,10 +16,20 @@ RSpec.describe Party, type: :model do
 
   describe 'instance methods' do
     describe '#movie_title' do
-      it "can find title of a movie by its id" do
+      it 'can find title of a movie by its id' do
         user = User.create!(name: 'John Smith', email: 'jsmith@aol.com')
         party = Party.create!(date: '2021-07-04', start_time: '17:00:00 UTC' , duration: 120, movie_id: 238, host_id: user.id)
         expect(party.movie_title(238)).to eq("The Godfather")
+      end
+    end
+
+    describe '#host_name' do
+      it 'finds the name of the host of a viewing party' do
+        user1 = User.create!(name: 'John Smith', email: 'jsmith@aol.com')
+        party1 = Party.create!(date: '2023-01-01', start_time: '08:00', duration: 275, movie_id: 238, host_id: user1.id)
+        party_user1 = PartyUser.create!(user_id: user1.id, party_id: party1.id)
+        
+        expect(party1.host_name).to eq('John Smith')
       end
     end
   end
