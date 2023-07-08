2d706ea4e2d89d63f727e53d4a9367605cf7535b
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-08 13:25:12 -0600

    shorten expected string responses.

--- spec/poros/review_spec.rb
+++ spec/poros/review_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Movie do
     expect(@reviews).to be_a(Review)
     expect(@reviews.all_reviews).to be_an(Array)
     expect(@reviews.all_reviews[0]).to be_a(String)
-    expect(@reviews.all_reviews[0]).to eq("talisencrw's review: This unique take on the heist-film-gone-wrong was excellent--stylish and intelligently made, yet very funny and inexpensive. Tarantino's accolades from giving American cinema the resuscitation it needed mirrors what has happened, at least since the 70's, with Martin Scorsese's 'Mean Streets', both in terms of entertaining violence and usage of music in the scoring of films. I greatly thank Harvey Keitel for taking a chance on Tarantino back then--It paid off in spades.")
+    expect(@reviews.all_reviews[0].slice(0, 50)).to eq("talisencrw's review: This unique take on the heist")
     expect(@reviews.review_count).to be_an(Integer)
     expect(@reviews.review_count).to eq(2)
   end
@@ -19,6 +19,6 @@ RSpec.describe Movie do
   it "#get_review_info(review_details)" do
     expect(@reviews.get_review_info(@movie_review_info_data)).to be_an(Array)
     expect(@reviews.get_review_info(@movie_review_info_data)[0]).to be_a(String)
-    expect(@reviews.get_review_info(@movie_review_info_data)[0]).to eq("talisencrw's review: This unique take on the heist-film-gone-wrong was excellent--stylish and intelligently made, yet very funny and inexpensive. Tarantino's accolades from giving American cinema the resuscitation it needed mirrors what has happened, at least since the 70's, with Martin Scorsese's 'Mean Streets', both in terms of entertaining violence and usage of music in the scoring of films. I greatly thank Harvey Keitel for taking a chance on Tarantino back then--It paid off in spades.")
+    expect(@reviews.get_review_info(@movie_review_info_data)[0].slice(0, 50)).to eq("talisencrw's review: This unique take on the heist")
   end
 end
