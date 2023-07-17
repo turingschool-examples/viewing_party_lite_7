5a4c53d32e3eecb3f307016d372cc367d6900e0b
Amy Spears <amspears007@gmail.com>
2023-07-17 17:48:12 -0600

    add path helpers in to complete US3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      redirect_to "/users/#{user.id}"
+      redirect_to(user_path(user))
+      # redirect_to "/users/#{user.id}"
       flash[:notice] = 'User successfully registered.'
     else
       redirect_to register_path
@@ -26,9 +27,7 @@ class UsersController < ApplicationController
   end
 
   def login
-    # require 'pry'; binding.pry
     user = User.find_by(email: params[:email])
-
     redirect_to(user_path(user))
   end
 
