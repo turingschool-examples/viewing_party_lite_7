c48584ec6458e69472222ac1c347f9a18b8fd3c0
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-07-17 14:48:22 -0600

    verify data submission user creation functionality

--- app/controllers/users/register_controller.rb
+++ app/controllers/users/register_controller.rb
@@ -1,16 +1,25 @@
 class Users::RegisterController < ApplicationController
   def index
-
   end
 
   def create
-    @user = User.new(email: params[:email], name: params[:name], password_digest: params[:password])
-    if @user.save
-      redirect_to user_path(@user.id)
+    if params[:password] != params[:password_confirmation]
+      flash[:error] = "Invalid email or password"
+      redirect_to users_register_index_path
+    else
+      @user = User.new(name: params[:name], email: params[:email], password_digest: params[:password])
+      if @user.save
+        flash[:success] = "Welcome #{@user.name}, you have sucussfully registered"
+        redirect_to user_path(@user.id)
+      else
+        flash[:error] = "Invalid email or password"
+        redirect_to users_register_index_path
+      end
     end
   end
 
   private
-    def user_params
-    end
+    # def user_params
+    #   params.permit(:email, :name)
+    # end
 end
