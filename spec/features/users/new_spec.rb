0d7b0efcfc26e0e80e1d23af8176064d614f3a37
Steven Stackhouse <stackmm@gmail.com>
2023-07-17 13:33:05 -0600

    add password checks to registration tests

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -7,22 +7,28 @@ RSpec.describe 'User registration page' do
 
   it 'displays a new user registration form' do
     expect(page).to have_content('Register New User')
-    expect(page).to have_field('name')
-    expect(page).to have_field('email')
-    expect(page).to have_button('Create')
+    expect(page).to have_field(:user_name)
+    expect(page).to have_field(:user_email)
+    expect(page).to have_field(:user_password)
+    expect(page).to have_field(:user_password_confirmation)
+    expect(page).to have_button('Register')
   end
 
   it 'registers a new user' do
-    fill_in 'name', with: 'John Adams'
-    fill_in 'email', with: 'jadams76@gmail.com'
-    click_button 'Create'
+    fill_in :user_name, with: 'John Adams'
+    fill_in :user_email, with: 'jadams76@gmail.com'
+    fill_in :user_password, with: 'password'
+    fill_in :user_password_confirmation, with: 'password'
+    click_button 'Register'
 
     expect(current_path).to eq("/users/#{User.last.id}")
   end
 
   it 'displays an error if form is incomplete' do
-    fill_in 'email', with: 'jadams6@gmail.com'
-    click_button 'Create'
+    fill_in :user_email, with: 'jadams6@gmail.com'
+    fill_in :user_password, with: 'password'
+    fill_in :user_password_confirmation, with: 'password'
+    click_button 'Register'
 
     expect(current_path).to eq('/register')
     expect(page).to have_content("Name can't be blank")
