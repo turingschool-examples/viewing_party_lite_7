776ab3d9ce1db81614dad4a462103fdacb7b1de8
Alex Kiselich <alexander.kiselich@gmail.com>
2023-07-17 17:14:13 -0600

    Added create, login_form and login_user action to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-
     if user.save
       redirect_to user_path(user)
     else
@@ -19,9 +18,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def login_form
+  end
+
+def login_user
+  user = User.find_by(email: params[:email])
+  if user.authenticate(params[:password])
+    session[:user] = user.id
+    flash[:success] = "Welcome #{user.user_name}!"
+    redirect_to user_path(user)
+  else
+    flash[:error] = "Sorry, your credentials are bad."
+    render :login_form
+  end
+end
+
+
+
   private
 
   def user_params
-    params.permit(:user_name, :email)
+    params.permit(:user_name, :email, :password, :password_confirmation)
   end
 end
