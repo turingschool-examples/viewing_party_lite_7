799b555e0a6c04c259e31fcd4a18fc237ae077c3
Amy Spears <amspears007@gmail.com>
2023-07-17 13:56:57 -0600

    Add validations and password_digest to model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name,  presence: true
+  validates :email,uniqueness: true, presence: true
+  validates_presence_of :password
+  # has_secure_password
   has_many :user_viewing_parties
   has_many :viewing_parties, through: :user_viewing_parties
+
+
 
   def parties_hosting
     viewing_parties.where('user_viewing_parties.host = true')
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,10 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:password_digest)} 
+    # -- Remember, bcrypt will check out passwords, and store it as `password_digest`
+    it { should have_secure_password}
   end
 
   describe 'associatons' do
