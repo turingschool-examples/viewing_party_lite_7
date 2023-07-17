3946f4b65a34453d6bc1682759ff88c101ac638c
Steven Stackhouse <stackmm@gmail.com>
2023-07-17 13:32:16 -0600

    add email downcase and strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    user_data = user_params
+    user_data[:email] = user_data[:email].downcase
+    @user = User.new(user_data)
+
     if @user.save
-      redirect_to "/users/#{@user.id}"
+      redirect_to user_path(@user)
     else
       flash[:notice] = @user.errors.full_messages.to_sentence
-      redirect_to '/register'
+      redirect_to register_path
     end
   end
 
@@ -17,8 +21,7 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
-    params.permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
