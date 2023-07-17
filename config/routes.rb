c6787ae0c4fbabd64c8edefbe33211c5b53b6cca
Amy Spears <amspears007@gmail.com>
2023-07-17 17:44:50 -0600

    Add unrestful routes for both user login and login_form

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/', to: 'application#home'
+  root 'application#home'
   get '/register', to: 'users#new'
   get '/users/:id/discover', to: 'movies#index'
   get '/users/:user_id/movies', to: 'user_movies#index'
   get '/users/:user_id/movies/:movie_id', to: 'user_movies#show'
   get  '/users/:user_id/movies/:movie_id/viewing-party/new', to: 'user_viewing_parties#new'
   post '/users/:id', to: 'user_viewing_parties#create'
+  get '/login', to: "users#login_form"
+  post '/login', to: 'users#login'
 
   resources :movies, only: [:show]
   resources :users, only: %i[index create show]
