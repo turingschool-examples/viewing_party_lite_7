6cca3f027b6e89b1120f4735e709af704738f86e
Amy Spears <amspears007@gmail.com>
2023-07-18 16:14:07 -0600

    Add column role to users defined by enums and default as regular user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_195336) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_220723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_195336) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   create_table "viewing_parties", force: :cascade do |t|
