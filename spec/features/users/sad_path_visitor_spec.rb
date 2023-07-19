d81da10cd62053e1b9cde46e93fb54a15d64bfb9
Javen Wilson <javenb022@gmail.com>
2023-07-18 21:55:18 -0400

    Test: Errors if not logged in visiting dashboard

--- spec/features/users/sad_path_visitor_spec.rb
+++ spec/features/users/sad_path_visitor_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe "Sad Path Visitor" do
     it "Errors if trying to visit dashboard while not logged in" do
       visit root_path
 
-      
+      expect(current_path).to eq(root_path)
+
+      visit dashboard_path
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("You must be logged in to visit that page.")
     end
   end
 end
