2668c8399d263488289a134d374f8d0fd663e49e
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-07-18 18:15:21 -0600

    visitor session - users no in view on landing page

--- spec/features/sessions/landing_spec.rb
+++ spec/features/sessions/landing_spec.rb
@@ -2,45 +2,45 @@ require 'rails_helper'
 
 RSpec.describe 'Application landing page' do
   describe 'landing page view' do
-    before :each do
-      @user1 = FactoryBot.create(:user)
-      @user2 = FactoryBot.create(:user)
-      @user3 = FactoryBot.create(:user)
-      @user4 = FactoryBot.create(:user)
-
-      visit "/"
-    end
-
     it "has title header with create user button" do
-      within "#title" do
-        expect(page).to have_content("Movie Viewing Party!")
-        expect(page).to have_button "Create New User"
-      end
+      visit root_path
+
+      expect(page).to have_content("Movie Viewing Party!")
+      expect(page).to have_link "Already have an account - Login"
+      expect(page).to have_button "Create New User"
+      expect(page).to_not have_content "Movie Viewing Users:"
     end
 
-    it "has user list with links" do
-      within "#users" do
-        expect(page).to have_link "#{@user1.name}"
-        expect(page).to have_link "#{@user2.name}"
-  
-        click_link "#{@user1.name}"
-        expect(current_path).to eq user_path(@user1.id)
-      end
-    end
-
-    it 'link to return to landing page' do
+    it 'has link to return to landing page' do
+      visit root_path
       expect(page).to have_link "Return to Movie Viewing Party Landing Page"
       click_link "Return to Movie Viewing Party Landing Page"
       expect(current_path).to eq "/"
     end
 
     it "has link to login form" do
+      visit root_path
       within "#title" do
         expect(page).to have_link "Already have an account - Login"
 
         click_link "Already have an account - Login"
         expect(current_path).to eq login_path
       end
+    end
+  end
+
+  describe "landing page sessions" do
+    before :each do
+      @user = User.create!(name: "Email", email: "email@email.com", password: "secretpassword")
+    end
+
+    it "does not show current users to visitors" do
+      visit root_path
+
+      expect(page).to have_link "Already have an account - Login"
+      expect(page).to have_button "Create New User"
+      expect(page).to_not have_link "#{@user.name}"
+      expect(page).to_not have_content "Movie Viewing Users:"
     end
   end
 end
