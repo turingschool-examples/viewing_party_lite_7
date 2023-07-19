2b94802d81903c2e3874104e6fb7c80caafeaa37
Javen Wilson <javenb022@gmail.com>
2023-07-19 00:00:14 -0400

    Test: user can't create viewing party when logged out

--- spec/features/users/sad_path_visitor_spec.rb
+++ spec/features/users/sad_path_visitor_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Sad Path Visitor" do
+RSpec.describe "Sad Path Visitor", :vcr do
   describe "as a visitor" do
     it "Errors if trying to visit dashboard while not logged in" do
       visit root_path
@@ -11,6 +11,17 @@ RSpec.describe "Sad Path Visitor" do
 
       expect(current_path).to eq(root_path)
       expect(page).to have_content("You must be logged in to visit that page.")
+    end
+
+    it "can't create a viewing party when not logged in" do
+      movie = MovieFacade.new(255).movie_id
+
+      visit movie_path(movie)
+
+      click_button "Create Viewing Party"
+
+      expect(current_path).to eq(movie_path(movie))
+      expect(page).to have_content("You must be logged in to create a viewing party.")
     end
   end
 end
