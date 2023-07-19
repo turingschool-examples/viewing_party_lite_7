546508181c28c7751d73540a2142c5c33d16b5ae
Javen Wilson <javenb022@gmail.com>
2023-07-18 21:55:00 -0400

    Test: can visit dashboard when logged in

--- spec/features/users/can_login_a_user_spec.rb
+++ spec/features/users/can_login_a_user_spec.rb
@@ -61,4 +61,21 @@ RSpec.describe "Logging In" do
     expect(page).to have_content(user.email)
     expect(page).to have_content(user1.email)
   end
+
+  it "can visit dashboard if logged in" do
+    user = User.create!(name: "Bob", email: "bob@gmail.com", password: "password")
+
+    visit root_path
+    click_link "Log In"
+    expect(current_path).to eq(login_path)
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    click_button "Log In"
+
+    expect(current_path).to eq(root_path)
+
+    visit dashboard_path
+
+    expect(current_path).to eq(dashboard_path)
+  end
 end
