327d9a8f836c9ad74a5c1294bcd56f1052f4da6f
Kaina Cockett <kainacockett@gmail.com>
2023-08-22 17:11:16 -0600

    test and features for User Dashboard
    Co-authored-by: Kaina Cockett <kainacockett@gmail.com>
    
    Co-authored-by: Derek Chavez <dmchavez1824@yahoo.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,9 @@ class UsersController < ApplicationController
 
   # end
 
-  # def show
-    
-  # end
+  def show
+    @user = User.find(params[:id])
+  end
 
   # def create
   #   @user = User.new(user_params)
