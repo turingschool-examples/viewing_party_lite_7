5fbbf267a8da77d75b3934eb6e97377c2cf41c32
Dani Wilson <danirae.powell@gmail.com>
2023-08-21 18:28:31 -0600

    chore: create db tables and spec files
    Co-authored-by: Bradley <bmilton2412@gmail.com>

---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_001654) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_score"
+    t.integer "runtime"
+    t.string "genre"
+    t.string "description"
+    t.string "cast"
+    t.integer "review_count"
+    t.string "review"
+    t.string "reviewer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "viewing_parties", force: :cascade do |t|
+    t.integer "duration"
+    t.string "date"
+    t.string "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
+    t.bigint "movies_id", null: false
+    t.index ["movies_id"], name: "index_viewing_parties_on_movies_id"
+    t.index ["users_id"], name: "index_viewing_parties_on_users_id"
+  end
+
+  add_foreign_key "viewing_parties", "movies", column: "movies_id"
+  add_foreign_key "viewing_parties", "users", column: "users_id"
+end
