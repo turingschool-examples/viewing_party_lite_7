b5118c4d711b725747573fa4b81347737990bad4
Allen Russell <garyrusselljr@gmail.com>
2023-08-22 16:38:27 -0600

    feat: welcome index and user show page
    Co-authored-by: Connor <richmond.connor@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user_1 = User.create!(name: "Ringo", email: "ringo@gmail.com")
+user_2 = User.create!(name: "Paul", email: "paul@gmail.com")
+user_3 = User.create!(name: "George", email: "george@gmail.com")
+user_4 = User.create!(name: "John", email: "john@gmail.com")
+user_5 = User.create!(name: "Yoko", email: "yoko@gmail.com")
+
