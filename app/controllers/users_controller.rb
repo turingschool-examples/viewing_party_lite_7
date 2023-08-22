22dcaec602c6a795da3593df9c7aef0d9b723c8f
Nick Sacco <nicksacco10@gmail.com>
2023-08-22 16:23:56 -0700

    feat: happy path user creation with new form, routes and controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
   end
 
   def show
-    
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to dashboard_path(@user.id)
+    else
+      flash[:error] = "Please fill in all fields."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
