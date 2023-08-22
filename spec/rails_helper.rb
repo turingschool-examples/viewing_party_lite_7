464f4449115b7fc89f2cac703f99d0d9aaa27fc7
Allen Russell <garyrusselljr@gmail.com>
2023-08-22 15:06:12 -0600

    docs: vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,20 +69,11 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
+end
 
-  VCR.configure do |config|
-    if Rails.env.test?
-      vcr_mode = ENV['VCR_MODE'] =~ /rec/i ? :all : :once
-      config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-      config.hook_into :webmock
-    
-      config.default_cassette_options = {
-        record: vcr_mode,
-        match_requests_on: %i[method uri body]
-      }
-    
-      config.allow_http_connections_when_no_cassette = true
-      config.configure_rspec_metadata!
-    end
-  end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<API_KEY_AUTH>') { ENV["API_KEY_AUTH"] }
+  config.configure_rspec_metadata!
 end
