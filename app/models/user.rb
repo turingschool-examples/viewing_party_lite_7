80c545e64c89684229995ea294c517f449c15376
Jorja Fleming <aset284@gmail.com>
2023-08-21 19:07:18 -0600

    Feat: complete registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :email, presence: true
+  validates :name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
   # end
   describe "validations" do
     it { should validate_presence_of :email }
-    # it { should validate_presence_of :name }
-    # it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of :name }
+    it { should validate_uniqueness_of(:email).case_insensitive }
   end
 end
