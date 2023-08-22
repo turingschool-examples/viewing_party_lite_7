332d18525c509bf79863e9c75d8cea212c5cf8a0
Davis Weimer <davisweimer@gmail.com>
2023-08-22 15:50:20 -0600

    route to controller action and creation

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe "Landing Page", type: :feature do
 
       expect(page).to have_content("Viewing Party!")
     end
+
+    it "has a button to create a new user" do
+      visit root_path
+
+      within "#new_user" do
+        expect(page).to have_button("Create New User")
+        click_button("Create New User")
+      end
+      expect(current_path).to eq(register_path)
+    end
   end
 end
