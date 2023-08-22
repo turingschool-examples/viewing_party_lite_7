e5639713face08aa296344a10cf83455c2db8dba
Nick Sacco <nicksacco10@gmail.com>
2023-08-22 16:40:32 -0700

    test: new user tests and user vallidation tests

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -4,14 +4,23 @@ RSpec.describe 'User Registration' do
   describe "When I visit the user registration page" do
     it "can create a new user" do
       visit register_path
-
-      fill_in :name, with: "Bob"
-      fill_in :email, with: "bob@bob.com"
-
+      save_and_open_page
+      fill_in :user_name, with: "Bob"
+      fill_in :user_email, with: "bob@bob.com"
       click_button "Register"
-
+      
       user = User.last
       expect(current_path).to eq(dashboard_path(user.id))
+    end
+    
+    it "can't create a new user without a name" do
+      visit register_path
+      fill_in :user_name, with: "Tim"
+      fill_in :user_email, with: ""
+      click_button "Register"
+
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Please fill in all fields.")
     end
   end
 end
