409fd1b6ac6cc30b6dcbcdc1e0080f723698c847
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-08-22 15:55:58 -0600

    user model validations

---
+++ spec/features/landing_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Landing Page' do 
+  scenario "visit the root path" do 
+    scenario "displays the title of the application, create new user button, list of users, homepage link" do 
+      
+    end
+  end
+end
