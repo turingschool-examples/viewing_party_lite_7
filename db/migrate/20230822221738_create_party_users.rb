ed5bc97ba3190745b0dcf2df9a046567b7dacaab
Davis Weimer <davisweimer@gmail.com>
2023-08-22 16:28:07 -0600

    create tables, models and relationships

---
+++ db/migrate/20230822221738_create_party_users.rb
@@ -0,0 +1,11 @@
+class CreatePartyUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :party_users do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :party, null: false, foreign_key: true
+      t.boolean :host, default: false
+
+      t.timestamps
+    end
+  end
+end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Landing Page", type: :feature do
+  before :each do
+    @user_1 = User.create!(name: "Jimmy", email: "movie_buff333@gmail.com")
+    @user_2 = User.create!(name: "Timmy", email: "i_hate_movies@gmail.com")
+    @user_3 = User.create!(name: "Tammy", email: "gamer4134@gmail.com")
+  end
   describe "#index" do
     it "displays the title of the application" do
       visit root_path
@@ -16,6 +21,16 @@ RSpec.describe "Landing Page", type: :feature do
         click_button("Create New User")
       end
       expect(current_path).to eq(register_path)
+    end
+
+    it "displays a list of existing users" do
+      visit root_path
+
+      within "#existing_users" do
+        expect(page).to have_content("movie_buff333@gmail.com")
+        expect(page).to have_content("i_hate_movies@gmail.com")
+        expect(page).to have_content("gamer4134@gmail.com")
+      end
     end
   end
 end
