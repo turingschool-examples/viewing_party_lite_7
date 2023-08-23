2bf88a766db16de1acefb26362b60fb37d94fa2a
Anna Wiley <awiley33@gmail.com>
2023-08-23 16:05:56 -0500

    test: Add testing for Discover Movies Page
    Co-authored-by: Anna Wiley <awiley33@gmail.com>
    Co-authored-by: Ian Lyell <ilyell23@turing.edu>

---
+++ spec/features/movies/discover_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Discover Movies Page" do
+  before do
+    create_list(:user, 1)
+    @user = User.first
+    visit user_discover_path(@user)
+  end
+
+  it "has a button to discover top rated movies" do
+    expect(page).to have_button("Top Rated Movies")
+
+    click_button("Top Rated Movies")
+    expect(current_path).to eq(user_movies_path(@user))
+  end
+
+  it "has a form to search for movies by title" do
+    expect(page).to have_field("search")
+    expect(page).to have_button("Search")
+
+
+    click_button("Search")
+    expect(current_path).to eq(user_movies_path(@user))
+  end
+end
