33135f0e7efe9c0fa0c30a72d7e4053613ac6dd1
Nick Sacco <nicksacco10@gmail.com>
2023-08-23 08:56:17 -0700

    test: discover movies beginning tests

---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Discover Movies page '/users/:id/discover'", type: :feature do
+  before(:each) do
+    @user_1 = User.create!(name: "Jimmy", email: "movie_buff333@gmail.com")
+    @user_2 = User.create!(name: "Timmy", email: "i_hate_movies@gmail.com")
+    @user_3 = User.create!(name: "Tammy", email: "gamer4134@gmail.com")
+  end
+
+  describe "When I visit a user's discover movies page" do
+    it "Displays a button to find top rated movies" do
+      visit discover_path(@user_1.id)
+
+      within "#discover_top_movies" do
+        expect(page).to have_button("Find Top Rated Movies")
+      end
+    end
+
+    it "Displays a text field and button to search for a movie title" do
+      visit discover_path(@user_2.id)
+
+      within "title_search" do
+        expect(page).to have_field("Movie Title")
+        expect(page).to have_button("Find Movies")
+      end
+    end
+  end
+end
