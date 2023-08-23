d6f0f98ac553daa90109a9de32ca2e15f3882087
Gabriel Torres <thomastgtorres21@gmail.com>
2023-08-23 00:05:23 -0500

    create feature test

---
+++ spec/features/user/discover/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover Movies', type: :feature do
+  describe 'Movies Results Page ' do
+    scenario "i visit the discover movie page and click either button then im redirected and see api data" do 
+      u1 = User.create!(name: "Sean", email: "champion4lyfe@gmail.com")
+
+      visit user_discover_index_path(u1)
+
+      expect(page).to have_button("Discover Top Rated Movies")
+
+      click_button("Discover Top Rated Movies")
+    end
+  end
+end
