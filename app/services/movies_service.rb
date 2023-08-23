8a2e8e71d887c5de05c14ba067e8383fa955eb43
Artemy Gibson <artemygibson@gmail.com>
2023-08-23 12:56:39 -0700

    Add skeleton for search method, change three separate API calls into one call using append_to_response

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -1,25 +1,25 @@
 class MoviesService 
   def connection 
-    require 'pry'; binding.pry
     connection = Faraday.new("https://api.themoviedb.org/") do |faraday|
       faraday.params["api_key"] = ENV["MOVIE_API_KEY"]
     end
-    
+  end
+
+  def search(keyword)
+
   end
 
   def top_rated
     response = connection.get("3/movie/top_rated")
+    JSON.parse(response.body)["results"]
   end
 
   def find_movie(id)
-
-  end
-
-  def reviews_for(id)
-
-  end
-
-  def cast_of(id)
-
+    response = connection.get("3/movie/#{id}") do |f|
+      f.params["append_to_response"] = "reviews,credits"
+      # makes all three calls at once
+      # Includes the details, reviews, and credits in one response
+    end
+    Movie.new(JSON.parse(response.body))
   end
 end
