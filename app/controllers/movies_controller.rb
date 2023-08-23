4af99decc64458bedde52b44de69d2cade2692ff
Ethan Black <ethanrossblack@gmail.com>
2023-08-23 16:41:43 -0600

    refactor to include user and remove comments

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,17 +4,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @user = User.find(params[:user_id])
     @facade = MoviesFacade.new(params[:q])
-
-    # conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-    #   faraday.headers["Authorization"] = ENV["TMBD_API_KEY"]
-    # end
-
-    # response = conn.get("/3/movie/top_rated?language=en-US&with_original_language=en&page=1")
-
-    # json = JSON.parse(response.body, symbolize_names: true)
-    # @top_20 = json[:results].first(20).map do |movie_data|
-    #   Movie.new(movie_data)
-    # end
   end
 end
