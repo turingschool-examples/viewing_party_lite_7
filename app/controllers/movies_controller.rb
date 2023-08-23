2588bab0a1949f4b2899357bd5467163f6870df5
Ethan Black <ethanrossblack@gmail.com>
2023-08-23 15:42:17 -0600

    fix: add poros to controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,8 @@ class MoviesController < ApplicationController
     response = conn.get("/3/movie/top_rated?language=en-US&with_original_language=en&page=1")
 
     json = JSON.parse(response.body, symbolize_names: true)
-    @top_20 = json[:results]
+    @top_20 = json[:results].first(20).map do |movie_data|
+      Movie.new(movie_data)
+    end
   end
 end
