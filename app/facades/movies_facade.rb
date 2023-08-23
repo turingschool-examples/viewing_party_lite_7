02e6dd7434f7fca44b9e141baeb63310322e4305
Ethan Black <ethanrossblack@gmail.com>
2023-08-23 16:42:12 -0600

    refac: incorporate service into facade

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,17 +1,13 @@
 class MoviesFacade
   def initialize(search)
-
   end
 
-  def top20rated
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["Authorization"] = ENV["TMBD_API_KEY"]
-    end
+  def top_20_rated
+    service = MoviesService.new
 
-    response = conn.get("/3/movie/top_rated?language=en-US&with_original_language=en&page=1")
+    json = service.top_20_rated
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    @top20rated = json[:results].first(20).map do |movie_data|
+    @top_20_rated = json[:results].first(20).map do |movie_data|
       Movie.new(movie_data)
     end
   end
