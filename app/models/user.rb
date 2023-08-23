1e33a3b60cac264c9919371ef16eb4daf587918d
Dani Wilson <danirae.powell@gmail.com>
2023-08-22 20:00:08 -0600

    add validations
    Co-authored-by: Bradley <bmilton2412@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+
   has_many :viewing_parties
   has_many :movies, through: :viewing_parties
 end
