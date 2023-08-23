4f81312407d0055f1d469a68245169f539d12824
Chris Cullinane <chriscullinane@protonmail.com>
2023-08-22 18:59:27 -0800

    user controller create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    user = User.new(user_params)
+    if user.save
+      flash[:success] = "User successfully created"
+      redirect_to user_path(user.id)
+    else
+      flash[:error] = "Error: #{error_message(user.errors)}"
+      redirect_to register_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
   end
 end
