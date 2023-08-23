c13752ab7b2aeb9607c3d892a19423f697669d6e
andi lovetto <andilovetto@gmail.com>
2023-08-23 12:11:03 -0600

    feat: Adds gems and configs for VCR

---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Discover Index' do
+  it "has a button to discover top rated movies", :vcr do
+    aquaria = User.create!(name: "Aquaria", email: "Aquariahoe@hotmess.com")
+
+  end
+
+
+end
