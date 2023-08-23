252b5599ff5e56d8e072d4320e79930c52c51379
Anna Wiley <128400776+awiley33@users.noreply.github.com>
2023-08-23 11:54:19 -0500

    Delete movies_service.rb
    Delete pluralized service file

--- app/services/movies_service.rb
+++
@@ -1,9 +0,0 @@
-class MoviesService
-  def conn
-    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.params["api_key"] = ENV["movie_api_key"]
-    end
-
-    response = conn.get("/3/discover/movie") 
-  end
-end
