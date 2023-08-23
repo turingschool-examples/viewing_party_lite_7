038b4f5bc0c1a749f36429286427a8dc8d2b07be
Nick Sacco <nicksacco10@gmail.com>
2023-08-22 17:39:06 -0700

    feat: routes to controller action fill in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by_id(params[:id])
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "welcome#index"
 
   get "/register", to: "users#new"
   post "/register", to: "users#create"
   
-  get "/dashboard", to: "users#show"
+  get "/users/:id", to: "users#show", as: :dashboard
 end
