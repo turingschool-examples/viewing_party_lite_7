bca0f8b01964d6ead993fc32832aaab83d132071
Chris Cullinane <chriscullinane@protonmail.com>
2023-08-22 20:35:02 -0800

    email validation through rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, options[:message] || "'#{value}' is not a valid email"
+    end
+  end
+end
+
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, email: true
 end
