55a9fc0e8b5c5b76c5b638a22a598166b9f61cdd
Nick Sacco <nicksacco10@gmail.com>
2023-08-23 09:57:44 -0700

    feat: movie service top rated movie

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -4,4 +4,13 @@ class MovieService
       faraday.params['api_key'] = ENV['MOVIE_API_KEY']
     end
   end
+
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.top_rated_movies
+    get_url('movie/top_rated')
+  end
 end
