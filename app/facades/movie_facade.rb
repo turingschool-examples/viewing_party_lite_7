125b55b30bc219cde902a4e85e048eea373a7565
Nick Sacco <nicksacco10@gmail.com>
2023-08-23 11:53:40 -0700

    feat: create movie poro and add comment to movie facade

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,6 +1,6 @@
 class MovieFacade
-  def self.top_rated_movies
-    MovieService.top_rated_movies.map do |movie_data|
+  def self.top_movies
+    MovieService.top_rated_movies[:results].map do |movie_data| ##<-- top_rated_movies is a method in the MovieService class
       Movie.new(movie_data)
     end
   end
