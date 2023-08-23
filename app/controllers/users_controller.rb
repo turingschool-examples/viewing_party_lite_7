8193563794d01dbe6de3fb6d153a949c8de878d0
Dani Wilson <danirae.powell@gmail.com>
2023-08-22 19:59:48 -0600

    add new, create, and private methods
    Co-authored-by: Bradley <bmilton2412@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,34 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    # @user = User.find(params[:id])
+  end
+
+  # def create
+  #   User.create!(new_user_params)
+  #   if @user.save
+  #     redirect_to user_path(@user)
+  #   else
+  #     flash[:notice] = "Data Invalid"
+  #     render :new
+  #   end
+  # end
+
+  def create
+    user = User.new(new_user_params)
+    if user.save
+      redirect_to user_path(user)
+    else 
+      flash[:error] = "Data Invalid"
+      render :new
+    end
+  end
+
+  private
+
+  def new_user_params
+    params.permit(:name, :email)
+  end
 end
