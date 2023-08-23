4370ab6f3c61deeef22f243d5c74755f5677af61
Matthew Lim <matlim200@gmail.com>
2023-08-22 18:09:44 -0700

    feat: added /discover to user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    get "discover", on: :member
+  end
+
   resources :movies, only: [:index]
 end
