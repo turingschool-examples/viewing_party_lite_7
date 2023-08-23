e8973e62cb7b6064ef651f8dd8f1de1eae9e774e
Dani Wilson <danirae.powell@gmail.com>
2023-08-22 21:03:11 -0600

    complete tests, add sad path tests
    Co-authored-by: Bradley <bmilton2412@gmail.com>

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -6,25 +6,29 @@ RSpec.describe "user registration page" do
     @user2 = User.create!(name: "Rom Com Ron", email: "comron@yahoo.com")
     @user3 = User.create!(name: "Action Jackson", email: "itsjack@aol.com")
   end
-  it "should contain a user registration form" do
-    #user story
-    #   When a user visits the '/register' path they should see a form to register.
-
-    # The form should include:
-
-    # Name
-    # Email (must be unique)
-    # Register Button
-    # Once the user registers they should be taken to a dashboard page '/users/:id', where :id is the id for the user that was just created.
+  it "should contain a user registration form and test uniqueness of email" do
     visit "/register"
 
     expect(page).to have_field("Name")
     fill_in "Name", with: "PopCorn"
     expect(page).to have_field("Email")
+
     fill_in "Email", with: "popcornfool@gmail.com"
 
     expect(page).to have_button("Register")
     click_button "Register"
     expect(current_path).to eq(user_path(User.last))
+    
+    visit "/register"
+
+    expect(page).to have_field("Name")
+    fill_in "Name", with: "PopCorn"
+    expect(page).to have_field("Email")
+
+    fill_in "Email", with: "popcornfool@gmail.com"
+
+    expect(page).to have_button("Register")
+    click_button "Register"
+    expect(page).to have_content("Data Invalid")
   end
 end
