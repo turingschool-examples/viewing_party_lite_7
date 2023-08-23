063e73c0b4a9577ed7cbbb8257e65798d0c2dddc
Nick Sacco <nicksacco10@gmail.com>
2023-08-23 11:30:09 -0700

    test: begin movie facade tests

---
+++ spec/facades/movie_facade_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe MovieFacade do
+  describe 'creates movie facade' do
+    it 'returns top rated movies', :vcr do
+      movies = MovieFacade.top_rated_movies
+
+      expect(movies).to be_a Array
+      expect(movies.first).to be_a Movie
+      expect(movies.first.title).to be_a String
+      expect(movies.count).to eq(20)
+    end
+  end
+end
