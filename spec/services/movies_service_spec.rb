24d8a17b4d1295f5d9e3b8770af36995c41b1236
Artemy Gibson <artemygibson@gmail.com>
2023-08-23 12:57:07 -0700

    Write skeleton tests for movie service

--- spec/services/movies_service_spec.rb
+++ spec/services/movies_service_spec.rb
@@ -1,26 +1,26 @@
 require "rails_helper"
 
 RSpec.describe MoviesService do
-  it "can connect to movie db" do
-    require 'pry'; binding.pry
-    get "https://api.themoviedb.org/" # pass API key
+  # set up VCR?
+  xit "can connect to movie db" do
+    response = MoviesService.new.connection
 
-    expect(response).to have_http_status(:success)
+    # expect(response).to be_successful
   end
 
   it "can retrieve top 20 movies" do
-    # top_rated&include_adult=false
+    movies = MoviesService.new.top_rated 
+    
+    expect(movies).to be_an(Array)
+    expect(movies.count).to eq(20)
   end
 
-  it "can retrieve a specific movie" do
+  it "can search for movies by keyword in title" do
 
   end
 
-  it "can retrieve a specific movie's reviews" do
-
-  end
-
-  it "can retrieve a specific movie's cast" do
-
+  it "can generate a poro for a specific movie" do
+    movie = MoviesService.new.find_movie(234)
+    
   end
 end
