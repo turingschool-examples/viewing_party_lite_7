2ead743688ec210c79f4fc317d8db9d2d2dcd894
Dani Wilson <danirae.powell@gmail.com>
2023-08-23 09:23:57 -0600

    edit styling

---
+++ spec/features/register_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe "user registration page" do
+  before(:each) do
+    @user1 = User.create!(name: "Flick Chick", email: "flick@yahoo.com")
+    @user2 = User.create!(name: "Rom Com Ron", email: "comron@yahoo.com")
+    @user3 = User.create!(name: "Action Jackson", email: "itsjack@aol.com")
+  end
+  it "should contain a user registration form and test uniqueness of email" do
+    visit "/register"
+
+    expect(page).to have_field("Name")
+    fill_in "Name", with: "PopCorn"
+    expect(page).to have_field("Email")
+
+    fill_in "Email", with: "popcornfool@gmail.com"
+
+    expect(page).to have_button("Register")
+    click_button "Register"
+    expect(current_path).to eq(user_path(User.last))
+    
+    visit "/register"
+
+    expect(page).to have_field("Name")
+    fill_in "Name", with: "PopCorn"
+
+    expect(page).to have_field("Email")
+    fill_in "Email", with: "beans"
+
+    click_button "Register"
+
+    expect(page).to have_content("Data Invalid")
+
+    fill_in "Email", with: "popcornfool@gmail.com"
+
+    click_button "Register"
+    
+    expect(page).to have_content("Data Invalid")
+  end
+end
