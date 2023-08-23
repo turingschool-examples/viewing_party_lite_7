2cdf0c7fa40578cb2146228f38fef0b851e0c2cb
Kaina Cockett <kainacockett@gmail.com>
2023-08-23 17:36:51 -0600

    feat: partial.  need to create movie find method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,5 +10,8 @@ class MoviesController < ApplicationController
   end
   def show
     @user = User.find(params[:user_id])
+    tmdb_service = TmdbService.new
+    @movie = tmdb_service.find_movie(params[:id])
+    # Next step:  Find movie by ID
   end
 end
