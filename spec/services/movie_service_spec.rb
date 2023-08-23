fbfd59fc808232d6694b285cf6db0951931bca8e
Nick Sacco <nicksacco10@gmail.com>
2023-08-23 09:57:23 -0700

    test: complete movie service tests for api call

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe MovieService do
   describe 'class methods' do
-    response = MovieService.top_rated_movies
-    @movies = response[:results]
-
-    expect(@movies).to be_an(Array)
-    expect(@movies.count).to eq(20)
+    it '.top_rated_movies' do
+      response = MovieService.top_rated_movies
+      
+      expect(response).to be_a Hash
+      expect(response[:results]).to be_an Array
+      
+      response[:results].each do |movie|
+        expect(movie).to have_key :original_title
+        expect(movie[:original_title]).to be_a String
+        expect(movie).to have_key :vote_average
+        expect(movie[:vote_average]).to be_a Float
+      end
+    end
   end
 end
