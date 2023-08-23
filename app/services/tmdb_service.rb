63c05a4e7a2d7de3059c8afb8108fb8cfb7d1caa
Parker Boeing <parker.boeing@gmail.com>
2023-08-23 15:20:43 -0600

    wip: Attempting to consume api

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -1,11 +1,11 @@
 class TmdbService
   def self.top_rated_movies
-    response = conn.get("/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}?limit=20")
+    response = conn.get("/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}&page=1")
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.search_movies(query)
-    response = conn.get("/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&query=#{query}?limit=20")
+    response = conn.get("/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&query=#{query}&page=1")
     JSON.parse(response.body, symbolize_names: true)
   end
 
