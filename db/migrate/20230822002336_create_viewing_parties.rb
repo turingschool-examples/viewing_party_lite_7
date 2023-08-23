272d3df4752846fd7e84f1fb54e34c91b36c7893
Ian Lyell <ilyell23@turing.edu>
2023-08-23 10:36:29 -0500

    Update 20230822002336_create_viewing_parties.rb
    Change movie_id to bigint from string.

--- db/migrate/20230822002336_create_viewing_parties.rb
+++ db/migrate/20230822002336_create_viewing_parties.rb
@@ -2,7 +2,7 @@ class CreateViewingParties < ActiveRecord::Migration[7.0]
   def change
     create_table :viewing_parties do |t|
       t.string :date
-      t.string :movie_id
+      t.bigint :movie_id
       t.timestamps
     end
   end
