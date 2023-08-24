7158976e6c1bea198e6561f7c7424ac86384caff
Dani Wilson <danirae.powell@gmail.com>
2023-08-23 20:39:10 -0600

    begin adding facades
    Co-authored-by: Bradley <bmilton2412@gmail.com>

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,2 +1,16 @@
 class MovieFacade
+  def initialize(movie)
+    @movie = movie
+  end
+
+  def top_twenty
+    service = MovieService.new
+
+    json = service.top_20_flicks(@movie)
+
+    @top_twenty = json[:results].map do |top_twenty_data|
+      TopTwenty.new(top_twenty_data)
+    end
+  end
+end
   
