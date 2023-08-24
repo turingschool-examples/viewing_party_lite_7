685a76457208a76cd46e9ecfbdb946e5e0646137
Anna Wiley <awiley33@gmail.com>
2023-08-24 16:34:45 -0500

    test: Finish testing for Movies Results Page> > Co-authored-by: Anna Wiley <awiley33@gmail.com> Co-authored-by: Ian Lyell <ilyell23@turing.edu>

--- spec/facades /search_facade_spec.rb
+++ spec/facades /search_facade_spec.rb
@@ -1,14 +1,39 @@
 require 'rails_helper'
 
-describe MovieService do
+describe SearchFacade do
     context "instance methods" do
         it "returns top 20 Movies", :vcr do
-            service = MovieService.new
-            facade = MovieFacade.new(service.top_rated)
+            params = { search: "top_rated" }
+            facade = SearchFacade.new(params)
 
             facade.top_movies.each do |movie|
                 expect(movie).to be_a Movie
             end
+        end
+
+        it "returns 20 movies from a keyword query", :vcr do
+            params = { search: "Holiday" }
+            facade = SearchFacade.new(params)
+
+            facade.movie_query(params[:search]).each do |movie|
+                expect(movie).to be_a Movie
+            end
+        end
+        it "determines search type by params and searches accordingly", :vcr do
+            params_1 = { search: "top_rated" }
+            params_2 = { search: "Holiday" }
+
+            facade_1 = SearchFacade.new(params_1)
+            facade_1.search_type.each do |movie|
+                expect(movie).to be_a Movie
+            end
+
+            facade_2 = SearchFacade.new(params_2)
+            facade_2.search_type.each do |movie|
+                expect(movie).to be_a Movie
+            end
+
+            expect(facade_1).to_not eq(facade_2)
         end
     end
 end
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -24,5 +24,27 @@ describe MovieService do
                 expect(movie[:popularity]).to be_a(Float)
             end
         end
+        context "#movie_query" do
+            it "returns 20 movies from a keyword query", :vcr do
+                discover = MovieService.new.movie_query("Holiday")
+
+                expect(discover).to be_a Hash
+                expect(discover[:results]).to be_an Array
+
+                movie = discover[:results].first
+
+                expect(movie).to have_key :title
+                expect(movie[:title]).to be_a(String)
+        
+                expect(movie).to have_key :original_title
+                expect(movie[:original_title]).to be_a(String)
+        
+                expect(movie).to have_key :overview
+                expect(movie[:overview]).to be_a(String)
+        
+                expect(movie).to have_key :popularity
+                expect(movie[:popularity]).to be_a(Float)
+            end
+        end
     end
 end
