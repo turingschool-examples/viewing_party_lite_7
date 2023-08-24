f6f746a087a1f8a9f8d9a6881b35a89bd24754d8
Anna Wiley <awiley33@gmail.com>
2023-08-23 22:47:39 -0500

    feat: Add some functionality for Movie Results Page > > Co-authored-by: Anna Wiley <awiley33@gmail.com> Co-authored-by: Ian Lyell <ilyell23@turing.edu>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,9 +1,10 @@
 class Movie
 
-  attr_reader :title, :vote_average
+  attr_reader :title, :vote_average, :id
 
   def initialize(movie_params)
     @title = movie_params[:title]
     @vote_average = movie_params[:vote_average]
+    @id = movie_params[:id]
   end
 end
