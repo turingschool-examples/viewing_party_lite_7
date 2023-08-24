22c7054a38df9510b000ac26efb29620d7f1eb97
Connor Richmond <richmond.connor@gmail.com>
2023-08-24 19:59:36 -0400

    feat/fix: new viewing party

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -2,6 +2,20 @@ class ViewingPartiesController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @viewing_party = @user.viewing_parties.new
-    @movie = Movie.find(params[:movie_data])
+    @movie = params[:movie_data]
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @viewing_party = @user.viewing_parties.new
+    @movie = params[:movie_data]
+
+    require 'pry'; binding.pry
+    if params[:duration_minutes].to_i < @movie[:runtime].to_i
+      flash[:error] = "Duration cannot be less than runtime - #{@movie[:runtime]} minutes"
+      render :new
+    else
+      @viewing_party.save
+    end
   end
 end
