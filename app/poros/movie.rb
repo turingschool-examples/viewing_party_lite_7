68ff78210b0607a47b5aef2d023bca0f24356575
Nick Sacco <nicksacco10@gmail.com>
2023-08-23 18:58:08 -0700

    feat: movie poro updated wip

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -3,11 +3,17 @@
 class Movie
   attr_reader :id,
               :title,
-              :vote_average
+              :vote_average,
+              :runtime,
+              :genres,
+              :summary
 
   def initialize(movie_data) ## < -- movie_data is coming in from the Movie Facade
     @id = movie_data[:id]
     @title = movie_data[:original_title]
     @vote_average = movie_data[:vote_average]
+    @runtime = movie_data[:runtime]
+    @genres = movie_data[:genres]
+    @summary = movie_data[:summary]
   end
 end
--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -4,16 +4,6 @@ require 'rails_helper'
 
 RSpec.describe Movie do
   describe 'creates movie object' do
-    it 'has attributes' do
-      movie = Movie.new({ id: 1, original_title: 'The Movie', vote_average: 10.58 })
-
-      expect(movie).to be_a Movie
-      expect(movie.title).to be_a String
-      expect(movie.title).to eq('The Movie')
-      expect(movie.vote_average).to be_a Float
-      expect(movie.vote_average).to eq(10.58)
-    end
-
     it 'initializes with attributes' do
       movie = Movie.new({ original_title: "Jarhead",
                           vote_average: 6.617,
