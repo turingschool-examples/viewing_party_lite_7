2204583b0b1b567505aceea3e32f53fce1c7e33d
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-24 13:35:18 -0400

    Test: adds tests

--- spec/services/movies_service_spec.rb
+++ spec/services/movies_service_spec.rb
@@ -1,26 +1,36 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe MoviesService do
-  # set up VCR?
-  xit "can connect to movie db" do
-    response = MoviesService.new.connection
+  describe 'instance methods' do
+    it 'can connect to movie db', :vcr do
+      service = MoviesService.new
+      response = service.top_rated
 
-    # expect(response).to be_successful
+      expect(response).to be_an(Array)
+      expect(response.first).to have_key('title')
+      expect(response.first).to have_key('vote_average')
+      expect(response.first).to have_key('id')
+    end
   end
 
-  it "can retrieve top 20 movies" do
-    movies = MoviesService.new.top_rated 
-    
+  it 'can retrieve top 20 movies', :vcr do
+    movies = MoviesService.new.top_rated
+
     expect(movies).to be_an(Array)
     expect(movies.count).to eq(20)
   end
 
-  it "can search for movies by keyword in title" do
+  it 'can search for movies by keyword in title', :vcr do
+    movies = MoviesService.new.search('Inception')
 
+    expect(movies).to be_an(Array)
+    expect(movies.first).to have_key('title')
   end
 
-  it "can generate a poro for a specific movie" do
+  it 'can generate a poro for a specific movie', :vcr do
     movie = MoviesService.new.find_movie(234)
-    
+
+    expect(movie).to be_a(Movie)
+    expect(movie.title).to_not be_nil
   end
 end
