49cf003095431388045c248bd6f39f0a9f6518c1
Dani Wilson <danirae.powell@gmail.com>
2023-08-23 20:41:10 -0600

    add fixtures and test data and tests
    Co-authored-by: Bradley <bmilton2412@gmail.com>

---
+++ spec/features/results_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "the movie results page" do
+  before(:each) do
+    @user3 = User.create!(name: "Action Jackson", email: "itsjack@aol.com")
+    json_response = File.read("spec/fixtures/top_20.json")
+    stub_request(:get, "https://themoviedb.org/movie?q=top%20rated").
+    with(
+      headers: {
+      'X-Api-Key'=> ENV["TMDB_API_KEY"]
+      }).
+    to_return(status: 200, body: json_response)
+  end
+  # When I visit the discover movies page,
+  # and click on either the Top Movies button or the Search button,
+  # I should be taken to the movies results page (users/:user_id/movies) where I see:
+  
+  # Title (As a Link to the Movie Details page)
+  # Vote Average of the movie
+  # Details: There should only be a maximum of 20 results. The above details should be listed for each movie.
+  
+  # I should also see a button to return to the Discover Page.
+  it "displays the movie title as a link to the movie details page" do
+    visit "/users/#{@user3.id}/movies"
+
+    expect(page).to have_link("Elemental")
+  end
+end
