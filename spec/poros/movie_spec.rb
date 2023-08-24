bffc8f13abf7008c02dd565097f5673d3511aa15
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-24 13:35:07 -0400

    Test: adds tests to poros test file

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -1,56 +1,47 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Movie do
-  #set up VCR and change before block 
+  # set up VCR and change before block
   before do
     connection = MoviesService.new.connection
-    response = connection.get("3/movie/234") do |f|
-      f.params["append_to_response"] = "reviews,credits"
+    response = connection.get('3/movie/234') do |f|
+      f.params['append_to_response'] = 'reviews,credits'
     end
     @movie = Movie.new(JSON.parse(response.body))
   end
 
-  it "has an id" do
-
+  it 'has an id' do
   end
 
-  it "has a title" do
-
+  it 'has a title' do
+    expect(@movie.title).to eq('The Cabinet of Dr. Caligari')
   end
 
-  it "has a vote average" do
-
+  it 'has a vote average' do
+    expect(@movie.rating).to eq(7.955)
   end
 
-  it "has a runtime" do
-
+  it 'has a runtime' do
   end
 
-  it "has genres" do
-
+  it 'has genres' do
   end
 
-  it "can format genres" do
-
+  it 'can format genres' do
   end
 
-  it "has a summary" do
-
+  it 'has a summary' do
   end
 
-  it "has an array of ten cast members" do
-
+  it 'has an array of ten cast members' do
   end
 
-  it "formats cast members into an array with maximum 10" do
-
+  it 'formats cast members into an array with maximum 10' do
   end
 
-  it "can count total reviews" do
-
+  it 'can count total reviews' do
   end
 
-  it "stores reviews with author, rating, and comments" do
-
+  it 'stores reviews with author, rating, and comments' do
   end
 end
