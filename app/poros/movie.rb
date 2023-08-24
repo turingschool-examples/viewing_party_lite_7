2fe8698d573f9c509dfab9a3efe9df9a96c99d0d
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-24 13:27:11 -0400

    Feat: updates movie poros

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,3 +1,9 @@
 class Movie
+  attr_reader :title, :rating, :id
 
+  def initialize(attributes)
+    @title = attributes['title']
+    @rating = attributes['vote_average']
+    @id = attributes['id']
+  end
 end
