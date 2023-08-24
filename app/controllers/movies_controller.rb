11ffea843279dfe91f222c203ada67a551cc863c
Dani Wilson <danirae.powell@gmail.com>
2023-08-23 20:38:55 -0600

    messed with search action in controller
    Co-authored-by: Bradley <bmilton2412@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,25 @@
 class MoviesController < ApplicationController
   def index
     @user = User.find(params[:id])
+    @movie_facade = MovieFacade.new(params[:movie])
   end
 
   def search
+    if params[:search].blank?
+      redirect_to "/users/#{@user.id}/discover" and return
+    else
+      movie_search
+    end
+  end
+
+  # def search
     # if params[:search].blank?
     #   redirect_to "/users/#{@user.id}/discover" and return
     # else
     #   @parameter = params[:search].downcase
     #   @results = Movies.all.where("lower(title) LIKE :search", search: "%#{@parameter}%")
     # end
-  end
+  # end
 
 
 end
