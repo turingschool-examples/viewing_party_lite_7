e338ec5b686872db70f1354d42f9f9bc12df9c1b
Artemy Gibson <artemygibson@gmail.com>
2023-08-24 13:48:17 -0700

    Create file for movies facade (commented out for now)

---
+++ app/facades/movies_facade.rb
@@ -0,0 +1,12 @@
+# class MoviesFacade 
+#   def initialize(something)
+#     @something = collect "cached" movies here?
+#   end
+
+  # def find_movie(id)
+  #   search through cached movies to see if already generated, 
+  #   or else create that movie through hitting api
+  # end
+# end
+
+# need to test this
