ab2e5c4fb59aa4f84a4b3fcb5a4a84c2b8780f98
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-24 13:28:05 -0400

    Feat/Refactor: updates search method and applies rubocop to other methods

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -1,22 +1,25 @@
-class MoviesService 
-  def connection 
-    connection = Faraday.new("https://api.themoviedb.org/") do |faraday|
-      faraday.params["api_key"] = ENV["MOVIE_API_KEY"]
+class MoviesService
+  def connection
+    connection = Faraday.new('https://api.themoviedb.org/') do |faraday|
+      faraday.params['api_key'] = ENV['MOVIE_API_KEY']
     end
   end
 
   def search(keyword)
-
+    response = connection.get('3/search/movie') do |faraday|
+      faraday.params['query'] = keyword
+    end
+    JSON.parse(response.body)['results']
   end
 
   def top_rated
-    response = connection.get("3/movie/top_rated")
-    JSON.parse(response.body)["results"]
+    response = connection.get('3/movie/top_rated')
+    JSON.parse(response.body)['results']
   end
 
   def find_movie(id)
     response = connection.get("3/movie/#{id}") do |f|
-      f.params["append_to_response"] = "reviews,credits"
+      f.params['append_to_response'] = 'reviews,credits'
       # makes all three calls at once
       # Includes the details, reviews, and credits in one response
     end
