133e4ecc662ab2c4251914ef154fff0fb654818f
Anna Wiley <awiley33@gmail.com>
2023-08-23 22:46:59 -0500

    test: Add some testing for Movie Results Page > > Co-authored-by: Anna Wiley <awiley33@gmail.com> Co-authored-by: Ian Lyell <ilyell23@turing.edu>

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -4,11 +4,25 @@ RSpec.describe "Movies Results Page" do
   before do
     create_list(:user, 1)
     @user = User.first
-
   end
   
   it "has a title", :vcr do
     visit user_movies_path(@user)
     expect(page).to have_content("Movies Results Page")
+  end
+
+  it "displays top rated movies when queried for", :vcr do
+    visit "users/#{@user.id}/movies?q=top-rated"
+
+    expect(page).to have_css(".movie", count: 20)
+
+    within(first(".movie")) do
+      expect(page).to have_css(".title")
+      expect(page).to have_css(".vote_average")
+    end
+  end
+
+  it "displays movies which match keyword search" do
+    
   end
 end
