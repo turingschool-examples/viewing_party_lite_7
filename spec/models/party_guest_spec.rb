a05692c4a16107038ad19e084cf9bb5db6c6e396
Artemy Gibson <artemygibson@gmail.com>
2023-08-24 18:20:19 -0700

    Add testing for party guest model

---
+++ spec/models/party_guest_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper" 
+
+RSpec.describe PartyGuest do
+  describe "validatons" do
+    it { should validate_presence_of :host }
+  end
+
+  describe "relationships" do
+    it { should belong_to :user }
+    it { should belong_to :viewing_party}
+  end
+end
