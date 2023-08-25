e985dd3402a3c87899aba7aaeb8734a1fa80b7a0
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-24 21:08:24 -0400

    Fix: adds code to handle null values not present in top movies or movie search result

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -9,21 +9,22 @@ class Movie
               :total_reviews,
               :reviews,
               :image
+
   def initialize(data)
     @id = data[:id]
     @title = data[:original_title]
     @image = data[:backdrop_path]
-    @rating= data[:vote_average]
+    @rating = data[:vote_average]
     @runtime = data[:runtime]
-    @genres = format_genres(data[:genres])
+    @genres = data[:genres] ? format_genres(data[:genres]) : 'Unknown'
     @summary = data[:overview]
-    @cast = generate_cast(data[:credits][:cast])
-    @total_reviews = data[:reviews][:total_results]
-    @reviews = generate_reviews(data[:reviews][:results])
+    @cast = data.dig(:credits, :cast) ? generate_cast(data[:credits][:cast]) : []
+    @total_reviews = data.dig(:reviews, :total_results) || 0
+    @reviews = data.dig(:reviews, :results) ? generate_reviews(data[:reviews][:results]) : []
   end
 
   def format_genres(genres)
-    genres.map { |genre| genre[:name] }.join(", ")
+    genres.map { |genre| genre[:name] }.join(', ')
   end
 
   def generate_cast(cast)
@@ -31,11 +32,10 @@ class Movie
   end
 
   def generate_reviews(reviews)
-    reviews.map { |review| Review.new(review)}
+    reviews.map { |review| Review.new(review) }
   end
 
   def format_runtime
-    "#{@runtime / 60}hr #{@runtime % 60 }min"
+    "#{@runtime / 60}hr #{@runtime % 60}min"
   end
-
 end
