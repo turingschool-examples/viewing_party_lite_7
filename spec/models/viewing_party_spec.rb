61c53882375b75a6118c07c996ce3d279c29a00d
Artemy Gibson <artemygibson@gmail.com>
2023-08-24 19:33:24 -0700

    Removes validation test for user and tests for party_guests

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 RSpec.describe ViewingParty, :vcr do
   describe "validations" do
-    it { should belong_to :user }
+    it { should have_many(:users).through(:party_guests) }
+    it { should have_many :party_guests }
     it { should validate_presence_of :movie_id }
     it { should validate_presence_of :duration}
     it { should validate_numericality_of :duration } 
@@ -11,15 +12,18 @@ RSpec.describe ViewingParty, :vcr do
     it "validates duration has a minimum value of the movie runtime" do
       ally = User.create!(name: "Ally Jean", email: "allyjean@example.com")
       movie = MoviesService.new.find_movie(234)
-      expect(ViewingParty.create(user: ally, movie_id: 234, duration: 77, party_date: Date.today, start_time: Time.now)).to_not be_valid
-      expect(ViewingParty.create(user: ally, movie_id: 234, duration: 78, party_date: Date.today, start_time: Time.now)).to be_valid
+      expect(ViewingParty.create(movie_id: 234, duration: 77, party_date: Date.today, start_time: Time.now)).to_not be_valid
+      expect(ViewingParty.create(movie_id: 234, duration: 78, party_date: Date.today, start_time: Time.now)).to be_valid
     end
   end
 
   it "can report movie duration" do
-    ally = User.create!(name: "Ally Jean", email: "allyjean@example.com")
     movie = MoviesService.new.find_movie(234)
-    party = ViewingParty.create!(user: ally, movie_id: 234, duration: 78, party_date: Date.today, start_time: Time.now)
+    party = ViewingParty.create!(movie_id: 234, duration: 78, party_date: Date.today, start_time: Time.now)
     expect(party.movie_duration).to eq(78)
+  end
+
+  it "can send invites" do
+
   end
 end
