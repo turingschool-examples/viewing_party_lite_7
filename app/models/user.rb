50bc8028f3be5800b4d864a395b10b57ac965c17
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-25 17:10:38 -0400

    Refactor: adds hosted_parties and invited_parties methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   validates :email, uniqueness: { case_sensitive: false }
   has_many :party_guests
   has_many :viewing_parties, through: :party_guests
+
+  def hosted_parties
+    viewing_parties.joins(:party_guests).where('party_guests.user_id = ? AND party_guests.host = ?', id, true).distinct
+  end
+
+  def invited_parties
+    viewing_parties.joins(:party_guests).where('party_guests.user_id = ? AND party_guests.host = ?', id,
+                                               false).distinct
+  end
 end
