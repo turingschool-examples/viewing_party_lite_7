bc901ba149a20700d2995565328cd59bf9af1830
Artemy Gibson <artemygibson@gmail.com>
2023-08-25 09:59:35 -0700

    Add private methods and flash alerts for create

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -5,19 +5,23 @@ class ViewingPartyController < ApplicationController
   end
 
   def create
-    party = ViewingParty.new(
-      duration: params[:duration],
-      party_date: params[:start_date],
-      start_time: params[:start_time],
-      movie_id: params[:movie_id]
-    )
+    party = ViewingParty.new(party_params)
     if party.save
-      party.send_invites(party, params)
-      flash[:success] = 'Party Created Successfully'
+      party.send_invites(params[:user_id], guest_hash)
       redirect_to user_path(params[:user_id])
+      flash[:success] = 'Party Created Successfully'
     else
       redirect_to new_user_movie_viewing_party_path(params[:user_id], params[:movie_id])
-      # flash alert?
+      flash[:alert] = "Error: #{party.errors.full_messages.to_sentence}"
     end
+  end
+
+  private
+  def party_params
+    params.permit(:duration, :party_date, :start_time, :movie_id)
+  end
+
+  def guest_hash
+    params[:guests].permit!.to_h
   end
 end
