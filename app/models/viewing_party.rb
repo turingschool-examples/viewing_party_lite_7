ecb48c4b39e87f1e70716bb2cc3e224e38771baf
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-25 17:13:02 -0400

    Refactor: adds movie_image_url and movie_title_link methods

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,19 @@
 class ViewingParty < ApplicationRecord
+  include Rails.application.routes.url_helpers
   has_many :party_guests
   has_many :users, through: :party_guests
+
+  def movie_image_url(movie_images)
+    if movie_images[movie_id].present?
+      "https://image.tmdb.org/t/p/w500#{movie_images[movie_id]}"
+    else
+      'path/to/your/default/image.jpg'
+    end
+  end
+
+  def movie_title_link(movies, user)
+    movie_title = movies[movie_id]
+    movie_link = user_movie_show_path(user_id: user.id, movie_id:)
+    [movie_title, movie_link]
+  end
 end
