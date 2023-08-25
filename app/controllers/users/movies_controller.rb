b1f7636a78480fcce263e0ff6da4d863f039f39f
Gabriel Torres <thomastgtorres21@gmail.com>
2023-08-24 21:20:06 -0500

    complete user story 11

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -17,6 +17,7 @@ class Users::MoviesController < ApplicationController
 
   def show 
     @user = params[:user_id]
+
     conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
       faraday.params["api_key"] = ENV["MOVIES_API_KEY"]
     end
@@ -27,6 +28,30 @@ class Users::MoviesController < ApplicationController
     movies = data[:results]
     found_movies = movies.find_all {|m| m[:id] == params[:id].to_i}
     @movie = found_movies.first
+#-----------------cast--------------------
+    conn2 = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = ENV["MOVIES_API_KEY"]
+    end
+
+    response2 = conn2.get("/3/movie/#{@movie[:id]}/credits")
+    data2 = JSON.parse(response2.body, symbolize_names: true)
+    @cast = data2[:cast].take(10)
+  #-----------------reviews--------------------
+  conn3 = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+    faraday.params["api_key"] = ENV["MOVIES_API_KEY"]
+  end
+
+  response3 = conn3.get("/3/movie/#{@movie[:id]}/reviews")
+  data3 = JSON.parse(response3.body, symbolize_names: true)
+  @reviews = data3[:results].take(10)
+  @total_reviews = data[:total_results]
+  #-----------------details--------------------
+  conn4 = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+    faraday.params["api_key"] = ENV["MOVIES_API_KEY"]
+  end
+
+  response4 = conn4.get("/3/movie/#{@movie[:id]}")
+  @data4 = JSON.parse(response4.body, symbolize_names: true)
   end
 
   def search 
