4d637f41d5a91e5b60837073bd9e8de1d4e0851c
Bradley Milton <bmilton2412@gmail.com>
2023-08-25 13:01:32 -0700

    add movie attributes and genres method
    Co-authored-by: Dani <danirae.powell@gmail.com>

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -4,22 +4,38 @@ class Movie
               :vote_average,
               :runtime,
               :genres,
-              :summary
+              :summary,
+              :cast,
+              :total_review_count,
+              :reviewer
 
 
   def initialize(movie_data)
-  #   require 'pry'; binding.pry
     @id = movie_data[:id]
     @title = movie_data[:title]
     @vote_average = movie_data[:vote_average]
     @runtime = movie_data[:runtime]
-    @genres = movie_data[:genres]
+    @genres = genre_parse(movie_data[:genres])
     @summary = movie_data[:overview]
     @cast = movie_data[:cast]
     @total_review_count = movie_data[:total_results]
     @reviewer = movie_data[:results]
-    # @reviewer_info = movie_data[:reviewer_info]
+    @reviewer_info = movie_data[:results]
   end
 end
 
-# <li>Reviewer Information: <%= @movie.reviewer_info %></li>
+def genre_parse(movie_data)
+  return "" if movie_data.nil?
+
+  genres = movie_data.map { |hash| hash[:name] }
+  genres.join(", ")
+end
+
+# def format_time(movie_data)
+#   hours = movie_data / 60
+#   minutes = movie_data % 60
+# end
+
+# def formatted_runtime
+#   "#{@runtime / 60}h #{@runtime % 60}min"
+# end
