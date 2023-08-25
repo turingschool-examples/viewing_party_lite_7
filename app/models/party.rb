ced7bebdc5e84d7c5e11da8b147f2bbaf7552b32
Nick Sacco <nicksacco10@gmail.com>
2023-08-25 11:18:01 -0700

    feat/test: add validations to party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,4 +3,8 @@
 class Party < ApplicationRecord
   has_many :party_users
   has_many :users, through: :party_users
+
+  validates :date , presence: true
+  validates :time , presence: true
+  validates :duration , numericality: { only_integer: true }
 end
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Party, type: :model do
     it { should have_many(:party_users) }
     it { should have_many(:users).through(:party_users) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:time) }
+    it { should validate_numericality_of(:duration).only_integer }
+  end
 end
