8090b03829d5e05180d9b3f555a7b6b2244ad0de
Artemy Gibson <artemygibson@gmail.com>
2023-08-24 21:38:57 -0700

    Add create method

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -3,4 +3,21 @@ class ViewingPartyController < ApplicationController
     @user = User.find(params[:user_id])
     @movie = MoviesService.new.find_movie(params[:movie_id])
   end
+
+  def create
+    party = ViewingParty.new(
+      duration: params[:duration],
+      party_date: params[:start_date],
+      start_time: params[:start_time],
+      movie_id: params[:movie_id]
+    )
+    if party.save
+      party.send_invites(party, params)
+      flash[:success] = 'Party Created Successfully'
+      redirect_to user_path(params[:user_id])
+    else
+      redirect_to new_user_movie_viewing_party_path(params[:user_id], params[:movie_id])
+      # flash alert?
+    end
+  end
 end
