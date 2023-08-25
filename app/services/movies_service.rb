e87de8b648601845eef598032fbbd8a4c51f6ab8
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-24 21:29:21 -0400

    Fix: updates code to use merge conflict symbols

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -9,17 +9,17 @@ class MoviesService
     response = connection.get('3/search/movie') do |faraday|
       faraday.params['query'] = keyword
     end
-    JSON.parse(response.body)['results']
+    JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
   def top_rated
-    response = connection.get("3/movie/top_rated")
-    JSON.parse(response.body, symbolize_names: true)["results"]
+    response = connection.get('3/movie/top_rated')
+    JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
   def find_movie(id)
     response = connection.get("3/movie/#{id}") do |f|
-      f.params["append_to_response"] = "reviews,credits"
+      f.params['append_to_response'] = 'reviews,credits'
     end
     Movie.new(JSON.parse(response.body, symbolize_names: true))
   end
