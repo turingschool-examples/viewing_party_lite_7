06b5a8c91ac7016e2652b822c88ca4b9ead45fb5
Dani Wilson <danirae.powell@gmail.com>
2023-08-24 21:00:47 -0600

    add index action
    Co-authored-by: Bradley <bmilton2412@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,10 +2,10 @@ class MoviesController < ApplicationController
   def index
     @user = User.find(params[:id])
     @movies = 
-    # if params[:search].present?
-    #   MoviesFacade.movies_by_keyword(params[:search])
-    # else
-      MoviesFacade.top_twenty_movies
-    # end
+    if params[:search].present?
+      MoviesFacade.movies_by_keyword(params[:search])
+    else
+      MoviesFacade.top_twenty_movies[0..19]
+    end
   end
 end
