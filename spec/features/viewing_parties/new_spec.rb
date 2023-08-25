1bb4f27bedfaacab827fedad56f1fac231a71420
Ethan Black <ethanrossblack@gmail.com>
2023-08-25 12:37:34 -0600

    test: refactor new_viewing_party tests

--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe "New Viewing Party Page" do
   before do
     VCR.use_cassette("godfather-movie-details") do
-      @user1 = create(:user)
-      @user2 = create(:user)
-      @user3 = create(:user)
+      @user1 = User.create(name: "Ethan", email: "ethan@ethan.com")
+      @user2 = User.create(name: "Chris", email: "chris@chris.com")
+      @user3 = User.create(name: "Vito", email: "vito@corleone.com")
 
       @movie = MoviesFacade.new(238, nil).movie_details
 
@@ -15,28 +15,26 @@ describe "New Viewing Party Page" do
 
   describe "new viewing party form" do
     it "displays the movie title above a form" do
-      within("#viewing-party-details") do
-        expect(page).to have_content(@movie.title)
-      end
+      expect(page).to have_content(@movie.title)
     end
 
-    xit "has a 'Duration of Party' field prefilled with the movie's runtime in minutes" do
+    it "has a 'Duration of Party' field prefilled with the movie's runtime in minutes" do
       expect(page).to have_field("Duration of Party", with: @movie.runtime)
     end
 
-    xit "has date and time selection fields" do
+    it "has date and time selection fields" do
       expect(page).to have_field("When", type: "date")
       expect(page).to have_field("Start Time", type: "time")
     end
 
-    xit "has individual checkboxes to invite every other existing user" do
-      expect(page).to have_checked_field("#{@user2.name} (#{@user2.email})")
-      expect(page).to have_checked_field("#{@user3.name} (#{@user3.email})")
+    it "has individual checkboxes to invite every other existing user" do
+      expect(page).to have_unchecked_field("#{@user2.name} (#{@user2.email})")
+      expect(page).to have_unchecked_field("#{@user3.name} (#{@user3.email})")
 
       expect(page).to_not have_checked_field("#{@user1.name} (#{@user1.email})")
     end
 
-    xit "has a button to create a party" do
+    it "has a button to create a party" do
       expect(page).to have_button("Create Party").once
     end
   end
