687d93994e3f749f22a690b17ae0ef20a59c1cc9
Allen Russell <garyrusselljr@gmail.com>
2023-08-24 19:08:20 -0600

    wip: viewing_parties new.html hidden field tag and create action

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -8,14 +8,16 @@ class ViewingPartiesController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @viewing_party = @user.viewing_parties.new
-    @movie = params[:movie_data]
+    runtime = params[:runtime]
 
-    require 'pry'; binding.pry
-    if params[:duration_minutes].to_i < @movie[:runtime].to_i
-      flash[:error] = "Duration cannot be less than runtime - #{@movie[:runtime]} minutes"
-      render :new
-    else
+    if params[:duration_minutes].to_i >= runtime.to_i
+
       @viewing_party.save
+   
+      redirect_to "/users/#{@user.id}"
+    else
+      flash[:error] = "Duration cannot be less than runtime - #{runtime} minutes"
+      # render :new, locals: { movie_data: params[:movie_data] }
     end
   end
 end
