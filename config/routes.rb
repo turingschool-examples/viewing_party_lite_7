a7c5bf24f1e4ecc94ecfb935796081a0839a11a2
Dani Wilson <danirae.powell@gmail.com>
2023-08-25 17:17:32 -0600

    edit styling and spacing
    Co-authored-by: Bradley <bmilton2412@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   post "/register", to: "users#create"
 
   get "/users/:id/discover", to: "discover#index"
+
   get "/users/:id/movies", to: "movies#index"
   get "/users/:id/movies", to: "movies#search"
   get "/users/:id/movies/:movie_id", to: "movies#show"
+  
   get "/users/:id/movies/:movie_id/viewing_party", to: "viewing_parties#new"
   post "/users/:id/movies/:movie_id/viewing_party", to: "viewing_parties#create"
 end
