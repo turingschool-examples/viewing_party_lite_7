006ba432a536b4183a9dbf3f29a592df9c882781
Connor Richmond <richmond.connor@gmail.com>
2023-08-25 15:50:48 -0400

    feat: email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :movies
   has_many :users_viewing_parties
   has_many :viewing_parties, through: :users_viewing_parties
+
+  validates :email, presence: true, uniqueness: true
 end
