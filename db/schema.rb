ff69f901e3f45e61ef5960364e2fda1e365fd944
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-08-25 10:05:30 -0600

    user_viewing_party table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_25_143201) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_160102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_viewing_parties", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "viewing_party_id", null: false
+    t.boolean "host"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_viewing_parties_on_user_id"
+    t.index ["viewing_party_id"], name: "index_user_viewing_parties_on_viewing_party_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -29,4 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_25_143201) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_viewing_parties", "users"
+  add_foreign_key "user_viewing_parties", "viewing_parties"
 end
