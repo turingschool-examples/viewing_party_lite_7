8930c9c9d3ddc2f7af5473e43f661f1c8527a517
Ethan Black <ethanrossblack@gmail.com>
2023-08-25 13:36:13 -0600

    feat: create party and assign hosts/invitees method

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -6,6 +6,34 @@ class ViewingPartiesController < ApplicationController
   end
 
   def create
-    # require "pry"; binding.pry
+    party = ViewingParty.new(viewing_party_params)
+    #sad path
+    if party.duration < params[:movie_runtime].to_i
+      flash[:error] = "Error: Viewing party duration must be longer than the movie's runtime"
+      redirect to user_movie_viewing_party_new_path(params[:user_id], params[:user_id])
+    elsif party.save
+      flash[:success] = "Viewing Party successfully created"
+      assign_host(params[:user_id], party.id)
+      assign_guests(params[:invitees], party.id)
+      redirect_to user_path(params[:user_id])
+    end
+  end
+
+
+  def assign_host(user_id, vp_id)
+    ViewingPartyUser.create(user_id: user_id, viewing_party_id: vp_id, hosting?: true)
+  end
+
+  def assign_guests(invitees, vp_id)
+    invitees.flatten.map do |user_id|
+      ViewingPartyUser.create(user_id: user_id, viewing_party_id: vp_id, hosting?: false)
+    end
+  end
+
+
+  private
+
+  def viewing_party_params
+    params.permit(:movie_id, :duration, :date, :start_time)
   end
 end
