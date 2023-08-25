72b721108158f7f21c2af608cf09ec38dfab613e
Bradley Milton <bmilton2412@gmail.com>
2023-08-25 15:49:48 -0700

    movie def show from viewing party controller
    Co-authored-by: Dani <danirae.powell@gmail.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,5 +12,7 @@ class MoviesController < ApplicationController
   def show
     @user = User.find(params[:id])
     @movie = MoviesFacade.movie_by_id(params[:movie_id])
+    @movie1 = MoviesFacade.movies_cast(params[:movie_id])
+    @movie2 = MoviesFacade.movie_reviewed(params[:movie_id])
   end
 end
