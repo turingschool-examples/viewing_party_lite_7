e9361c500e1891aa3a627c51ac344f7463889449
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-25 11:36:16 -0400

    Feat: updates user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email
   validates :email, uniqueness: { case_sensitive: false }
+  has_many :party_guests
+  has_many :viewing_parties, through: :party_guests
 end
