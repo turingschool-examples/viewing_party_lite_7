b9e3286d906d189e8ca702374c52a962f78b1ad5
Dani Wilson <danirae.powell@gmail.com>
2023-08-24 21:01:52 -0600

    add testing
    Co-authored-by: Bradley <bmilton2412@gmail.com>

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -18,5 +18,18 @@ RSpec.describe "the movie results page" do
 
     expect(page).to have_link("The Godfather")
     expect(page).to_not have_link("Elemental")
+    expect(page).to have_link count:21
+  end
+
+  it "can return a list of like names from search" do
+    visit "/users/#{@user3.id}/discover"
+
+    fill_in "Find Movies", with: "Spider"
+
+    click_button "Find Movies"
+
+    expect(current_path).to eq("/users/#{@user3.id}/movies")
+
+    expect(page).to have_content("Spiderman")
   end
 end
