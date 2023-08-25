4501e6323f55302b8666c3000df30db2937d5fb2
Artemy Gibson <artemygibson@gmail.com>
2023-08-24 18:12:59 -0700

    Test relationship to viewing parties

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,6 @@ RSpec.describe User do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should have_many :viewing_parties }
   end
 end
