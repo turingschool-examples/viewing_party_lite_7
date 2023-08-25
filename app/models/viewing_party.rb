b491dd3d958d0a080c7c467cde9fa8867d94ceed
Ian Lyell <ian.lyell@yahoo.com>
2023-08-25 11:00:45 -0500

    db: Add enum to viewing_party_users

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -2,4 +2,7 @@ class ViewingParty < ApplicationRecord
     belongs_to :user
     has_many :viewing_party_users
     has_many :users, through: :viewing_party_users
+    validates_presence_of :date
+    validates_presence_of :movie_id
+    validates_presence_of :user_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_004730) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_155107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_004730) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_viewing_party_users_on_user_id"
     t.index ["viewing_party_id"], name: "index_viewing_party_users_on_viewing_party_id"
   end
