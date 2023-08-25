13b38731c58c66ce41f6b08f56e863c2f1b5a4da
Kaina Cockett <kainacockett@gmail.com>
2023-08-25 14:29:38 -0600

    complete testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,4 @@ class User < ApplicationRecord
 
   has_many :user_parties
   has_many :parties, through: :user_parties
-  
-  def hosted_parties
-    parties.where(user_parties: {host_id: self.id})
-  end
-
-  def invited_parties
-    parties.where(user_parties: {user_id: self.id}).where.not(user_parties: {host_id: self.id})
-  end
-
-  def get_hosted_movies(parties)
-    result = []
-    parties.each do |party|
-      result << party.movie_id
-    end
-    result
-  end
-
-  def get_invited_movies(parties)
-    #
-  end
 end
