096cb171507200cf41ea6dcf4414e1f831f5c23b
Ethan Black <ethanrossblack@gmail.com>
2023-08-25 11:15:53 -0600

    Merge branch 'chris-draft' of github.com:ethanrossblack/viewing_party_lite_7 into 9-new-viewing-party-page-e

---
+++ app/models/viewing_party.rb
@@ -0,0 +1,3 @@
+class ViewingParty < ApplicationRecord
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_222452) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_164358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_222452) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "viewing_parties", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "duration"
+    t.date "date"
+    t.time "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "viewing_party_users", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "viewing_party_id", null: false
+    t.boolean "hosting?", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_viewing_party_users_on_user_id"
+    t.index ["viewing_party_id"], name: "index_viewing_party_users_on_viewing_party_id"
+  end
+
+  add_foreign_key "viewing_party_users", "users"
+  add_foreign_key "viewing_party_users", "viewing_parties"
 end
