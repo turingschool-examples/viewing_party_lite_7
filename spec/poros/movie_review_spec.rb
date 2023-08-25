d25615eee457dc8a44c0aeb41f2a909dcf2f83e0
Parker Boeing <parker.boeing@gmail.com>
2023-08-25 10:23:50 -0600

    feat: Add rubocop .yml file

--- spec/poros/movie_review_spec.rb
+++ spec/poros/movie_review_spec.rb
@@ -5,19 +5,12 @@ RSpec.describe MovieReview do
     it "has attributes" do
       movie_review_attributes = {
         author: "TestUsername",
-        name: "Test",
-        username: "TestUsername",
-        avatar_path: "test/path",
-        rating: 123
-
+        content: "Test content"
       }
       movie_review = MovieReview.new(movie_review_attributes)
 
       expect(movie_review.author).to eq(movie_review_attributes[:author])
-      expect(movie_review.name).to eq(movie_review_attributes[:name])
-      expect(movie_review.username).to eq(movie_review_attributes[:username])
-      expect(movie_review.avatar_path).to eq(movie_review_attributes[:avatar_path])
-      expect(movie_review.rating).to eq(movie_review_attributes[:rating])
+      expect(movie_review.content).to eq(movie_review_attributes[:content])
     end
   end
 end
