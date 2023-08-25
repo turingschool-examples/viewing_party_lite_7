eb97f18f4e9608b1239096e462b9357c89fce68e
Artemy Gibson <artemygibson@gmail.com>
2023-08-24 19:33:03 -0700

    Remove validation for user- not meant to be there

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,5 +1,6 @@
 class ViewingParty < ApplicationRecord
-  belongs_to :user
+  has_many :party_guests
+  has_many :users, through: :party_guests
   validates :movie_id, presence: true
   validates :duration, presence: true, numericality: true, comparison: { greater_than_or_equal_to: :movie_duration}
   validates :party_date, presence: true, timeliness: { on_or_after: Date.today, type: :date }
@@ -7,5 +8,9 @@ class ViewingParty < ApplicationRecord
 
   def movie_duration
     MoviesService.new.find_movie(self.movie_id).runtime
+  end
+
+  def send_invites(params)
+    require 'pry'; binding.pry
   end
 end
