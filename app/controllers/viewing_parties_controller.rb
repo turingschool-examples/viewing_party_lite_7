0dbb65dec56a2d45e9f5de692c09a87f20205146
Ethan Black <ethanrossblack@gmail.com>
2023-08-25 12:41:53 -0600

    feat: add @users variable to new viewing_parties_controller method

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -2,9 +2,10 @@ class ViewingPartiesController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @movie = MoviesFacade.new(params[:movie_id], nil).movie_details
+    @users = User.where.not(id: params[:user_id]).order(:name)
   end
 
   def create
-    
+    # require "pry"; binding.pry
   end
 end
