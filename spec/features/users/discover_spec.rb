86811bf46e93df082324db44bc501a8c25352136
Artemy Gibson <artemygibson@gmail.com>
2023-08-25 16:17:09 -0700

    remove comments

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -25,17 +25,6 @@ RSpec.describe 'User Discover Movies Page', :vcr do
     expect(page).to have_content('Discover Movies')
   end
 
-  # As an user,
-  # When I visit the '/users/:id/discover' path, where :id, is the id of a valid user,
-  # I should see
-
-  #  Button to Discover Top Rated Movies
-  #  A text field to enter keyword(s) to search by movie title
-  #  A Button to Search by Movie Title
-  # Details When the user clicks on the Top Rated Movies OR the search button, they should be taken to the movies results page (more details of this on the Movies Results Page issue.
-
-  # The movies will be retrieved by consuming The MovieDB API
-
   it 'can link to the find top rated movies page' do
     visit user_discover_path(@ally)
     expect(page).to have_button('Find Top Rated Movies')
