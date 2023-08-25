77d2189019a642e504a71fcda3bd8d5396233534
Ethan Black <ethanrossblack@gmail.com>
2023-08-25 09:13:44 -0600

    test: additional new viewing party tests

--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -5,33 +5,49 @@ describe "New Viewing Party Page" do
     @user1 = create(:user)
     @user2 = create(:user)
     @user3 = create(:user)
+
+    @movie = MoviesFacade.new(238, nil).movie_details
   end
 
   describe "new viewing party form" do
-    it "displays the movie title above a form" do
+    xit "displays the movie title above a form" do
       within("#viewing-party-details") do
         expect(page).to have_content(@movie.title)
       end
     end
 
-    it "has a 'Duration of Party' field prefilled with the movie's runtime in minutes" do
-        expect(page).to have_field("Duration of Party", with: @movie.runtime)
+    xit "has a 'Duration of Party' field prefilled with the movie's runtime in minutes" do
+      expect(page).to have_field("Duration of Party", with: @movie.runtime)
     end
 
-    it "has date and time selection fields" do
+    xit "has date and time selection fields" do
       expect(page).to have_field("When", type: "date")
       expect(page).to have_field("Start Time", type: "time")
     end
 
-    it "has individual checkboxes to invite every other existing user" do
+    xit "has individual checkboxes to invite every other existing user" do
       expect(page).to have_checked_field("#{@user2.name} (#{@user2.email})")
       expect(page).to have_checked_field("#{@user3.name} (#{@user3.email})")
 
       expect(page).to_not have_checked_field("#{@user1.name} (#{@user1.email})")
     end
 
-    it "has a button to create a party" do
+    xit "has a button to create a party" do
       expect(page).to have_button("Create Party").once
     end
   end
+
+  describe "happy path" do
+    xit "redirects to the user's dashboard where the new event is shown" do
+      
+    end
+  end
+
+  describe "sad path" do
+    xit "will return an error if the party's duration is less than the film's duration" do
+
+    end
+  end
+
+  
 end
