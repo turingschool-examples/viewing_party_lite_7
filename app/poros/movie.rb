bffd421e7c5925e1887674c9a04bb7f801c72a86
Ethan Black <ethanrossblack@gmail.com>
2023-08-24 18:13:35 -0600

    feat: movie PORO (incomplete)

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,8 +1,32 @@
 class Movie
-  attr_reader :title, :rating
+  attr_reader :genres,
+              :id,
+              :rating,
+              :runtime,
+              :summary,
+              :title
+
 
   def initialize(info)
-    @title = info[:title]
-    @rating = info[:vote_average]
+    @cast_data   = info[:credits][:cast]
+    @genres      = info[:genres].map { |genre| genre[:name] }
+    @id          = info[:id]
+    @rating      = info[:vote_average]
+    @reviews_data = info[:reviews][:results]
+    @runtime     = info[:runtime]
+    @summary     = info[:overview]
+    @title       = info[:title]
+  end
+
+  def cast_list
+    @cast_data.first(10).map do |cast_member_data|
+      CastMember.new(cast_member_data)
+    end
+  end
+
+  def reviews
+    @reviews_data.first(10).map do |review_data|
+      Review.new(review_data)
+    end
   end
 end
