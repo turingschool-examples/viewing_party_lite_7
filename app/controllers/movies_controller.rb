93271f50d5e6f9999d948bac3b41131553bedee1
Chris Cullinane <chriscullinane@protonmail.com>
2023-08-24 17:29:28 -0800

    adds show to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,10 @@ class MoviesController < ApplicationController
     @user = User.find(params[:user_id])
     @facade = MoviesFacade.new(params[:q], params[:keywords])
   end
+
+  def show
+    @user = User.find(params[:user_id])
+    @facade = MoviesFacade.new(params[:id], nil).movie_details
+     
+  end
 end
