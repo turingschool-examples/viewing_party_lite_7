5e836cd7ea5a1b87a7b2b227e88be2e6898e47fd
Allen Russell <garyrusselljr@gmail.com>
2023-08-27 12:43:02 -0600

    fix: bug with attendees

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @viewing_party = params[:viewing_party_params]
-    @attendees = User.find(params[:attendees])
+
+    if params[:attendees].nil?
+      @attendees = params[:attendees]&.map { |attendee_id| User.find(id) }
+    else
+      @attendees = User.find(params[:attendees])
+    end
+
     @facade = MovieFacade.new(params[:id])
     @movie = @facade.get_movie_by_id(params[:movie_id])
     @image = @facade.get_movie_image_by_id(params[:movie_id])
