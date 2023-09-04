bdc759b93f094f7821441080154010fea6bdffc6
Nick Sacco <nicksacco10@gmail.com>
2023-09-04 16:18:03 -0700

    add pw digest and clear test data errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, confirmation: true
+  has_secure_password
 end
