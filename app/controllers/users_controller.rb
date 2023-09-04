862dc1f652ffcf2cb2e116510522624f131bc2da
Weston Schutt <wtschutt@gmail.com>
2023-09-04 14:13:10 -0600

    Add non-restful routes (for now) and controller actions to handle logins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def login_form
+
+  end
+
+  def login_user
+    begin
+      user = User.find_by(email: params[:email])
+      raise "Invalid credentials. Please Try again." unless user && user.authenticate(params[:password])
+      redirect_to user_path(user)
+    rescue StandardError => e
+      redirect_to login_path
+      flash[:alert] = e.message
+    end
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,15 @@ Rails.application.routes.draw do
 
   get '/register', to: 'users#new'
   resources :register, only: [:create], controller: 'users'
+  
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login_user'
 
   resources :users, only: [:show] do
     resources :discover, only: [:index]
 
-    resources :movies, only: %i[index show] do
-      resources :viewing_party, only: %i[create new]
+    resources :movies, only: [:index, :show] do
+      resources :viewing_party, only: [:create, :new]
     end
   end
 end
