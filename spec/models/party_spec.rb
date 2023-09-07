8585b2bab4fa835d0dfe1687abc30e1d09f2d2c3
Nick Sacco <nicksacco10@gmail.com>
2023-09-06 18:20:58 -0700

    added more model tests

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe Party, type: :model do
     it { should validate_numericality_of(:duration).only_integer }
     it { should validate_presence_of(:movie_id) }
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @party = Party.create!(date: '2021-01-01', time: '12:00', duration: 120, movie_id: 59)
+    end
+
+    it 'can get the movie title', :vcr do
+      expect(@party.movie_title).to eq("A History of Violence")
+    end
+
+    it 'can get the movie image', :vcr do
+      expect(@party.get_image).to be_a(String)
+    end
+  end
 end
