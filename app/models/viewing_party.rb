7559de2a941f3ca9054be12ac261f0dace227d9c
Reed Hillmar <reedhillmar@gmail.com>
2023-10-09 20:41:58 -0600

    setup: viewing party validations

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,8 @@
 class ViewingParty < ApplicationRecord
   has_many :user_viewing_parties
   has_many :users, through: :user_viewing_parties
+
+  validates :movie_title, presence: true
+  validates :duration, presence: true, numericality: { only_integer: true }
+  validates :date_time, presence: true
 end
