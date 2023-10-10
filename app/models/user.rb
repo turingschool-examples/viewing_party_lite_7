6f239ca353ff716acd5eff0bc2c57ced97f6f27f
Connor Richmond <richmond.connor@gmail.com>
2023-10-10 11:47:49 -0400

    landing page testing/removed user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ApplicationRecord
-  has_many :users_viewing_party
-  has_many :viewing_parties, though: :users_viewing_party
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, email: true
+  
 end
