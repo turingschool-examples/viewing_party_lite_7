96cf0a864b0e759a91b1dc157bd8ecb11e6b3b36
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-09 22:01:29 -0500

    Initial Setup of Rubocop and other gems

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module ViewingPartyLite7
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators.after_generate do |files|
+      parsable_files = files.filter { |file| file.end_with?('.rb') }
+      unless parsable_files.empty?
+        system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+      end
+    end
   end
 end
