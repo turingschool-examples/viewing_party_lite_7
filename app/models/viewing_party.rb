6e8af84657e2f33251c749e1490378a4300bd834
Antoine Aube <aaube3@gmail.com>
2023-10-09 18:49:20 -0600

    models: validations/relationships - uniqueness of emails

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,2 +1,7 @@
 class ViewingParty < ApplicationRecord
+  has_many :users, through: :user_viewing_parties
+  has_many :user_viewing_parties
+  validates :duration, presence: true
+  validates :day, presence: true
+  validates :view_time, presence: true
 end
