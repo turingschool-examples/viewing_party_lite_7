b3fd397865a880c964fae2ef8340f291d37c68ca
Joop Stark <stark.joop@gmail.com>
2023-10-10 12:03:27 -0600

    feat test: user show page has header, discover button, and table with viewing party attributes

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def date_formatter
+    date_time.strftime("%A, %B %d, %Y")
+  end
+
+  def time_formatter
+    date_time.strftime("%l:%M %p")
+  end
 end
