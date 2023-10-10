69948effe70485aa105d1e333fc6371f77138e2c
Jorja Fleming <aset284@gmail.com>
2023-10-10 15:34:06 -0600

    Feat: completed landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'landing_page#index'
+
+  resources :users, only: %i[show new create]
 end
