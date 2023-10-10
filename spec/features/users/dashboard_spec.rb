7d53336d8dd836fc5b753b0f7a4fd5ce5d308981
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-09 20:53:25 -0600

    test: add test for discover movies story

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -1,21 +1,32 @@
 require "rails_helper"
 
 RSpec.describe "User Dashboard Page", type: :feature do
+  before(:each) do
+    @user = User.create(name: "Brad", email: "bradsmith@gmail.com")
+    @viewing_party = @user.viewing_parties.create(duration: 180, day: "December 2, 2023", view_time: "7:00 pm")
+  end
   describe "when I visit 'users/:id'" do
     it "I should see 'User's Name Dashboard' at the top of the page, a button to discover movies, and a section that lists viewing parties" do
-      user = User.create(name: "Brad", email: "bradsmith@gmail.com")
-      viewing_party = user.viewing_parties.create(duration: 180, day: "December 2, 2023", view_time: "7:00 pm")
-
-      visit "/users/#{user.id}"
+      visit "/users/#{@user.id}"
 
       expect(page).to have_content("Brad's Dashboard")
 
       expect(page).to have_button("Discover Movies")
 
       within("#viewing_parties") do
-        expect(page).to have_content("December 2, 2023")
-        expect(page).to have_content("7:00 pm")
+        expect(page).to have_content("#{@viewing_party.day}")
+        expect(page).to have_content("#{@viewing_party.view_time}")
       end
+    end
+
+    it "When I click 'Discover Movies' button I am redirected to a discover page /users/:id/discover where the :id is the current user id" do
+      visit "/users/#{@user.id}"
+
+      click_button "Discover Movies"
+
+      save_and_open_page
+
+      expect(current_path).to eq("/users/#{@user.id}/discover")
     end
   end
 end
