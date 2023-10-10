c2076bc7847a46c5dee4a9cb280a0bee35823eb2
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-09 20:21:47 -0500

    update gemfiles with further gems, seed data, passing model associations
    co-author: Lane Lanebret85@gmail.com

--- spec/models/user_viewing_party_spec.rb
+++ spec/models/user_viewing_party_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
-RSpec.describe ViewingParty, type: :model do
+RSpec.describe UserViewingParty, type: :model do
   describe "relationships" do
-    it {should belong_to :user}
     it {should belong_to :viewing_party}
+    it {should belong_to :user}
   end
 end
