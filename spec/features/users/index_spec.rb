e643fb0deb45ef1d081576fc8c18178705cd274e
Allan Evans <oneblankpage27@gmail.com>
2023-10-10 16:53:45 -0600

    Blah

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Landing Page' do 
-  before :each do
+  before(:each) do
     load_test_data
   end
 
   it 'When a user visits the root path they should be on the landing page ('/') which includes:' do
 
+    visit "/"
+
     expect(page).to_have("ViewingPartyLite7")
     expect(page).to_have button("Create a New User")
 
     expect(page).to_have link("landing page")
+    expect(page).to_have link("Tom")
+    expect(page).to_have link("Jerry")
+    expect(page).to_have link("Bob")
 
   end
+end
--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,4 +1,5 @@
 def load_test_data
-  
-    
+  @user1 = User.create!(name: "Tom")
+  @user1 = User.create!(name: "Jerry")
+  @user1 = User.create!(name: "Bob")   
 end
