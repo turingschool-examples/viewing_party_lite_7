0f6dc5d1d86dde630bae71f2c8dded585c3af3c3
Reed Hillmar <reedhillmar@gmail.com>
2023-10-11 13:35:48 -0600

    test: adds test for basic page setup

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -2,7 +2,15 @@ class ViewingParty < ApplicationRecord
   has_many :user_viewing_parties
   has_many :users, through: :user_viewing_parties
 
-  validates :movie_title, presence: true
+  validates :movie_id, presence: true
   validates :duration, presence: true, numericality: { only_integer: true }
   validates :date_time, presence: true
+
+  def image
+    "image"
+  end
+
+  def movie_title
+    "movie title"
+  end
 end
