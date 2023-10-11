66da57e11984384f74fd64b139255331c86bbeff
Reed Hillmar <reedhillmar@gmail.com>
2023-10-11 14:02:00 -0600

    test: test code for create view party button

--- spec/features/movies/details_spec.rb
+++ spec/features/movies/details_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Movie Details" do
     load_test_data
   end
 
-  it 'has a header' do
+  it 'has a header', :vcr do
     # require 'pry';binding.pry
     visit user_movie_path(@anne, @arthur[:movie_id])
 
@@ -21,7 +21,7 @@ RSpec.feature "Movie Details" do
     expect(page).to have_current_path("/")
   end
 
-  it 'has a sub header' do
+  it 'has a sub header', :vcr do
     visit user_movie_path(@anne, @arthur[:movie_id])
 
     within('#sub_header') do
@@ -29,14 +29,24 @@ RSpec.feature "Movie Details" do
     end
   end
 
-  it "has a button to the user's discover page" do
+  it "has a button to the user's discover page", :vcr do
     visit user_movie_path(@anne, @arthur[:movie_id])
 
     within('#buttons') do
-      click_button('Discover Page')
+      click_button('Discover Movies')
     end
 
     expect(page).to have_current_path(user_discover_path(@anne))
+  end
+
+  it 'has a button to create a view party for the movie', :vcr do
+    visit user_movie_path(@anne, @arthur[:movie_id])
+
+    within('#buttons') do
+      click_button('Create Viewing Party for Arthur')
+    end
+
+    expect(page).to have_current_path(new_user_movie_viewing_party_path(@anne, @arthur[:movie_id]))
   end
 end
 
