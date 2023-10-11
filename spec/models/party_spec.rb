1c37c04689ed5c77669b0758e2f7859849b736f4
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-11 01:25:09 -0500

    Create the model test for the party validations and relationships

---
+++ spec/models/party_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Party, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :user }
+    it { should validate_presence_of :movie }
+  end
+
+  describe "relationships" do
+    it { should have_many :user_parties }
+    it { should have_many(:users).through(:user_parties) }
+  end
+end
