971d4f1be89adfbe344ecec12952ec42b377a12a
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-10 21:51:40 -0600

    feat: add api call to get results in search action, add show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,23 @@
 class MoviesController < ApplicationController
   def index
-    @user = User.find(params[:id])
   end
 
   def search
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = "ac385d6c06bfaba2ca90eeb5b61150dd"
+    end
+
+    if params[:q] == "top20rated"
+      response = conn.get("/3/movie/top_rated")
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      @movies = json[:results][0..19]
+    end
+    
+    render "movies/search"
+  end
+  
+  def show
     
   end
 end
