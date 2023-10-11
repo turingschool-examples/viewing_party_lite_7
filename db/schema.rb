911998b4d0251af0b8d5dddbc66c670eae7b48e3
Joop Stark <stark.joop@gmail.com>
2023-10-11 11:36:58 -0600

    fix schema and test helper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_170200) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_173124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_170200) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "movie_id"
+    t.integer "duration"
   end
 
   add_foreign_key "user_viewing_parties", "users"
