9375c667945ed78af3c2fc89d31e13ad2115b8e7
Lane Bretschneider <lanebret85@gmail.com>
2023-10-10 18:33:31 -0700

    test: add tests for top rated movies button and for searching for movies by keyword title
    Co-authored-by: Kaylee Janes <132856753+kbug819@users.noreply.github.com>

--- spec/features/users/discover/index_spec.rb
+++ spec/features/users/discover/index_spec.rb
@@ -17,5 +17,33 @@ RSpec.describe "User Discover", type: :feature do
         expect(page).to have_content("Discover Movies")
       end
     end
+
+    feature "When I visit the discover movies page, where :id, is the id of a valid user" do
+      scenario "I see a button to discover Top Rated Movies" do
+        visit "/users/#{@user_1.id}/discover"
+  
+        expect(page).to have_button("Discover Top Rated Movies")
+  
+        click_button "Discover Top Rated Movies"
+  
+        expect(current_path).to eq("/users/#{@user_1.id}/movies")
+      end
+  
+      scenario "I see a text field to enter keyword(s) to search by movie title, and a button to search by movie title" do
+        visit "/users/#{@user_1.id}/discover"
+
+        expect(page).to have_content("Movie title")
+
+        expect(page).to have_field(:title)
+        
+        expect(page).to have_button("Search by Movie Title")
+
+        fill_in :title, with: "Fight Club"
+        
+        click_on "Search by Movie Title"
+
+        expect(current_path).to eq("/users/#{@user_1.id}/movies")
+      end
+    end
   end
 end
