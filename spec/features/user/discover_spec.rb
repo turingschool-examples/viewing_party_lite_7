4806009d9a8c729033bf728acea52e2c42f8a4dc
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-11 17:34:19 -0500

    Create the discover_spec file and added the test for discover movies button

---
+++ spec/features/user/discover_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+# frozen_string_literal: true
+
+# RSpec.describe "Discover Page" do
+#   describe "" do
+#     it "" do
+      
+#       expect(true).to eq(true)
+#     end
+#   end
+# end
