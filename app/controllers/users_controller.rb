fc78282da3a9863a2ce51732b7ba74d6049fecba
pcbennett108 <pcbennett42@gmail.com>
2023-10-11 11:19:25 -0600

    feat: add controller for welcome page, add new and create methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,25 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user)
+      flash[:success] = "New account created successfully."
+    else
+      flash[:error] = "#{error_message(user.errors)}"
+      redirect_to register_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email) 
+  end
 end
