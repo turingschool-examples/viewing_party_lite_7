f77b9c4784507426991e25b27e19d0bca7e4ffbf
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-11 16:11:57 -0500

    update of the movie controller with api calls
    Co-authored-by: Lane Bretschneider <128104820+lanebret85@users.noreply.github.com>

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,4 +1,12 @@
 class Users::MoviesController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    if params[:top_movies]
+      @movies = ThemoviedbService.new.top_movies
+      @header = "Top Movies"
+    elsif params[:title] != nil
+      @movies = ThemoviedbService.new.movie_titles(params[:title])
+      @header = "Search Results"
+    end
   end
 end
