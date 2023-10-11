e8709749e2d82f833ae78bd4070855ef1dd5161b
Will Weston <Williamweston81@gmail.com>
2023-10-11 10:03:49 -0500

    Weston(1) Fix: Versioning not needed on movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,9 @@
-module Api
-  module V1
-    class MoviesController < ApplicationController
-      def show
-        service = MovieService.new
-        @movie_details = service.movie_details(params[:id])
-        @movie_cast = service.movie_cast(params[:id])
-        @movie_reviews = service.movie_reviews(params[:id])
-      end
-    end
+class MoviesController < ApplicationController
+  def show
+    service = MovieService.new
+    @movie_details = service.movie_details(params[:id])
+    @movie_cast = service.movie_cast(params[:id])
+    @movie_reviews = service.movie_reviews(params[:id])
   end
 end
+
