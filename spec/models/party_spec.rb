719af5ddedb0d7cc7229573841c2e78bdee0cb85
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-11 11:59:51 -0500

    Refactor prior tests to eliminate redundancy and erroneous lines of code

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Party, type: :model do
   describe "validations" do
-    it { should validate_presence_of :user }
     it { should validate_presence_of :movie }
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # describe "validations" do
-  #   it { should validate_presence_of :name }
-  #   it { should validate_presence_of :email }
-  # end
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+  end
 
   describe "relationships" do
     it { should have_many :user_parties }
