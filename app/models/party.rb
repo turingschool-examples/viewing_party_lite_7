7d79c3d5891e9f3f8f2c0ae9e9566ff6c7ec4f1f
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-11 11:47:24 -0500

    Remove the 'user' attribute from the Party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,5 @@
 class Party < ApplicationRecord
-  validates_presence_of :user,
-                        :movie
+  validates_presence_of :movie
 
   has_many :user_parties
   has_many :users, through: :user_parties
