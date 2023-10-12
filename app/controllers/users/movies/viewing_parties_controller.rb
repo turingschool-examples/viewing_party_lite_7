5c37a80a960c12dd9de5674aa2b018093b24983b
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-11 20:53:41 -0500

    feat: controller create and new
    Co-authored-by: Lane Bretschneider <128104820+lanebret85@users.noreply.github.com>

--- app/controllers/users/movies/viewing_parties_controller.rb
+++ app/controllers/users/movies/viewing_parties_controller.rb
@@ -1,5 +1,10 @@
 class Users::Movies::ViewingPartiesController < ApplicationController
   def new
-    
+    @user = User.find(params[:user_id])
+    @movie = ThemoviedbService.new.movie_details(params[:movie_id])
+    @users = User.excluding(@user)
+  end
+
+  def create
   end
 end
