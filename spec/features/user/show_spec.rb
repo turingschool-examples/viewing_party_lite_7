4a7e303f611e26f06968e71032c82199981de134
Will Chen <wfchen1990@gmail.com>
2023-10-12 08:22:35 -0600

    Troubleshoot vcr

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -1,3 +1,4 @@
 require "rails_helper"
 
-RSpec.describe "User Show Page" do
+# RSpec.describe "User Show Page" do
+# end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -99,5 +99,5 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data('<API_KEY>') { Rails.application.credentials.the_movie_db[:key] }
+  config.filter_sensitive_data('<THE_MOVIE_DB_API_KEY>') { Rails.application.credentials.the_movie_db[:key] }
 end
