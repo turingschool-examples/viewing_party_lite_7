6f00afbc87777abf94f17b4a7466f2720491e759
Antoine Aube <aaube3@gmail.com>
2023-10-12 15:03:21 -0600

    controller: beginnings create methods for creating new viewing party

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,4 +1,21 @@
 class ViewingPartiesController < ApplicationController
+  before_action :find_movie
+
   def new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def find_movie
+    @movie = facade.movie
+    #require 'pry';binding.pry
+  end
+
+  def facade
+    MovieFacade.new(params[:movie_id])
   end
 end
