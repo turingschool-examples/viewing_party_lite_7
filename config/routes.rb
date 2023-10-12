bd5b7d8869ba2c9ee5af9461da687ce659d64749
Antoine Aube <aaube3@gmail.com>
2023-10-12 15:06:44 -0600

    routes: alias for create viewing party route

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   post "/register", to: "users#create"
 
   get "/users/:id/movies/:movie_id/viewing_party/new", to: "viewing_parties#new"
-  post "/users/:id/movies/:movie_id/viewing_party", to: "viewing_parties#create"
+  post "/users/:id/movies/:movie_id/viewing_party", to: "viewing_parties#create", as: :viewing_party_create
 end
