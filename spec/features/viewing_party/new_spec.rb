790901aff4a7796a3349272c6bd5fee46302e5d5
Connor Richmond <richmond.connor@gmail.com>
2023-10-13 17:55:14 -0400

    SAD path testing for viewing party

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -39,5 +39,67 @@ RSpec.describe 'the movies viewing party new page', type: :feature do
 
       expect(current_path).to eq("/users/#{@user.id}")
     end
+
+    it 'does not fill in params for viewing party',
+       :vcr do
+      visit user_discover_path(@user)
+
+      click_button 'Discover Top Rated Movies'
+
+      click_on 'The Godfather'
+
+      click_button 'Create Viewing Party for The Godfather'
+
+      expect(current_path).to eq("/users/#{@user.id}/movies/238/viewing_party/new")
+
+      expect(page).to have_field('Duration')
+      expect(page).to have_field('When')
+      expect(page).to have_field('Start Time')
+      expect(page).to have_field("guests_#{@user2.id}")
+      expect(page).to have_button('Create Party')
+
+      within('#users') do
+        expect(page).to have_content(@user2.name)
+      end
+
+      click_button 'Create Party'
+
+      expect(current_path).to eq("/users/#{@user.id}/movies/238/viewing_party/new")
+      expect(page).to have_content("Duration, Start Time, and Date are required fields")
+
+
+    end
+
+    it 'does not add guests to the party',
+        :vcr do
+
+      visit user_discover_path(@user)
+
+      click_button 'Discover Top Rated Movies'
+
+      click_on 'The Godfather'
+
+      click_button 'Create Viewing Party for The Godfather'
+
+      expect(current_path).to eq("/users/#{@user.id}/movies/238/viewing_party/new")
+
+      expect(page).to have_field('Duration')
+      expect(page).to have_field('When')
+      expect(page).to have_field('Start Time')
+      expect(page).to have_field("guests_#{@user2.id}")
+      expect(page).to have_button('Create Party')
+
+      within('#users') do
+        expect(page).to have_content(@user2.name)
+      end
+
+      fill_in 'Duration', with: 190
+      fill_in 'When', with: '12/10/2023'
+      fill_in 'Start Time', with: '6:30 PM'
+      click_button 'Create Party'
+
+      expect(current_path).to eq("/users/#{@user.id}/movies/238/viewing_party/new")
+      expect(page).to have_content("Add guests to the party")
+    end
   end
 end
