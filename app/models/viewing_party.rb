0b9ad52ac78401f1dd40ae0307e10835c9d248f6
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-13 11:30:40 -0600

    feat: add find_movie and find_host methods to viewing party model

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -4,4 +4,18 @@ class ViewingParty < ApplicationRecord
   validates :duration, presence: true
   validates :day, presence: true
   validates :view_time, presence: true
+
+  def find_movie
+    MovieFacade.new(movie_id).movie
+  end
+
+  def find_host(current_user)
+    host_id = UserViewingParty.find_by("user_viewing_parties.host = true AND viewing_party_id = ?", self.id).user_id
+
+    if current_user.id == host_id
+      "Me"
+    else
+      User.find(host_id).name
+    end
+  end
 end
