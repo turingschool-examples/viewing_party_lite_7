3347ef9b831f827689527057b5c2e8addd02fec8
Captainlearyo <clayoleary@gmail.com>
2023-10-13 13:08:59 -0500

    re format application html

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -18,13 +18,34 @@ RSpec.describe 'Movie Results' do
     click_button "Discover Movies"
     expect(current_path).to eq(user_discover_index_path(user_id: @user1.id))
 
+    # stub_request(:get, "https://api.themoviedb.org/3/movie/popular?api_key=2c14ab06be3bfe4ca5fecd0f9b2c73fc").
+    # to_return(status: 200, body: "Your mock response data here")
+
+
     visit "/users/#{@user1.id}/movies/268"
-    save_and_open_page
 
     expect(page).to have_content("Movie Id: 268")
     expect(page).to have_content("Vote Average: 7.2")
     expect(page).to have_content("Runtime: 2 hours 6 minutes") 
+    expect(page).to have_content("Fantasy")
+    expect(page).to have_content("Action")
+    expect(page).to have_content("Crime")
 
+    expect(page).to have_content("Batman must face his most ruthless nemesis when a deformed madman calling himself")
+
+    expect(page).to have_content("Michael Keaton")
+    expect(page).to have_content("Jack Nicholson")
+    expect(page).to have_content("Kim Basinger")
+    expect(page).to have_content("Michael Gough")
+    expect(page).to have_content("Jerry Hall")
+    expect(page).to have_content("Robert Wuhl")
+    expect(page).to have_content("Pat Hingle")
+    expect(page).to have_content("Billy Dee Williams")
+    expect(page).to have_content("Jack Palance")
+    expect(page).to have_content("Tracey Walter")
+
+    expect(page).to have_content("Review Count: 7")
+    
   end
 end
 
