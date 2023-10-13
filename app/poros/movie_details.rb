64d8da7fcccb11fd62428f093d44f9a6fd83be02
Lane Bretschneider <lanebret85@gmail.com>
2023-10-13 11:12:31 -0700

    feat: create poro with initialize method
    Co-authored-by: Kaylee Janes <132856753+kbug819@users.noreply.github.com>

--- app/poros/movie_details.rb
+++ app/poros/movie_details.rb
@@ -1,4 +1,4 @@
-class Movie
+class MovieDetails
   attr_reader :id, :title, :vote_average, :vote_count, :runtime, :summary, :genres
   def initialize(movie_info)
     @id = movie_info[:id]
@@ -7,6 +7,5 @@ class Movie
     @runtime = movie_info[:runtime]
     @summary = movie_info[:overview]
     @genres = movie_info[:genres]
-    @total_reviews = movie_info[:total_reviews]
   end
 end
