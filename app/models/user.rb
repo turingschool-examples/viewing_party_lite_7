df7759d7a9f10f53b7c1dbca4f2650d9426f0b78
Lane Bretschneider <lanebret85@gmail.com>
2023-10-13 11:25:27 -0700

    fix: remove broken invitees methods to move to the correct model class, add self. to the front of user_host_name method to reflect class method
    Co-authored-by: Kaylee Janes <132856753+kbug819@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,7 @@ class User < ApplicationRecord
   has_many :user_viewing_parties
   has_many :viewing_parties, through: :user_viewing_parties
 
-  def invited_parties_invitees
-    User.joins(:viewing_parties).select(:viewing_parties.id).where("user_viewing_parties.user_id != #{self.id}")
-  end
-
-  def hosted_parties_invitees
-
-  end
-
-  def user_host_name(id)
+  def self.user_host_name(id)
     user = User.find(id)
     user.name
   end
