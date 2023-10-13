7e8f771c4e95e9d61faca15934a9142913b1e74f
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-13 11:43:03 -0600

    feat: add conver_time and format_genres methods to movie poro, also add backdrop_path attribute and update title to work wtih rest of app

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,12 +1,33 @@
 class Movie
-  attr_reader :id, :title, :vote_average, :runtime, :genres, :overview
+  attr_reader :id, :title, :vote_average, :runtime, :genres, :overview, :backdrop_path
 
   def initialize(data)
     @id = data[:id]
-    @title = data[:original_title]
+    @backdrop_path = data[:backdrop_path]
+    @title = data[:title]
     @vote_average = data[:vote_average]
     @runtime = data[:runtime] # will need to accounr for formatting
     @genres = data[:genres] # will need to create format method, also account for sad path as some movies may not have explicit genres
     @overview = data[:overview]
+
+    def convert_time
+      if @runtime > 60
+        "#{@runtime/60}h #{@runtime % 60}min"
+      else
+        "#{@runtime}min"
+      end
+    end
+
+    def format_genres
+      formatted = "" 
+      @genres.map do |genre|
+        if genre != @genres.last
+          formatted += "#{genre[:name]}, "
+        else
+          formatted += genre[:name]
+        end
+      end
+      formatted
+    end
   end
 end
