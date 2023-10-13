f2b0f28c83a06a259e77b21f319acfdccf203b6b
Connor Richmond <richmond.connor@gmail.com>
2023-10-13 17:22:48 -0400

    add users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(name: 'John Smith', email: 'jsmith@aol.com')
+User.create!(name: 'Jane Doe', email: 'jdoe@aol.com')
