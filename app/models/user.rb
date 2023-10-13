c857d6a1326e2946bd65c4e4634247d487e341a7
Lane Bretschneider <lanebret85@gmail.com>
2023-10-12 19:40:23 -0700

    feat: start writing methods to retrieve invitee user_ids
    Co-authored-by: Kaylee Janes <132856753+kbug819@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   has_many :user_viewing_parties
   has_many :viewing_parties, through: :user_viewing_parties
+
+  def invited_parties_invitees
+    User.joins(:viewing_parties).select(:viewing_parties.id).where("user_viewing_parties.user_id != #{self.id}")
+  end
+
+  def hosted_parties_invitees
+
+  end
+
+  def user_host_name(id)
+    user = User.find(id)
+    user.name
+  end
 end
