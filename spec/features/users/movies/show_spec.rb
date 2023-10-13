a4f18d43a975577bca04d7a2b742479ff43afc42
Captainlearyo <clayoleary@gmail.com>
2023-10-13 11:24:59 -0500

    create Go to Landing Page link

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe 'Movie Results' do
   it 'When I go to a user dashbaord, and click "Discover Movies" button, I am redirected to a discover page /users/:id/discover' do
 
     visit "/users/#{@user1.id}/movies/268"
+    click_link "Go to Landing Page"
+    expect(current_path).to eq("/")
+
+    visit "/users/#{@user1.id}/movies/268"
     click_button "Create a Viewing Party"
     expect(current_path).to eq("/users/#{@user1.id}/movies/268/viewing-party/new")
 
@@ -15,6 +19,7 @@ RSpec.describe 'Movie Results' do
     expect(current_path).to eq(user_discover_index_path(user_id: @user1.id))
 
     visit "/users/#{@user1.id}/movies/268"
+    save_and_open_page
 
     expect(page).to have_content("Movie Id: 268")
     expect(page).to have_content("Vote Average: 7.2")
