f71e61e27a9fae7ce08e61c315ea2f89bd5f840a
Jorja Fleming <aset284@gmail.com>
2023-10-13 16:52:01 -0600

    Feat Search movies still working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,11 @@
 class MoviesController < ApplicationController
   def index
-    @movies = MovieFacade.top_rated_movies
     @user = User.find(session[:user_id])
+    if params[:search_by_movie_title]
+      @movies = MovieFacade.search(params[:search_by_movie_title])
+    else
+      @movies = MovieFacade.top_rated_movies
+    end
   end
 
   def show
--- spec/features/movies/movies_spec.rb
+++ spec/features/movies/movies_spec.rb
@@ -24,4 +24,25 @@ RSpec.describe 'Movies Results Page', type: :feature do
 
     expect(page).to have_link('Return to Discover Page', href: user_discover_index_path(user_id: @u1.id))
   end
+
+  it "can search for movies by title and have a return button" do
+    visit user_path(@u1)
+        
+    stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=d84e59db219943d545dafbf5f7631afa&query=Barbie").
+      to_return(status: 200, body: File.read("spec/features/fixtures/search_movie_results.json"), headers: {})
+
+    click_button 'Discover Movies'
+
+    expect(page).to have_current_path(user_discover_index_path(@u1.id))
+
+    fill_in 'Search By Movie Title', with: 'Barbie'
+    click_button 'Search'
+
+    expect(current_path).to eq(user_movies_path(@u1.id))
+    expect(page).to have_content('Barbie')
+    expect(page).to have_content('Vote Average')
+    expect(page).to have_content('7.3')
+    expect(page).to have_link('Return to Discover Page', href: user_discover_index_path(user_id: @u1.id))
+    
+  end
 end
