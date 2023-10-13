45aae503165881c30fef4d162571c1262de743c8
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-13 18:04:43 -0500

    refactor: viewing party controller
    Co-authored-by: Lane Bretschneider <128104820+lanebret85@users.noreply.github.com>

--- app/controllers/users/movies/viewing_parties_controller.rb
+++ app/controllers/users/movies/viewing_parties_controller.rb
@@ -1,28 +1,15 @@
 class Users::Movies::ViewingPartiesController < ApplicationController
   def new
-    @user = User.find(params[:user_id])
-    @movie = ThemoviedbService.new.movie_details(params[:movie_id])
-    @users = User.excluding(@user)
+    @facade = ViewingPartiesFacade.new(params)
   end
 
   def create
-    users = User.all
-    if params[:duration].to_i < params[:runtime].to_i
+    @facade = ViewingPartiesFacade.new(params)
+    if @facade.duration_less_than_runtime
       redirect_to new_user_movie_viewing_party_path((params[:user_id]), (params[:movie_id]))
       flash[:error] = "Party duration cannot be less than movie runtime"
     else
-      party = ViewingParty.create!(
-        duration: (params[:duration]),
-        date: (params[:date]),
-        start_time: (params[:start_time]),
-        movie_id: (params[:movie_id]),
-        host_user_id: (params[:user_id])
-        )
-      result = users.map do |user|
-        if params["#{user.id}"] == "1"
-          UserViewingParty.create!(user_id: user.id, viewing_party_id: party.id)
-        end
-      end
+      @facade.create_user_viewing_party
       redirect_to user_path(params[:user_id])
     end
   end
