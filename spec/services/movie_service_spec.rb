b2c3d05cd25411701bfeaa0e19e1f6e90fd02440
Connor Richmond <richmond.connor@gmail.com>
2023-10-13 12:47:23 -0400

    update tests to be passing

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -3,15 +3,14 @@ require "rails_helper"
 RSpec.describe MovieService do
   it "exists" do
     ms = MovieService.new
-
     expect(ms).to be_a MovieService
   end
 
   context "class methods" do
-    context "#end_point", :vcr do
+    context "#get_url", :vcr do
       it "connects endpoints to the base api url" do
         service = MovieService.new
-        results = service.end_point("3/movie/238?language=en-US")
+        results = service.get_url("3/movie/238?language=en-US")
         
         expect(results).to be_a Hash
         expect(results[:genres]).to be_an Array
