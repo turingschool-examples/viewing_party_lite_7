6bcc5c75fe70437afbc84c32e8bc216dd2d6bf73
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-12 23:26:16 -0600

    feat: add get_guest_names to party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -12,4 +12,11 @@ class Party < ApplicationRecord
       .where("party_users.party_id = ? AND party_users.is_host = true", self.id)
       .first.name
   end
+
+  def get_guest_names
+    Party.joins(:users)
+      .select("users.name")
+      .where("party_users.party_id = ? AND party_users.is_host = false", self.id)
+      .pluck(:name)
+  end
 end
