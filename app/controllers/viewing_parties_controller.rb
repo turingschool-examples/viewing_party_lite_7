2da771a1810a2f4ea3a3b29d99bc8e1d6abfb662
Eliza Keating <ek.elizakeating@gmail.com>
2023-10-13 14:21:29 -0600

    feat: add find_user, party_params, adjust create
    Co-authored-by: Antoine Aube <55931218+Antoine-Aube@users.noreply.github.com>

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,24 +1,20 @@
 class ViewingPartiesController < ApplicationController
   before_action :find_movie
+  before_action :find_user, only: [:new, :create]
 
   def new
     @users = User.where("id !=  ?", params[:id])
   end
 
   def create
-    viewing_party = ViewingParty.new({
-      duration: params[:duration],
-      day: params[:day],
-      view_time: params[:view_time],
-      movie_id: @movie.id
-    })
+    viewing_party = ViewingParty.new(party_params)
     
     if viewing_party.duration < @movie.runtime
       flash[:error] = "Viewing party duration can not be less than the run time of the movie. Please try again."
       redirect_to "/users/#{params[:id]}/movies/#{@movie.id}/viewing_party/new"
     else
       viewing_party.save
-      UserViewingParty.create(user: User.find(params[:id]), viewing_party: viewing_party, host: true)
+      UserViewingParty.create(user: @user, viewing_party: viewing_party, host: true)
       
       User.all.each do |user|
         if params["#{user.name}"] == "1"
@@ -34,8 +30,16 @@ class ViewingPartiesController < ApplicationController
 
   private
 
+  def find_user
+    @user = User.find(params[:id])
+  end
+
   def find_movie
     @movie = facade.movie
+  end
+
+  def party_params
+    params.permit(:movie_id, :duration, :day, :view_time)
   end
 
   def facade
