8d50c673021cffe3d5039b2af01c841e6c56dc8d
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-13 00:00:36 -0600

    feat: add facade and begin refactoring user show page

---
+++ app/services/movie_service.rb
@@ -0,0 +1,20 @@
+class MovieService
+  def initialize
+    WebMock.allow_net_connect!
+  end
+
+  def conn
+    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = Rails.application.credentials.tmdb[:key]
+    end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def movie_poster(id)
+    get_url("/3/movie/#{id}/images")
+  end
+end
