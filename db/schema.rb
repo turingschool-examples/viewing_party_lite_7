9ade6bdc4e697b7f8ddfad4b516fc9db1e305a69
Nick Spencer <strangebrew202@gmail.com>
2023-10-13 17:18:23 -0600

    Add: new column to parties table to save image path data for movie posters, added image to parties that show on user dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_204132) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_225343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_204132) do
     t.datetime "updated_at", null: false
     t.string "movie_title"
     t.datetime "party_date"
+    t.string "poster_path"
   end
 
   create_table "user_parties", force: :cascade do |t|
