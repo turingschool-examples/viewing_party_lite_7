7c1ef8e0054faa44560909da380150df9966c7cb
Lane Bretschneider <lanebret85@gmail.com>
2023-10-12 19:41:35 -0700

    feat: add @hosted_viewing_parties and @invited_viewing_parties variables
    Co-authored-by: Kaylee Janes <132856753+kbug819@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # poster_base_url = ThemoviedbService.poster_image_base_url[:base_url]
+    # size = ThemoviedbService.poster_image_base_url[:logo_sizes][1]
+    # ThemoviedbService.movie_details
+    @hosted_viewing_parties = ViewingParty.where(host_user_id: @user.id)
+    @invited_viewing_parties = @user.viewing_parties
   end
 
   def create
