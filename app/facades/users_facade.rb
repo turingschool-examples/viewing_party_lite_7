8ca3926e76b4970bec5711eef1af00e311b89f44
Lane Bretschneider <lanebret85@gmail.com>
2023-10-13 14:26:25 -0700

    feat: add facade to take info out of users_controller
    Co-authored-by: Kaylee Janes <132856753+kbug819@users.noreply.github.com>

---
+++ app/facades/users_facade.rb
@@ -0,0 +1,30 @@
+class UsersFacade
+  attr_reader :id,
+              :user
+
+  def initialize(user_params)
+    @id = user_params[:id]
+    @user = User.find(@id)
+  end
+
+  def invited_viewing_parties
+    @user.viewing_parties
+  end
+
+  def hosted_viewing_parties
+    ViewingParty.where(host_user_id: @id)
+  end
+
+  def invited_party_movie(id)
+    movie_info = ThemoviedbService.new.movie_details(id)
+    MovieDetails.new(movie_info)
+  end
+
+  def hosted_party_movies
+    ThemoviedbService.new.movie_details(id)
+  end
+
+  def poster_url(id)
+    ThemoviedbService.new.poster_image_base_url[:base_url] + ThemoviedbService.new.poster_image_base_url[:logo_sizes][1] + invited_party_movie(id).poster_path
+  end
+end
