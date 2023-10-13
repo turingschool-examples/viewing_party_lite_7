f5636bbc8b5eadde8a4d1c24a1ff1102d91d330d
Connor Richmond <richmond.connor@gmail.com>
2023-10-12 21:27:01 -0400

    update for flow

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,14 +1,14 @@
 class MovieService
   
-  def end_point(url)
-    response = conn.get(url) do |f|
+  def conn
+    Faraday.new(url: "https://api.themoviedb.org/") do |f|
       f.params["api_key"] = Rails.application.credentials.themoviedb[:key]
     end
-    a = JSON.parse(response.body, symbolize_names: true)
-    # require 'pry'; binding.pry
   end
 
-  def conn
-    Faraday.new(url: "https://api.themoviedb.org/")
+  def end_point(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
+  
 end
