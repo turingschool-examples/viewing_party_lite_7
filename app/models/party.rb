5f342c46a77cecf6be4338662675eb39fca8a09e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-13 19:19:37 -0500

    Create the attendees query method for listing the names of the users invited to the watch party

--- app/models/party.rb
+++ app/models/party.rb
@@ -19,4 +19,14 @@ class Party < ApplicationRecord
   def formated_time
     self.party_date.strftime("%A, %B %-d, %Y, %I:%M %P")
   end
+
+  def attendees
+    Party.find_by_sql(
+      "select users.name from users
+      inner join user_parties on user_parties.user_id = users.id
+      inner join parties on user_parties.party_id = parties.id
+      where parties.id = #{self.id}
+      and user_parties.host = false"
+    )
+  end
 end
