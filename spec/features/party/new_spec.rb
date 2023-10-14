0c520d6b842e61813429dc0160a4172c94c8e2da
Nick Spencer <strangebrew202@gmail.com>
2023-10-13 19:06:49 -0600

    Refactor: changed the way we pass check box values forward for creating a new party, utilized fields_for to batch all checkbox values together.

--- spec/features/party/new_spec.rb
+++ spec/features/party/new_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe "Create new Viewing Party page", type: :feature do
     @alex = User.create!(name: "Alex", email: "alexthegreat@gmail.com")
     @party1 = Party.create!(movie: 1, movie_title: "Bar Wars", party_date: "2023-10-11T14:43")
     UserParty.create!(user_id: @sally.id, party_id: @party1.id, host: true)
-    UserParty.create!(user_id: @alex.id, party_id: @party1.id, host: false)
   end
   describe "As a visitor", :vcr do
     describe "When I visit the create new party page" do
@@ -21,7 +20,7 @@ RSpec.describe "Create new Viewing Party page", type: :feature do
           expect(page).to have_field :duration
           expect(page).to have_field :party_date
           expect(page).to have_button "Create Party"
-          expect(page).to have_unchecked_field @alex.id
+          expect(page).to have_unchecked_field "[invitees][#{@alex.id}]"
         end
       end
 
@@ -34,13 +33,18 @@ RSpec.describe "Create new Viewing Party page", type: :feature do
           fill_in :duration, with: 200
           fill_in :party_date, with: DateTime.current
           expect(page).to have_button "Create Party"
-          page.check(@alex.name)
+          page.check("[invitees][#{@alex.id}]")
           click_on "Create Party"
         end
 
         expect(page).to have_content("The Godfather")
         expect(page).to have_content(DateTime.current.strftime("%A, %B %-d, %Y, %I:%M %P"))
         expect(page).to have_content("Hosting")
+
+        within(".#{@alex.name}") do
+         expect(page).to have_content("Alex")
+        end
+
       end
     end
   end
