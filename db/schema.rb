f11761cf11b41999f2a8defae7d3f8a4c6dbc138
Will Chen <wfchen1990@gmail.com>
2023-10-14 16:24:29 -0600

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,53 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_014_184_211) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_14_184211) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'movies', force: :cascade do |t|
-    t.string 'title'
-    t.float 'vote_average'
-    t.integer 'runtime'
-    t.string 'genres'
-    t.string 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'poster_path'
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.float "vote_average"
+    t.integer "runtime"
+    t.string "genres"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "poster_path"
   end
 
-  create_table 'user_viewing_parties', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'viewing_party_id', null: false
-    t.boolean 'is_host', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_user_viewing_parties_on_user_id'
-    t.index ['viewing_party_id'], name: 'index_user_viewing_parties_on_viewing_party_id'
+  create_table "user_viewing_parties", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "viewing_party_id", null: false
+    t.boolean "is_host", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_viewing_parties_on_user_id"
+    t.index ["viewing_party_id"], name: "index_user_viewing_parties_on_viewing_party_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'viewing_parties', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'movie_id', null: false
-    t.datetime 'date_time'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'is_host', default: false, null: false
-    t.integer 'duration'
-    t.datetime 'start_time'
-    t.index ['movie_id'], name: 'index_viewing_parties_on_movie_id'
-    t.index ['user_id'], name: 'index_viewing_parties_on_user_id'
+  create_table "viewing_parties", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "movie_id", null: false
+    t.datetime "date_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_host", default: false, null: false
+    t.integer "duration"
+    t.datetime "start_time"
+    t.index ["movie_id"], name: "index_viewing_parties_on_movie_id"
+    t.index ["user_id"], name: "index_viewing_parties_on_user_id"
   end
 
-  add_foreign_key 'user_viewing_parties', 'users'
-  add_foreign_key 'user_viewing_parties', 'viewing_parties'
-  add_foreign_key 'viewing_parties', 'movies'
-  add_foreign_key 'viewing_parties', 'users'
+  add_foreign_key "user_viewing_parties", "users"
+  add_foreign_key "user_viewing_parties", "viewing_parties"
+  add_foreign_key "viewing_parties", "movies"
+  add_foreign_key "viewing_parties", "users"
 end
