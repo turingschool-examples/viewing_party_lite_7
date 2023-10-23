6e41feffdf4a6c2e096b3d85080b63b2e85bdc90
Lane Bretschneider <lanebret85@gmail.com>
2023-10-23 13:32:08 -0700

    change re-record interval in vcr cassettes from 1 day to 1 week

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,5 +75,5 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.filter_sensitive_data('<THEMOVIEDB_API_KEY>') { Rails.application.credentials.config.first.last[:api_key] }
-  config.default_cassette_options = { re_record_interval: 1.days }
+  config.default_cassette_options = { re_record_interval: 1.weeks }
 end
