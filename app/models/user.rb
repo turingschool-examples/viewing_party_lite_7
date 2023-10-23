f28d21bd0f3595287bc4a786d87e647a99dd77da
Lane Bretschneider <lanebret85@gmail.com>
2023-10-23 16:23:05 -0700

    feat: add { case_sensitive: false } to email uniquenes validation in order to ensure complete uniqueness of emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :viewing_parties, through: :user_viewing_parties
 
   validates :name, presence: true
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :password_digest, presence: true
 
   has_secure_password
