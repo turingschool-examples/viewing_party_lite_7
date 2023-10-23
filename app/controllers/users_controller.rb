0eb87c0ac538ecfb4a3ed8192d22d22ab137541b
Lane Bretschneider <lanebret85@gmail.com>
2023-10-23 16:26:11 -0700

    feat: add login_form action, add code to create and login actions to handle errors when creating a new user or logging into an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,29 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    user.save
-    if user.save
+    user = user_params
+    user[:email] = user[:email].downcase
+    new_user = User.new(user)
+    if new_user.save
+      redirect_to user_path(new_user.id)
+    else
+      redirect_to '/register'
+      flash[:error] = new_user.errors.full_messages.to_sentence
+    end
+  end
+
+  def login_form
+
+  end
+
+  def login
+    user = User.find_by(email: params[:email])&.authenticate(params[:password])
+    if user
+      flash[:success] = "Welcome, #{user.name}"
       redirect_to user_path(user.id)
     else
-      redirect_to '/users/new'
-      flash[:error] = 'User not created: Required information missing.'
+      flash[:error] = "Credentials are incorrect"
+      redirect_to '/login'
     end
   end
 
