61b6dab685ddf88528de4f28b9f10035cc796e98
Lane Bretschneider <lanebret85@gmail.com>
2023-10-23 16:17:40 -0700

    test: add case insensitivity to email uniqueness validation assertion

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password_digest) }
     it { should have_secure_password }
     it "has a password_digest attribute that changes when passed through bcrypts hashing algorithm" do
