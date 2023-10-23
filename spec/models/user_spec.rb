1c7abd91fa1ef8fbb04c353c6b7237bdcf0b0acc
Lane Bretschneider <lanebret85@gmail.com>
2023-10-23 13:31:43 -0700

    add password_digest attribute to each instance of user, add validations for user attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,9 +8,22 @@ RSpec.describe User, type: :model do
     it { should have_many(:viewing_parties).through(:user_viewing_parties) }
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password }
+    it "has a password_digest attribute that changes when passed through bcrypts hashing algorithm" do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
+  end
+
   describe '#user_host_name' do
     it 'can return the name of a user given their id' do
-      user_1 = User.create!(name: 'Jamie', email: '34@gmail.com')
+      user_1 = User.create!(name: 'Jamie', email: '34@gmail.com', password_digest: "$2a$12$8fZ4BOUSx4FlExUkBlPRdeQ9/k5bbDsOo2keHPuzE34")
 
       query = User.user_host_name(user_1.id)
 
@@ -20,9 +33,9 @@ RSpec.describe User, type: :model do
 
   describe '#all_users_except_self' do
     it 'can return a list of all users excluding user argument' do
-      user_1 = User.create!(name: 'Jamie', email: '34@gmail.com')
-      user_2 = User.create!(name: 'Katie', email: '34997@gmail.com')
-      user_3 = User.create!(name: 'George', email: 'george@gmail.com')
+      user_1 = User.create!(name: 'Jamie', email: '34@gmail.com', password_digest: "$2a$12$8fZ4BOUSx4FlExUkBlPRdeQ9/k5bbDsOo2keHPuzE34")
+      user_2 = User.create!(name: 'Katie', email: '34997@gmail.com', password_digest: "$2a$12$8fZ4BOUSx4FlExUkBlPRdeQ9/k5bbDsOo2keHPuzE35")
+      user_3 = User.create!(name: 'George', email: 'george@gmail.com', password_digest: "$2a$12$8fZ4BOUSx4FlExUkBlPRdeQ9/k5bbDsOo2keHPuzE36")
 
       query = User.all_users_except_self(user_1)
 
