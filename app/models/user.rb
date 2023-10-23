abad2e62c9a83ad0edf0b59b1ffc3167f3c9f5e4
Lane Bretschneider <lanebret85@gmail.com>
2023-10-23 13:33:20 -0700

    feat: add validations for user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   has_many :user_viewing_parties
   has_many :viewing_parties, through: :user_viewing_parties
 
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password_digest, presence: true
+
+  has_secure_password
+
   def self.user_host_name(id)
     user = User.find(id)
     user.name
