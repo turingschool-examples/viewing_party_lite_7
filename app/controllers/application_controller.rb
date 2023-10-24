892bb3d2d39eac93c239901f0dcfde15fabe5a7c
Antoine Aube <aaube3@gmail.com>
2023-10-23 19:02:44 -0600

    test: sad path for bad credentials login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  
   def current_user
     @_current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
