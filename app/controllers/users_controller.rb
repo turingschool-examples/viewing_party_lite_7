77ea65431d3fbc2ed9d282f8098b538009fd7d14
Lane Bretschneider <lanebret85@gmail.com>
2023-10-24 16:28:40 -0700

    feat: add log_out action to delete user_id from session upon logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     user[:email] = user[:email].downcase
     new_user = User.new(user)
     if new_user.save
+      session[:user_id] = new_user.id
       redirect_to user_path(new_user.id)
     else
       redirect_to '/register'
@@ -27,12 +28,18 @@ class UsersController < ApplicationController
   def login
     user = User.find_by(email: params[:email])&.authenticate(params[:password])
     if user
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}"
       redirect_to user_path(user.id)
     else
       flash[:error] = "Credentials are incorrect"
       redirect_to '/login'
     end
+  end
+
+  def log_out
+    session[:user_id] = nil
+    redirect_to "/"
   end
 
   private
