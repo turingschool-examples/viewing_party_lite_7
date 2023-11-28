a41c8bdeccfac2612bdd238b530423fb16afb1f1
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-28 07:48:29 -0700

    Update user show spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before(:each) do
-    user1 = User.create!(name: "Timmy Turner", email: "timmyturner1@gmail.com")
-    user2 = User.create!(name: "Wanda", email: "ilovecosmo1@gmail.com")
-    user3 = User.create!(name: "Cosmo", email: "ilovewanda1@gmail.com")
+    @user1 = User.create!(name: 'Timmy Turner', email: 'timmyturner1@gmail.com')
+    @user2 = User.create!(name: 'Wanda', email: 'ilovecosmo1@gmail.com')
+    @user3 = User.create!(name: 'Cosmo', email: 'ilovewanda1@gmail.com')
   end
 
-endrequire 'rails_helper'
-
-RSpec.describe User, type: :model do
   describe 'relationships' do
   end
 
