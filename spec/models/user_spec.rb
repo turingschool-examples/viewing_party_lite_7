bc3bb3b9c2744c1b218ad9f6ced2c610a7b3d854
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-28 07:50:42 -0700

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:each) do
-    user1 = User.create!(name: "Timmy Turner", email: "timmyturner1@gmail.com")
-    user2 = User.create!(name: "Wanda", email: "ilovecosmo1@gmail.com")
-    user3 = User.create!(name: "Cosmo", email: "ilovewanda1@gmail.com")
+  describe 'relationships' do
   end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:email) }
+  end
+
+  before(:each) do
+    @user1 = User.create!(name: 'Timmy Turner', email: 'timmyturner1@gmail.com')
+    @user2 = User.create!(name: 'Wanda', email: 'ilovecosmo1@gmail.com')
+    @user3 = User.create!(name: 'Cosmo', email: 'ilovewanda1@gmail.com')
+  end
 end
