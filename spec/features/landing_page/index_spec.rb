d94d3628e674c8428c4768ad21b810a29070bb82
Scott DeVoss <scottdevoss@yahoo.com>
2023-11-28 15:11:42 -0700

    added user story 1 test framework

---
+++ spec/features/landing_page/index_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "Landing Page Index", type: :feature do
+  describe "When I visit '/'" do
+    it "should show the title of application" do
+
+    end
+
+    it "should have a button to create a new user" do
+
+    end
+
+    it "should have a list of exisiting users which links to the users dashboard" do
+
+    end
+
+    it "should have a link to go back to the landing page (expected on every page)" do
+      
+    end
+  end
+end
