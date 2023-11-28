a9b55ba55995d9f9a6030bbbaf70d5553199a137
Kam Kennedy <kameronk013@gmail.com>
2023-11-28 16:24:29 -0700

    all tests passing

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -4,12 +4,11 @@ require 'rails_helper'
 
 RSpec.describe ViewingParty, type: :model do
   describe 'relationships' do
-    it { should belong_to(:user) }
     it { should have_many(:users).through(:user_viewing_parties) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:movie_id) }
     it { should validate_presence_of(:start_time) }
   end
 end
