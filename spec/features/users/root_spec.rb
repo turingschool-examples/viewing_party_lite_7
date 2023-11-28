325c79b37c729af572e40e47fe1be20e5b4bd2ba
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-27 17:31:24 -0700

    Write tests for landing page user story

---
+++ spec/features/users/root_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe "landing page" do
+  before :each do
+    @user1 = User.create!(name: "Shawn", email: "shawn@website.com")
+    @user2 = User.create!(name: "Anthea", email: "anthea@website.com")
+    visit root_path
+  end
+
+  it "has the title of the application" do
+    expect(page).to have_content("Viewing Party")
+  end
+
+  it "has a button to create a new user" do
+    expect(page).to have_button("Create New User")
+  end
+
+  it "has a list of existing users which links to the users dashboard" do
+    expect(page).to have_content(@user1.name)
+    expect(page).to have_content(@user2.name)
+    expect(page).to have_link("All Users")
+
+    click_link("All Users")
+
+    expect(current_path).to eq(users_path)
+  end
+
+  it "links to go back to the landing page" do
+    expect(page).to have_link("Go Home")
+
+    click_link("Go Home")
+
+    expect(current_path).to eq(root_path)
+  end
+end
