671ae8c43ad7fdca50b517e359d3545d7b7ae3f7
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-11-27 21:41:35 -0700

    test: Add User dashboard spec and user user factory

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'User Show Page', type: :feature do
+  describe 'when a user visits the users dashboard' do
+    it 'displays the users name and Dashboard on the top of the page' do
+      @user = create(:user)
+
+      visit user_path(@user)
+
+      expect(page).to have_content("#{@user.name}"'s Dashboard')
+    end
+  end
+end
