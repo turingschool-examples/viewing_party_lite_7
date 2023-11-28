e3c1f6ae6f61661bc69f76bcfc6cdf353619245a
Kam Kennedy <kameronk013@gmail.com>
2023-11-27 21:38:28 -0700

    progress on model specs

---
+++ app/models/movie.rb
@@ -0,0 +1,3 @@
+class Movie < ApplicationRecord
+  has_many :viewing_parties
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :viewing_parties
+  has_many :user_viewing_parties
+  has_many :viewing_parties, through: :user_viewing_parties
+end
