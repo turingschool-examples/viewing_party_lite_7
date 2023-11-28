ef93fcf89e481f415158efa542133b71d7ff3766
Allan Evans <oneblankpage27@gmail.com>
2023-11-28 15:59:11 -0700

    Added facades and tests with webmock

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "bootsnap", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
+gem 'faraday'
+gem 'figaro'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -71,4 +73,5 @@ group :test do
   gem "launchy"
   gem "shoulda-matchers"
   gem "simplecov"
+  gem 'webmock'
 end
--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -6,20 +6,20 @@ RSpec.describe 'Movie Results' do
   end
   describe 'happy path' do
     it 'Get top 20 rated movies' do
+      json_response = File.read('spec/fixtures/top_twenty_movies.json')
+      stub_request(:get, "https://api.themoviedb.org/3/movie/popular?api_key=#{Rails.application.credentials.tmdb[:key]}").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v2.7.12'
+           }).
+         to_return(status: 200, body: json_response, headers: {})
+
       visit "/users/#{@user1.id}/discover"
     
       expect(page).to have_button("Discover Top Rated Movies")
       expect(page).to have_button("Search by Movie Title")
-
-      # json_response = File.read('spec/fixtures/top_twenty_movies.json')
-      # stub_request(:get, "https://api.themoviedb.org/3/movie/popular?api_key=2c14ab06be3bfe4ca5fecd0f9b2c73fc").
-      #    with(
-      #      headers: {
-      #     'Accept'=>'*/*',
-      #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      #     'User-Agent'=>'Faraday v2.7.11'
-      #      }).
-      #      to_return(status: 200, body: json_response, headers: {})
 
       click_button "Discover Top Rated Movies"
 
@@ -27,9 +27,6 @@ RSpec.describe 'Movie Results' do
 
       expect(page).to have_link("The Equalizer 3")
       expect(page).to have_content(7.3)
-
-      # save_and_open_page
-      
     end
 
     it 'Get searched movies' do
@@ -38,15 +35,15 @@ RSpec.describe 'Movie Results' do
       expect(page).to have_button("Discover Top Rated Movies")
       expect(page).to have_button("Search by Movie Title")
 
-      # json_response = File.read('spec/fixtures/batman_movies.json')
-      # stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=2c14ab06be3bfe4ca5fecd0f9b2c73fc&query=Batman").
-      #    with(
-      #      headers: {
-      #     'Accept'=>'*/*',
-      #     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      #     'User-Agent'=>'Faraday v2.7.11'
-      #      }).
-      #    to_return(status: 200, body: json_response, headers: {})
+      json_response = File.read('spec/fixtures/batman_movies.json')
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{Rails.application.credentials.tmdb[:key]}&query=Batman").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v2.7.12'
+           }).
+         to_return(status: 200, body: json_response, headers: {})
 
       fill_in 'Movie_Title', with: "Batman"
       click_button "Search by Movie Title"
@@ -55,9 +52,6 @@ RSpec.describe 'Movie Results' do
 
       expect(page).to have_link("Batman Begins")
       expect(page).to have_content(7.2)
-
-      # save_and_open_page
-      
     end
   end
 end
