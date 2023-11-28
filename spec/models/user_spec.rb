c16d460b4613a6a5f37fbac759cf224c8d7a34b0
Charles Ren <cren04@syr.edu>
2023-11-27 22:50:51 -0800

    merge user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
   end
+
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      pending 'Not implemented'
+    end
+  end
 end
