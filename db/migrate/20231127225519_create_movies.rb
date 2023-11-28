e05c4ae4d0d509cae99f0891b033f9f087e33865
Taylor Pubins <tpubz@icloud.com>
2023-11-27 17:14:12 -0700

    Add migrations and schemas
    Co-authored-by: Charles Ren <135576410+chuckrenny@users.noreply.github.com>

---
+++ db/migrate/20231127225519_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :duration
+      t.string :genre
+      t.float :vote
+      t.string :summary
+      t.string :cast  #, array: true
+      t.string :reviews  #, array: true
+
+      t.timestamps
+    end
+  end
+end
