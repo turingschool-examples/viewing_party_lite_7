4892dad6d6429dd762c2140e52dab70dc5fef2b1
Eric Belongea <eric.belongea@gmail.com>
2023-11-27 17:15:32 -0700

    Add models, controllers, tests, and more.
    Co-authored-by: Capitainlearyo <clayoleary@gmail.com>

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+user1 = User.create!(name: "Capitainlearyo", email: "example1@yahoo.com")
+user2 = User.create!(name: "Slick Ric", email: "uhhh@gmail.com")
---
+++ spec/test_helper.rb
@@ -0,0 +1,4 @@
+def load_test_data
+  user1 = User.create!(name: "Capitainlearyo", email: "example1@yahoo.com")
+  user2 = User.create!(name: "Slick Ric", email: "uhhh@gmail.com")
+end
