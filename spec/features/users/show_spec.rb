a05344abc7c5c7ab1f266b26fe0dbda2494e542d
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-11-28 15:08:35 -0700

    test: Build out spec test for the Discover Movies button on the Users show page redirecting to the User Discover Index page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'User Show Page', type: :feature do
   describe 'when a user visits the users dashboard' do
-    it 'displays the users name and Dashboard on the top of the page' do
+    before(:each) do
       @user = create(:user)
-
+    end
+    
+    it 'displays the users name and Dashboard on the top of the page' do
       visit user_path(@user)
 
       expect(page).to have_content("#{@user.name}'s Dashboard")
+    end
+
+    it 'displays a button to discover movies which redirects the user to the users discover page' do
+      visit user_path(@user.id)
+
+      expect(page). to have_button("Discover Movies")
+
+      click_on("Discover Movies")
+
+      expect(current_path).to eq(user_discover_index_path(@user))
     end
   end
 end
