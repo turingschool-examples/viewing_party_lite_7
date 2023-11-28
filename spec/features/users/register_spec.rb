0f6df3c35b0827ffbfdb94502c7a1f990c56bbc2
Anthea Yur <acyur6@gmail.com>
2023-11-27 20:40:26 -0600

    Create test_data method for instantiating test data and complete Register Page requirements

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe "Register Page" do
   before :each do
+    test_data
     visit register_path
   end
 
@@ -15,7 +16,7 @@ describe "Register Page" do
     fill_in :name, with: "Janet Love"
     fill_in :email, with: "janetlovescooking@aol.com"
     click_button "Register"
-    # expect(current_path).to eq(user_path()) ## Not sure how to test the exact path on this one?
+    expect(current_path).to eq(user_path(User.last))
     expect(page).to have_content("Janet Love")
     expect(page).to have_content("janetlovescooking@aol.com")
   end
