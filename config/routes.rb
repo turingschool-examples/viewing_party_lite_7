bc5956fdcb582c9f1e1ff78fe8ac02a56c2b2ff6
Eric Belongea <eric.belongea@gmail.com>
2023-11-29 13:15:24 -0700

    Redo routing and config for new controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :users, only: [:new, :create, :show] do
-    get "/users/:id/discover", to: "movies#index"
+    # get "/users/:id/discover", to: "movies#index"
     # resources :discover, only: [:index, :search]
   end
-  
+  get "/users/:id/discover", to: "movies#index"
 end
