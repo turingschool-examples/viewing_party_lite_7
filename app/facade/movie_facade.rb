705d17602c3ce97ae21de681914a79a8c1b76e00
Eric Belongea <eric.belongea@gmail.com>
2023-11-29 14:01:40 -0700

    Make the movie poro

--- app/facade/movie_facade.rb
+++ app/facade/movie_facade.rb
@@ -1,10 +1,8 @@
 class MovieFacade
   def self.top_rated
     json_response = MovieService.top_rated[:results]
-    #top20 = json_response[0..20]
-    # @results = json_response.map do |movie|
-    #   Movie.new(movie)
-    # end
-
+    @results = json_response.map do |movie|
+      Movie.new(movie)
+    end.first(20)
   end
 end
--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,3 +1,17 @@
 class Movie
-  attr_reader :id, :title, :vote_average
+  attr_reader :id, :title, :vote_average, :overview, :popularity, :vote_average
+
+  def initialize(data)
+    @backdrop_path = data[:backdrop_path]
+    @genres = data[:genre_ids]
+    @id = data[:id]
+    @overview = data[:overview]
+    @popularity = data[:popularity]
+    @poster_path = data[:poster_path]
+    @release_date = data[:release_date]
+    @title = data[:title]
+    @vote_average = data[:vote_average]
+    @vote_count = data[:vote_count]
+    @runtime = data[:runtime]
+  end
 end
