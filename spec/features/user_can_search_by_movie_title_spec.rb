ba1e10d117ac75bd291ee27bc16198b9c21bd106
Sam Tran <samuelttran@gmail.com>
2023-11-29 16:01:43 -0700

    Remove old fixtures

--- spec/features/user_can_search_by_movie_title_spec.rb
+++ spec/features/user_can_search_by_movie_title_spec.rb
@@ -6,18 +6,6 @@ RSpec.describe 'user can search for movie by movie title', type: :feature do
   end
 
   it 'Allows user to search by title', :vcr do
-    # json_response = File.read('spec/fixtures/search_by_movie_title_star_wars.json')
-
-    # stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=d6e55850c9454bdba00520c7a05b8b40&query=Star%20Wars").
-    # with(
-    #   headers: {
-    #   'Accept'=>'*/*',
-    #   'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-    #   'User-Agent'=>'Faraday v2.7.12'
-    #   }).
-    # to_return(status: 200, body: json_response, headers: {})
-
-
     visit user_discover_index_path(@user)
 
     fill_in :query, with: 'Star Wars'
