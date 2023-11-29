03f8d26377cd749106d1e0bf83ce236dc706d3ce
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-11-29 12:00:44 -0700

    Implement API key and partial testing

---
+++ spec/features/users/movies/discover_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe "Movies" do
+  before :each do
+    test_data 
+  end
+
+  it "A user's show page has button to discover movies, links to an index of all movies" do
+    visit "/users/#{@user1.id}"
+
+    click_button "Discover Movies"
+    expect(current_path).to eq("/users/#{@user1.id}/discover")
+  end
+
+  it "A user's show page has button to discover movies, links to an index of all movies" do
+    visit "/users/#{@user1.id}/discover"
+
+    expect(page).to have_content("Viewing Party Lite")
+    expect(page).to have_content("Discover Movies")
+    expect(page).to have_button("Find Top Rated Movies")
+    expect(page).to have_link("Home")
+    expect(page).to have_button("Find Movies")
+    expect(page).to have_field(:movie_title)
+  end
+
+  # it "A user's show page has button to discover movies, links to an index of all movies" do
+  #   visit "/users/#{@user1.id}/discover"
+
+  # end
+end
