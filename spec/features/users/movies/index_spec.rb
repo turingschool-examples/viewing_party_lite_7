c6edfb346841f09046495a8ce65bbb8b8e97bd31
Nathan Trautenberg <Natester123@mac.com>
2023-11-29 09:42:47 -0700

    realized movie discover page for a user.

--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe "Movies" do
     click_button "Discover Movies"
     expect(current_path).to eq("/users/#{@user1.id}/discover")
   end
+
+  it "A user's show page has button to discover movies, links to an index of all movies" do
+    visit "/users/#{@user1.id}/discover"
+
+    expect(page).to have_content("Viewing Party Lite")
+    expect(page).to have_content("Discover Movies")
+    expect(page).to have_button("Find Top Rated Movies")
+    expect(page).to have_link("Home")
+    expect(page).to have_button("Find Movies")
+    expect(page).to have_field(:movie_title)
+  end
+
+  # it "A user's show page has button to discover movies, links to an index of all movies" do
+  #   visit "/users/#{@user1.id}/discover"
+
+  # end
 end
