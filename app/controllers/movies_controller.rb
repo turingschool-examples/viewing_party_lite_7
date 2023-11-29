0b59f679e9e1b34565133fc2c1e584bc2dc176ea
Scott DeVoss <scottdevoss@yahoo.com>
2023-11-29 16:51:11 -0700

    passed first 3 tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,15 @@
 class MoviesController < ApplicationController
-  # def top_movies
-  #   conn = Faraday.new("https://api.themoviedb.org") do |faraday|
-  #     faraday.params["api_key"] = Rails.application.credentials.tmdb[:key]
-  #   end
-  #   response = conn.get("/3/movie/top_rated?page=1")
 
-  #   data = JSON.parse(response.body)
-  #   # require 'pry';binding.pry
-  # end
+  def top_movies
+    conn = Faraday.new("https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = Rails.application.credentials.tmdb[:key]
+    end
+    response = conn.get("/3/movie/top_rated?page=1")
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    @movies = data[:results]
+   
+  end
 
   # def search
   #   movie = params[:search]
--- spec/features/movie_results/index_spec.rb
+++ spec/features/movie_results/index_spec.rb
@@ -8,26 +8,42 @@ RSpec.describe "Movie Results Page", type: :feature do
 
   describe "When I visit '/users/:id/discover'" do
     describe "And click on the 'Top Movies' or 'Search'" do
-      xit "I am taken to the movies results page where I see 'Title'(As a link to the Movie Details page - max 20)" do
+      it "I am taken to the movies results page where I see 'Title'(As a link to the Movie Details page - max 20)" do
         visit "/users/#{@user1.id}/discover"
 
         click_button "Find Top Rated Movies"
 
         expect(current_path).to eq "/users/#{@user1.id}/movies"
+
+        
       end
       
     end
   end 
 
   describe "When I visit '/users/:id/movies'" do
-    xit "I see the 'Vote Average of the movies'" do
+
+    it "I see the Titles as links to their movie detail page" do
       visit "/users/#{@user1.id}/movies"
-
-      expect(page).to have_content()
-
+      
+      expect(page).to have_link("The Godfather")
+      expect(page).to have_link("The Shawshank Redemption")
+      expect(page).to have_link("The Godfather Part II")
+      
+      expect("The Godfather").to appear_before("The Shawshank Redemption")
+      
     end
 
-    xit "should have a button to return to the Discover Page" do
+    it "I see the 'Vote Average of the movies'" do
+      visit "/users/#{@user1.id}/movies"
+
+      expect(page).to have_content("Vote Average: 8.708")
+      expect(page).to have_content("Vote Average: 8.705")
+      expect(page).to have_content("Vote Average: 8.59")
+      
+    end
+
+    it "should have a button to return to the Discover Page" do
       visit "/users/#{@user1.id}/movies"
 
       expect(page).to have_button("Discover Page")
