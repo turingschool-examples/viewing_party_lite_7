b377f4b3a090e8267ea320942f8fd1bc31991656
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-29 16:40:13 -0700

    Refactor top_movies in movies_search service to utilize helper methods

--- services/movies_search.rb
+++ services/movies_search.rb
@@ -1,11 +1,7 @@
 class MoviesSearch
   # should probably refactor conn and get into helper methods
   def top_movies
-    conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
-      faraday.headers[:Authorization] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
-    end
-    response = conn.get('movie/top_rated')
-    parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed = get_url('movie/top_rated')
     parsed[:results].map do |movie|
       Movie.new(movie)
     end
@@ -18,8 +14,31 @@ class MoviesSearch
     end
     response = conn.get('search/movie')
     parsed = JSON.parse(response.body, symbolize_names: true)
-    @search_result = parsed[:results].map do |movie|
+    parsed[:results].map do |movie|
       Movie.new(movie)
     end
   end
+
+  def conn
+    conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
+      faraday.headers[:Authorization] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
+    end
+  end
+
+  # def search_conn(keyword)
+  #   Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
+  #     faraday.headers[:Authorization] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
+  #     faraday.params[:query] = keyword
+  #   end
+  # end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  # def get_search_url(url)
+  #   response = search_conn.get(url)
+  #   JSON.parse(response.body, symbolize_names: true)
+  # end
 end
