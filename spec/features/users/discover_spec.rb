44880bdb4fa4d54e1042e6569febeca359f45f41
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-29 12:01:43 -0700

    Test:New tests for discover_spec

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe 'discover movies' do
     @user2 = User.create!(name: 'Paul', email: 'paul@turing.edu')
     @user3 = User.create!(name: 'Sooyung', email: 'sooyung@turing.edu')
   end
-
+  it 'click find top rated movies' do
+    visit discover_user_path(@user1.id)
+    click_button "Find Top Rated Movies"
+    expect(current_path).to eq(user_movies_path(@user1.id))
+  end
+  it 'should have a search button' do
+    visit discover_user_path(@user1.id)
+    expect(find_field('search').value).to eq(nil)
+    click_button 'Find Movies'
+    expect(current_path).to eq(user_movies_path(@user1.id))
+  end
 end
