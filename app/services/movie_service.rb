8fb3cd24db074000c6f520aa77049a89edfc7b81
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-29 11:54:18 -0700

    Create get detailed movie service to parse details endpoint

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -3,11 +3,10 @@ class MovieService
     conn = Faraday.new("https://api.themoviedb.org/3/")
 
     response = conn.get("movie/popular") do |faraday|
-      faraday.params["api_key"] = ENV["API_KEY"]
+      faraday.params["api_key"] = "67af88004fc9a5fe47497bb47e0dc073"
     end
 
-    movie_data = JSON.parse(response.body, symbolize_names: :true)
-    
+    movie_data = JSON.parse(response.body, symbolize_names: :true)  
     movie_data = movie_data[:results].map do |movie_hash|
       Movie.new(movie_hash)
     end
@@ -17,14 +16,24 @@ class MovieService
     conn = Faraday.new("https://api.themoviedb.org/3/")
 
     response = conn.get("search/movie") do |faraday|
-      faraday.params["api_key"] = ENV["API_KEY"]
+      faraday.params["api_key"] = "67af88004fc9a5fe47497bb47e0dc073"
       faraday.params["query"] = search_params
     end
 
-    movie_data = JSON.parse(response.body, symbolize_names: :true)
-    
+    movie_data = JSON.parse(response.body, symbolize_names: :true)   
     movie_data = movie_data[:results].map do |movie_hash|
       Movie.new(movie_hash)
     end
+  end
+
+  def self.get_detailed_movie(id)
+    conn = Faraday.new("https://api.themoviedb.org/3/")
+
+    response = conn.get("movie/#{id}") do |faraday|
+      faraday.params["api_key"] = "67af88004fc9a5fe47497bb47e0dc073"
+    end
+
+    movie_data = JSON.parse(response.body, symbolize_names: :true)   
+    DetailedMovie.new(movie_data)
   end
 end
