8b308c65fbc7b70e0165b69600048f47267119af
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-29 12:33:33 -0700

    Create get movie cast service to parse cast endpoint data

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -36,4 +36,17 @@ class MovieService
     movie_data = JSON.parse(response.body, symbolize_names: :true)   
     DetailedMovie.new(movie_data)
   end
+
+  def self.get_movie_cast(id)
+    conn = Faraday.new("https://api.themoviedb.org/3/")
+
+    response = conn.get("movie/#{id}/credits") do |faraday|
+      faraday.params["api_key"] = "67af88004fc9a5fe47497bb47e0dc073"
+    end
+
+    cast_data = JSON.parse(response.body, symbolize_names: :true)   
+    cast_data[:cast].map do |actor_hash|
+      MovieActor.new(actor_hash)
+    end
+  end
 end
