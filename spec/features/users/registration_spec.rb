52cf8d8bbbee17810d2d1875bfdadb288762a71b
Xander Hendry <vhendry.coding@gmail.com>
2023-11-28 20:51:47 -0500

    add spec files and tests for User model, and User registration page. model tests cover validation, registration tests cover presence of content, and functionality of form, including sad path

---
+++ spec/features/users/registration_spec.rb
@@ -0,0 +1,28 @@
+# frozen_sting_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'User Registration Page' do
+  it 'displays a form to register a new User' do
+    visit '/register'
+    expect(page).to have_field(:name)
+    expect(page).to have_field(:email)
+    expect(page).to have_button('Register')
+  end
+  it 'will add a new User to the website' do
+    visit '/register'
+    fill_in :name, with: 'Xander Hendry'
+    fill_in :email, with: 'xanders_fake_email@gmail.com'
+    click_button('Register')
+    new_user = User.last
+    expect(current_path).to eq("/users/#{new_user.id}")
+  end
+  it 'will only register unique emails' do
+    visit '/register'
+    fill_in :name, with: 'Xander Hendry'
+    fill_in :email, with: 'xanders_fake_email@gmail.com'
+    click_button('Register')
+    expect(current_path).to eq('/register')
+    expect(page).to have_content('Error: Email already in use!')
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:email) }
+  end
+end
