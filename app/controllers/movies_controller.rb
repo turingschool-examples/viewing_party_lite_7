5435dc8437ff75dc563cf34630ba674f4b08f6b6
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-28 20:13:40 -0700

    Add parameters to movies controller index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def index
-    
+    if params[:search_by].present?
+      # shortened to search by until I can get movie_search working
+      @movies = MovieService.get_movies.first.movie_search(params[:search_by_title])
+    else
+      @movies = MovieService.get_movies.first(20)
+    end
   end
 end
