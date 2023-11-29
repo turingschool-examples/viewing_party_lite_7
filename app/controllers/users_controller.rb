bd311df1eedbfb061ebcbb23c92f148306f40fa2
Kam Kennedy <kameronk013@gmail.com>
2023-11-29 10:24:14 -0700

    progress on landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "users/:id", to: "users#show"
+  get '/', to: 'users#index', as: :root
+  get 'users/:id', to: 'users#show'
+  post '/users/new', to: 'users#new', as: :new_user_path
   get '/users/:id/discover', to: 'users/discovers#index', as: :user_discover
 end
