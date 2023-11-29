edce0f22f3092cbe36f078b74e9ad69e6be5710b
Xander Hendry <vhendry.coding@gmail.com>
2023-11-29 12:25:21 -0500

    update email validation to be case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 end
--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -19,10 +19,12 @@ RSpec.describe 'User Registration Page' do
   end
   it 'will only register unique emails' do
     visit '/register'
+    User.create({ name: 'Xander Hendry', email: 'xanders_fake_email@gmail.com' })
     fill_in :name, with: 'Xander Hendry'
     fill_in :email, with: 'xanders_fake_email@gmail.com'
     click_button('Register')
     expect(current_path).to eq('/register')
+    save_and_open_page
     expect(page).to have_content('Error: Email already in use!')
   end
 end
