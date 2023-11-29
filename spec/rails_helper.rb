288cf8a426f373750bd4e6fa59c7b308056ff544
Sooyung Kim <kimx1279@gmail.com>
2023-11-29 14:40:20 -0700

    Uncomment VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,10 +65,10 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-# VCR.configure do |config|
-#   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-#   config.hook_into :webmock
-# end
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
