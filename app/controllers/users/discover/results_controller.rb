441a04eb235465bbd453291f1771201b95c7d019
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-29 09:49:08 -0700

    Modify user discover results controller for movie search functionality

--- app/controllers/users/discover/results_controller.rb
+++ app/controllers/users/discover/results_controller.rb
@@ -3,8 +3,12 @@ class Users::Discover::ResultsController < ApplicationController
     conn = Faraday.new(url: 'https://api.themoviedb.org/') do |faraday|
       faraday.params['api_key'] = Rails.application.credentials.tmdb[:key]
     end
-    response = conn.get('3/movie/top_rated')
-
-    @top_rated = JSON.parse(response.body, symbolize_names: true)
+    if params[:top_rated].present?
+      response = conn.get('3/movie/top_rated')
+      @top_rated = JSON.parse(response.body, symbolize_names: true)
+    elsif params[:search].present?
+      response = conn.get('')
+      @search_result = JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
