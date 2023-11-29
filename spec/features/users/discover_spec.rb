a144fb5e50da26f5ae779eeaf34a2fcf812ff6a3
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-29 11:57:29 -0700

    Update:Remove redundant tests

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -6,16 +6,5 @@ RSpec.describe 'discover movies' do
     @user2 = User.create!(name: 'Paul', email: 'paul@turing.edu')
     @user3 = User.create!(name: 'Sooyung', email: 'sooyung@turing.edu')
   end
-  it 'should have a top movies button' do
-    visit discover_user_path(@user1.id)
-    expect(page).to have_button('Top Movies')
-    click_button 'Top Movies'
-    expect(current_path).to eq(user_movies_path(@user1.id))
-  end
-  it 'should have a search button' do
-    visit discover_user_path(@user1.id)
-    expect(page).to have_button('Search')
-    expect(find_field('search').value).to eq(nil)
-    click_button 'Search'
-  end
+
 end
