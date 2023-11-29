290bff9e6c079c203a8e256dfa6650fd5aa8283b
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-29 12:08:47 -0700

    Functionalize results controller index action

--- app/controllers/users/discover/results_controller.rb
+++ app/controllers/users/discover/results_controller.rb
@@ -1,19 +1,25 @@
+require './poros/movie'
+
 class Users::Discover::ResultsController < ApplicationController
   def index
-    conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
-      faraday.headers[:Authorization] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
-    end
     # should use service to implement these (research implementation)
     if params[:top_rated].present?
+      conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
+        faraday.headers[:Authorization] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
+      end
       response = conn.get('movie/top_rated')
       parsed = JSON.parse(response.body, symbolize_names: true)
-      @top_rated = parsed.map do |movie|
+      @top_rated = parsed[:results].map do |movie|
         Movie.new(movie)
       end
     elsif params[:search].present?
+      conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
+        faraday.headers[:Authorization] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
+        faraday.params[:query] = keyword
+      end
       response = conn.get('search/movie')
       parsed = JSON.parse(response.body, symbolize_names: true)
-      @search_result = parsed.map do |movie|
+      @search_result = parsed[:results].map do |movie|
         Movie.new(movie)
       end
     end
--- services/movies_service.rb
+++ services/movies_service.rb
@@ -5,16 +5,21 @@ class MoviesSearch
       faraday.headers[:Authorization] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
     end
     response = conn.get('movie/top_rated')
-    top_rated = JSON.parse(response.body, symbolize_names: true)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed[:results].map do |movie|
+      Movie.new(movie)
+    end
   end
 
-  # need to add param for keyword
   def search_movies(keyword)
     conn = Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
       faraday.headers[:Authorization] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
       faraday.params[:query] = keyword
     end
     response = conn.get('search/movie')
-    search_result = JSON.parse(response.body, symbolize_names: true)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    @search_result = parsed[:results].map do |movie|
+      Movie.new(movie)
+    end
   end
 end
