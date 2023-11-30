c511899fbd0a1027f0e6d57c450fcf08f1b13277
pcbennett108 <pcbennett42@gmail.com>
2023-11-29 22:15:33 -0700

    Add test for movie details facade

---
+++ spec/facades/movie_details_facade_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe MovieDetailsFacade do
+  describe "facade returns details", :vcr do
+    before(:each) do
+      @movie_id = 438631
+      @facade = MovieDetailsFacade.new(@movie_id)
+    end
+    
+    it 'exists' do
+      expect(@facade).to be_an_instance_of(MovieDetailsFacade)
+      expect(@facade.movie_details).to be_an_instance_of(MovieDetails)
+    end
+
+    describe "#details" do
+      it "returns a newly created MoviesDetails poro" do
+        expect(@facade.details).to be_an_instance_of(MovieDetails)
+      end
+    end
+  end
+end
