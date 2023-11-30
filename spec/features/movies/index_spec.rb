61473dfa152cd411fc02618795f977434187a664
Eric Belongea <eric.belongea@gmail.com>
2023-11-29 20:58:13 -0700

    save

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -12,9 +12,5 @@ RSpec.describe "Movies Index Page" do
 
     expect(page).to have_button("Top Rated Movies")
     expect(page).to have_button("Find Movies")
-
-
-    # click_button("Top Rated Movies")
-    # expect(current_path).to eq("/users/#{@user2.id}/movies")
   end
 end
