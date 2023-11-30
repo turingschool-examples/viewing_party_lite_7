66f36e4dcceb8c89525e4ae2b5820050733bba3f
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-29 17:11:43 -0700

    Write incomplete test suite for movie_search service

---
+++ spec/services/movie_search_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe MoviesSearch do
+  before(:each) do
+    
+  end
+
+  describe '#top_movies' do
+    it 'returns a nested hash of the top rated movies' do
+      
+    end
+  end
+
+  describe '#search_movies' do 
+    it 'returns a nested hash of search results for a movie title' do
+      
+    end
+  end
+
+  describe '#conn' do
+    it 'establishes a connection with TMDB API' do
+      
+    end
+  end
+
+  describe '#get_url' do
+    it 'returns a parsed response for a valid API endpoint' do
+      
+    end
+  end
+end
