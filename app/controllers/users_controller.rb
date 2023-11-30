f519c37c8332d0a2378484af7919d8db826a620d
Joseph Lee <jhjlee702@gmail.com>
2023-11-29 21:34:49 -0700

    make sure all tests pass after the merge conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
   end
 
   def new
-    @user = User.create(user_params)
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_dashboard_path(@user)
+    else
+      render :new
+    end
   end
 
   private
 
   def user_params
-    params.permit(:name, :email)
+    params.require(:user).permit(:name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/', to: 'users#index', as: :root
-  get 'users/:id', to: 'users#show'
-  post '/users/new', to: 'users#new', as: :new_user_path
-  get '/users/:id/discover', to: 'users/discovers#index', as: :user_discover
+  get 'users/:id', to: 'users#show', as: :user_dashboard
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+  get '/users/:id/discover', to: 'users/discover#index', as: :user_discover
 end
