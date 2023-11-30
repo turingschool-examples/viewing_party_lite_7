2cef8046abc8a40bb656fc4ed081d2cf85a30ee6
Joseph Lee <jhjlee702@gmail.com>
2023-11-30 15:20:47 -0700

    figure out the api consumption

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,38 @@
 class MoviesController < ApplicationController
   def index
-    @user = User.find(params[:id])
+    if params[:q] == "top-rated"
+      @user = User.find(params[:id])
+
+      conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+        faraday.headers["authorization"] = Rails.application.credentials.TMDB[:authorization]
+      end
+
+      response = conn.get("/3/movie/top_rated")
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      @movies = json[:results].map do |movie_data|
+        Movie.new(movie_data)
+      end
+    else
+    end
+
+    if params[:search].present?
+      @user = User.find(params[:id])
+
+      search_term = params[:search] 
+
+      conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+        faraday.headers["authorization"] = Rails.application.credentials.TMDB[:authorization]
+      end
+
+      response = conn.get("/3/search/movie?query=#{search_term}")
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      @movies = json[:results].map do |movie_data|
+        Movie.new(movie_data)
+      end
+    end
   end
 end
