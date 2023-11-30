8f1191bf69b5ea8cdfc4a4bc7dcb2154e8d97795
Anthea Yur <acyur6@gmail.com>
2023-11-29 21:59:39 -0600

    Removes @@movies that was being used for reference

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,15 +1,10 @@
 class Movie
   attr_reader :title, :vote_average, :id
-  @@movies = []
 
   def initialize(movie_data)
     @title = movie_data[:original_title]
     @vote_average = movie_data[:vote_average]
     @id = movie_data[:id]
-    @@movies << self
   end
 
-  def self.all
-    @@movies
-  end
 end
