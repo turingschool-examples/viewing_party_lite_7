4ab8f3ec9bf5d5a6b4af1516efa280efe896ce47
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-30 09:48:43 -0700

    Write controller for parties new page

--- app/controllers/users/discover/results/parties_controller.rb
+++ app/controllers/users/discover/results/parties_controller.rb
@@ -1,16 +1,20 @@
 class Users::Discover::Results::PartiesController < ApplicationController
   def new
-    
+    movies_search = MoviesSearch.new
+    @movie = movies_search.genre_runtime(params[:movie_id])
   end
 
   def create
-    @party = Party.new(party_params)
+    party = Party.new(party_params)
+    user = User.find(params[:user_id])
+    movies_search = MoviesSearch.new
+    movie = movies_search.genre_runtime(params[:movie_id])
 
-    if @party.save
-      redirect_to 
-    else
+    if party.save
+      redirect_to user_path(user)
+    elsif # blank field, duration compare to movie
       flash[:alert] = ''
-      redirect_to 
+      redirect_to new_user_movie_party_path(user, movie)
     end
   end
 
