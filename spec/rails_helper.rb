363f9a939abfa5100de61ebf4d4dcecb5dd42785
pcbennett108 <pcbennett42@gmail.com>
2023-11-29 19:58:57 -0700

    Add keys filter and metadata configure to VCR configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,6 +68,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.filter_sensitive_data('<TMDB_API_KEY>') { Rails.application.credentials.tmdb[:key] }
+  config.configure_rspec_metadata!
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
