7d6b33b163ef2fcdc147cf83c39decd1675d7c94
Charles Ren <cren04@syr.edu>
2023-11-30 15:29:58 -0800

    dashboard viewing party test spec

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,8 +3,7 @@ require "rails_helper"
 RSpec.describe User, type: :feature do
   before :each do
     @user_1 = create(:user)
-    @user_2 = create(:user)
-    @user_3 = create(:user)
+    @movie = nil # godfather id is 238
   end
 
   describe "User Dashboard Page" do
@@ -14,6 +13,31 @@ RSpec.describe User, type: :feature do
 
       expect(page).to have_content("#{@user_1.name}'s Dashboard")
       expect(page).to have_button("Discover Movies")
+    end
+
+    # As a user,
+    # When I visit a user dashboard,
+    # I should see the viewing parties that the user has been invited to with the following details:
+
+    # Movie Image
+    # Movie Title, which links to the movie show page
+    # Date and Time of Event
+    # who is hosting the event
+    # list of users invited, with my name in bold
+    # I should also see the viewing parties that the user has created with the following details:
+
+    # Movie Image
+    # Movie Title, which links to the movie show page
+    # Date and Time of Event
+    # That I am the host of the party
+    # List of friends invited to the viewing party
+    describe "displays the viewing parties the user has been invited to" do
+      it "should display the movie image, title as a redirect link," do
+        visit user_path(@user_1)
+
+
+      end
+
     end
   end
 end
---
+++ spec/features/viewing_parties/new_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe "New Viewing Party Page" do
+  before do
+    VCR.use_cassette("godfather-movie-details") do
+      @user1 = User.create(name: "TP", email: "tp@gmail.com")
+      @user2 = User.create(name: "Charles", email: "charles@gmail.com")
+      @user3 = User.create(name: "Chris", email: "chris@gmail.com")
+
+      @movie = MovieFacade.new(238).add_details # God Father ID
+
+      visit "/users/#{@user1.id}/movies/#{@movie.id}/viewing_parties/new"
+    end
+  end
+
+  describe "new viewing party form" do
+    it "displays the movie title above a form" do
+      expect(page).to have_content(@movie.title)
+    end
+
+    it "has a 'Duration of Party' field prefilled with the movie's runtime in minutes" do
+      expect(page).to have_field("Duration of Party")
+    end
+
+    it "has date and time selection fields" do
+      
+    end
+
+    it "has individual checkboxes to invite every other existing user" do
+      expect(page).to have_unchecked_field("#{@user2.name} (#{@user2.email})")
+      expect(page).to have_unchecked_field("#{@user3.name} (#{@user3.email})")
+
+      expect(page).to_not have_checked_field("#{@user1.name} (#{@user1.email})")
+    end
+
+    it "has a button to create a party" do
+      expect(page).to have_button("Create Party").once
+    end
+  end
+
+  describe "happy path" do
+    it "redirects to the user's dashboard where the new event is shown" do
+      check (@user2.name_email)
+      click_button("Create Party")
+
+      expect(current_path).to eq(user_path(@user1.id))
+      expect(page).to have_content(@movie.title)
+
+      visit user_path(@user2.id)
+      expect(page).to have_content(@movie.title)
+    end
+  end
+end
