a0358b49f622d6298353ad4b61ed9c11b99ee8b1
Sam Tran <samuelttran@gmail.com>
2023-11-29 19:11:39 -0700

    Add functionality and test for search results page

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -22,5 +22,26 @@ RSpec.describe 'Movie Results page, movies index', type: :feature do
       click_link 'The Godfather'
       expect(current_path).to eq("/users/#{@user.id}/movies/238") # ID is hard coded, need to think of better way to grab 
     end
+
+    it 'They search something, see max 20 results, Title as link to details page, and vote average', :vcr do
+      fill_in :query, with: 'Star Wars'
+      click_button 'Search'
+  
+
+  
+      expect(page).to have_content('Star Wars')
+      movie_titles = all('li.title')
+      movie_vote_averages = all('li.voteAverage')
+
+      expect(movie_titles.count).to be <= 20
+      expect(movie_vote_averages.count).to be <= 20
+
+      expect(current_path).to eq(user_movies_path(@user))
+      expect(page).to have_link('Star Wars')
+      expect(page).to have_content('Vote Average: 8.2')
+
+      click_link 'Star Wars'
+      expect(current_path).to eq("/users/#{@user.id}/movies/11") # ID is hard coded, need to think of better way to grab 
+    end
   end
 end
