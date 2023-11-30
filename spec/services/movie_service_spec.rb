22fe41a7e6d8183d26db705c4f39383a9a149049
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-30 16:41:10 -0700

    Write tests for detailed movie, movie service

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -25,7 +25,7 @@ describe MovieService do
     end
 
     context "#search_movies" do
-      it "returns movies that match search criteria" do
+      it "returns movies data that match search criteria" do
         search = MovieService.new.search_movies("Trolls")
         expect(search).to be_a Hash
         expect(search[:results]).to be_an Array
@@ -39,6 +39,31 @@ describe MovieService do
 
         expect(movie_data).to have_key :id
         expect(movie_data[:id]).to be_a Integer
+      end
+    end
+
+    context "#detailed_movie" do
+      it "returns a single detailed movi data from movie details endpoint" do
+        movie = MovieService.new.detailed_movie(872585)
+        expect(movie).to be_a Hash
+
+        expect(movie).to have_key :title
+        expect(movie[:title]).to be_a String
+
+        expect(movie).to have_key :vote_average
+        expect(movie[:vote_average]).to be_a Float
+
+        expect(movie).to have_key :id
+        expect(movie[:id]).to be_an Integer
+
+        expect(movie).to have_key :runtime
+        expect(movie[:runtime]).to be_a Integer
+
+        expect(movie).to have_key :genres
+        expect(movie[:genres]).to be_an Array
+
+        expect(movie).to have_key :overview
+        expect(movie[:overview]).to be_a String
       end
     end
 
