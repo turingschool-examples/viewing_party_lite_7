7fc46e3659c80c93e05b05c31ea869b76b67e8bc
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-30 07:23:28 -0700

    Generate migrations for parties and user_parties

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_27_231604) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_142249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "parties", force: :cascade do |t|
+    t.integer "duration"
+    t.string "date"
+    t.string "start_time"
+    t.integer "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_parties", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "party_id", null: false
+    t.boolean "host"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["party_id"], name: "index_user_parties_on_party_id"
+    t.index ["user_id"], name: "index_user_parties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_27_231604) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_parties", "parties"
+  add_foreign_key "user_parties", "users"
 end
