804c1da108a6a17335d9a572250c30088f4a0de8
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-30 15:43:53 -0700

    create detailed movie facades and add first 20 to popular movies

--- app/facades/popular_movies_facade.rb
+++ app/facades/popular_movies_facade.rb
@@ -6,8 +6,9 @@ class PopularMoviesFacade
     service = MovieService.new
 
     json = service.popular_movies
-    @movies = json[:results].map do |movie_data|
+    movies = json[:results].map do |movie_data|
       Movie.new(movie_data)
     end
+    @movies = movies.first(20)
   end
 end
