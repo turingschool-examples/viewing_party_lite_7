4e2101e6104a9e43b2402a2097456bbc1209c890
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-30 14:47:56 -0700

    Refactor movie service to use helper methods and move some functionality to facade

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,29 +1,21 @@
 class MovieService
-  def self.get_movies
-    conn = Faraday.new("https://api.themoviedb.org/3/")
-
-    response = conn.get("movie/popular") do |faraday|
+  def conn
+    Faraday.new("https://api.themoviedb.org/3/") do |faraday|
       faraday.params["api_key"] = ENV["API_KEY"]
-    end
-
-    movie_data = JSON.parse(response.body, symbolize_names: :true)  
-    movie_data = movie_data[:results].map do |movie_hash|
-      Movie.new(movie_hash)
     end
   end
 
-  def self.search_movies(search_params)
-    conn = Faraday.new("https://api.themoviedb.org/3/")
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: :true)
+  end
+  
+  def popular_movies
+   get_url("movie/popular")
+  end
 
-    response = conn.get("search/movie") do |faraday|
-      faraday.params["api_key"] = ENV["API_KEY"]
-      faraday.params["query"] = search_params
-    end
-
-    movie_data = JSON.parse(response.body, symbolize_names: :true)   
-    movie_data = movie_data[:results].map do |movie_hash|
-      Movie.new(movie_hash)
-    end
+  def search_movies(search_params)
+    get_url("search/movie?query=#{search_params}")
   end
 
   def self.get_detailed_movie(id)
