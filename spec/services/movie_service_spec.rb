b21ccc4299d8f7cf7bde4feff2957d9a6a135b73
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-30 16:26:13 -0700

    Create new fixture file and testing for search_movies movie service

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -24,6 +24,24 @@ describe MovieService do
       end
     end
 
+    context "#search_movies" do
+      it "returns movies that match search criteria" do
+        search = MovieService.new.search_movies("Trolls")
+        expect(search).to be_a Hash
+        expect(search[:results]).to be_an Array
+        movie_data = search[:results].first
+
+        expect(movie_data).to have_key :original_title
+        expect(movie_data[:original_title]).to be_a String
+
+        expect(movie_data).to have_key :vote_average
+        expect(movie_data[:vote_average]).to be_a Float
+
+        expect(movie_data).to have_key :id
+        expect(movie_data[:id]).to be_a Integer
+      end
+    end
+
     context "get_url" do
       it "returns parsed json data" do
         url = "movie/popular"
