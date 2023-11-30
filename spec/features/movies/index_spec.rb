5d2799f1505e014e9f5c4411121fc428c625b9fd
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-29 20:09:23 -0700

    Refactor:More rubocop

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'discover movies' do
-  before (:each) do
+  before(:each) do
     @user1 = User.create!(name: 'Brendan', email: 'brendan@turing.edu')
     @user2 = User.create!(name: 'Paul', email: 'paul@turing.edu')
     @user3 = User.create!(name: 'Sooyung', email: 'sooyung@turing.edu')
@@ -9,26 +11,26 @@ RSpec.describe 'discover movies' do
 
   it 'shows the top rated movies and their average rating' do
     json_response = File.read("spec/fixtures/top_rated.json")
-      parsed = JSON.parse(json_response, symbolize_names: true)
-      movies = parsed[:results]
-      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{Rails.application.credentials.tmdb[:key]}").
-        with(
-          headers: {
-            'Accept' => '*/*',
-            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'User-Agent' => 'Faraday v2.7.12'
-          }).
-        to_return(status: 200, body: json_response, headers: {})
-      visit discover_user_path(@user1)
-      expect(page).to have_button('Find Top Rated Movies')
-      click_button('Find Top Rated Movies')
-      expect(current_path).to eq(user_movies_path(@user1))
-      movies.each do |movie|
-        expect(movie[:title]).to be_a(String)
-        expect(page).to have_content(movie[:vote_average])
-      end
+    parsed = JSON.parse(json_response, symbolize_names: true)
+    movies = parsed[:results]
+    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{Rails.application.credentials.tmdb[:key]}")
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Faraday v2.7.12'
+        }
+      )
+      .to_return(status: 200, body: json_response, headers: {})
+    visit discover_user_path(@user1)
+    expect(page).to have_button('Find Top Rated Movies')
+    click_button('Find Top Rated Movies')
+    expect(current_path).to eq(user_movies_path(@user1))
+    movies.each do |movie|
+      expect(movie[:title]).to be_a(String)
+      expect(page).to have_content(movie[:vote_average])
     end
-
+  end
 
   it 'has a button to return to discover' do
     visit user_movies_path(@user2)
