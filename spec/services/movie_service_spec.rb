3c29b0bb181686a8915b12696fa3a022c171e790
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-30 16:09:08 -0700

    Write tests for movie service popular movies

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
 describe MovieService do
-  context "class methods" do
+  before :each do
+    oppenheimer_test_data
+  end
 
+  context "instance methods" do
+    context "#popular_movies" do
+      it "returns popular movie data" do
+        popular = MovieService.new.popular_movies
+        expect(popular).to be_a Hash
+        expect(popular[:results]).to be_an Array
+        movie_data = popular[:results].first
+
+        expect(movie_data).to have_key :original_title
+        expect(movie_data[:original_title]).to be_a String
+
+        expect(movie_data).to have_key :vote_average
+        expect(movie_data[:vote_average]).to be_a Float
+
+        expect(movie_data).to have_key :id
+        expect(movie_data[:id]).to be_a Integer
+      end
+    end
+
+    context "get_url" do
+      it "returns parsed json data" do
+        url = "movie/popular"
+        parsed_response = MovieService.new.get_url(url)
+        expect(parsed_response).to be_a(Hash)
+        expect(parsed_response[:results]).to be_an(Array)
+      end
+    end
+
+    context "#conn" do
+      it "returns Faraday object" do
+        service = MovieService.new
+        expect(service.conn).to be_a(Faraday::Connection)
+      end
+    end
   end
 end
