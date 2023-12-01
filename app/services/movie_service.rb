599a79826a8defdcb13a706e547f424c5a124b61
Xander Hendry <vhendry.coding@gmail.com>
2023-11-30 20:40:50 -0500

    update MovieService, create VCR cassettes, all tests passing

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -6,11 +6,10 @@ class MovieService
   def json_from_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
   end
 
   def conn
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
+    conn = Faraday.new(url: 'https://api.themoviedb.org/3') do |faraday|
       faraday.headers['authorization'] =
         Rails.application.credentials.tmdb[:access_token]
     end
--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe MovieService do
     describe '#json_from_url' do
       it 'returns parsed JSON data from given URL', :vcr do
         parsed_response = MovieService.new.json_from_url('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&with_cast=true&with_genres=true&with_people=true&with_runtime=true&page=1&sort_by=popularity.desc')
-        expect(parsed_response).to be_a Hash
+        expect(parsed_response).to be_a(Hash)
         expect(parsed_response[:results]).to be_an(Array)
       end
     end
     describe '#discover' do
-      it 'returns a list of movies from the API, ordered by popularity' do
+      it 'returns a list of movies from the API, ordered by popularity', :vcr do
         discover = MovieService.new.discover
         expect(discover).to be_a(Hash)
         expect(discover[:results]).to be_an(Array)
