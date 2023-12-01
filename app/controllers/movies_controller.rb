610894c94a1f61b2bb86aeeedc77b9f00134b864
Xander Hendry <vhendry.coding@gmail.com>
2023-12-01 11:13:21 -0500

    beginning work on movie results page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
-class MoviesController < ApplicationController
-  def discover
-    response = conn.get('/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc')
+class MoviesController < ApplicationRecord
+  def index
+    @facade = DiscoverFacade.top_rated_movies
   end
 end
--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -2,16 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Discover Movies' do
   describe 'visiting the Discover Movies page (/users/:id/discover)' do
-    
     it 'has a button to "Discover Top Rated Movies" and a form to search by movie title' do
       user = User.last
       visit "/users/#{user.id}/discover"
-      expect(page).to have_button('Discover Top Rated Movies')
-      expect(page).to have_field(:movie_title)
-      expect(page).to have_button('Search by Movie Title')
-    end
-    it 'redirects to the movie results page when a user searches for a move, or clicks the discover button' do
-      
+      save_and_open_page
+      expect(page).to have_button('Find Top Rated Movies')
+      expect(page).to have_field(:search)
+      expect(page).to have_button('Find Movies')
     end
   end
 end
