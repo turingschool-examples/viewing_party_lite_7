30b1c42178dc83feeee7cb8cd7d67b6ff4f684f1
Captainlearyo <clayoleary@gmail.com>
2023-12-01 16:36:47 -0600

    story 11 tested

--- spec/models/user_party_spec.rb
+++ spec/models/user_party_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe UserParty, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:party) }
   end
+
+  it "#get_user_name" do
+    expect(@user_party1.get_user_name).to eq("Capitainlearyo")
+  end
 end
