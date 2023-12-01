e9694d07ecc8b6e7bda2dba563c064e231af843f
pcbennett108 <pcbennett42@gmail.com>
2023-12-01 13:22:38 -0700

    Add image endpoint to movie service

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -17,10 +17,15 @@ class MovieService
   end
 
   def get_credits(id)
-    get_url("3/movie/#{id}/credits")
+    get_url("/3/movie/#{id}/credits")
   end
 
   def get_reviews(id)
-    get_url("3/movie/#{id}/reviews")
+    get_url("/3/movie/#{id}/reviews")
   end
+
+  def movie_images(id)
+    get_url("/3/movie/#{id}/images")
+  end
+
 end
