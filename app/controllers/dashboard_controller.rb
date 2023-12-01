17be67dc6470ed1f16b004525ac29073d00ba012
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-01 13:33:24 -0700

    Update controllers to use user parties instead of parties

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def show
     @primary = User.find(params[:user_id])
+    # user who is viewing the user's dashobard
     @viewed = User.find(params[:id])
+    # user whose dashboard is being viewed
+    @user_parties = @viewed.user_parties
   end
 end
