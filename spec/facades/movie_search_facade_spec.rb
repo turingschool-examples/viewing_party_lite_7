63cd34f16a16e8b04a9ad8cc021f327508df03aa
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-01 09:32:03 -0700

    Test:Search Facade tests complete

--- spec/facades/movie_search_facade_spec.rb
+++ spec/facades/movie_search_facade_spec.rb
@@ -1,20 +1,23 @@
 require 'rails_helper'
 
-# RSpec.describe MovieSearchFacade do
-#   describe 'initialize' do
-#     it 'exists' do
-#       search = 'Star Wars'
-#       facade = MovieSearchFacade.new(search)
-#       require 'pry'; binding.pry
-#       expect(facade).to be_instance_of(MovieSearchFacade)
-#     end
-#   end
-#   describe 'search results' do 
-#     it 'returns search results' do 
-#       search = 'Star Wars'
-#       facade = MovieSearchFacade.new(search)
-#       require 'pry'; binding.pry
-#       expect(facade)
-#     end
-#   end
-# end
+RSpec.describe MovieSearchFacade do
+  describe 'initialize' do
+    it 'exists' do
+      VCR.use_cassette("star_wars_search") do
+      
+      search = 'Star Wars'
+      facade = MovieSearchFacade.new(search)
+      expect(facade).to be_instance_of(MovieSearchFacade)
+      end
+    end
+  end
+  describe 'search results' do 
+    it 'returns search results' do 
+      VCR.use_cassette("star_wars_search") do
+        search = 'Star Wars'
+        facade = MovieSearchFacade.new(search)
+        expect(facade)
+      end
+    end
+  end
+end
