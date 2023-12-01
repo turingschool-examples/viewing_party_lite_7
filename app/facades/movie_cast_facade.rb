cf3a866068fa289bad2bd985f9be7cdb259c7660
Anthea Yur <acyur6@gmail.com>
2023-12-01 00:04:25 -0600

    Refactor and create movie cast facade

---
+++ app/facades/movie_cast_facade.rb
@@ -0,0 +1,12 @@
+class MovieCastFacade
+  def initialize(movie_id)
+    @movie_id = movie_id
+  end
+
+  def movie_cast
+    service = MovieService.new
+
+    json = service.movie_cast(@movie_id)
+    @movie_cast = MovieCast.new(json)
+  end
+end
