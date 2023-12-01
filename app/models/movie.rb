2c9e1dde914a586b72a94acd918647d6226beac8
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-12-01 00:13:54 -0700

    Finish moving API calls to the services file

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,7 +4,7 @@ class Movie < ApplicationRecord
   validates :runtime, presence: true
 
   def self.create_from_api(movie_id)
-    data = MovieService.details_api_call("https://api.themoviedb.org/3/movie/#{movie_id}")
+    data = MovieService.details_api_call(movie_id)
     Movie.create!({
       title: data[:title],
       tmdb_id: data[:id],
