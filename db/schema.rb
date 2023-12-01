477fdc431a64a89c431f5ea440c457c75f5e891b
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-30 22:13:47 -0700

    Add movie id column to party table to access movie from party

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_031336) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_01_044418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_031336) do
     t.time "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "movie_id"
   end
 
   create_table "user_parties", force: :cascade do |t|
