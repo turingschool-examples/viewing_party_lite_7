b51d33b013751e7f1809ee61a802fa5e36861047
Kevin Zolman <kevin.zolman@gmail.com>
2023-12-01 16:37:55 -0700

    Add movie_search_service to results controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+
+@user1 = User.create!(name: 'Joe', email: 'joe@gmail.com')
+@user2 = User.create!(name: 'Mama', email: 'mama@gmail.com')
+
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
