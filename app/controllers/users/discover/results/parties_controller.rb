b7b19fb4a7cd1ea593bc0a7922b30be46eaa6157
Miranda Delaney <delaney.miranda1@gmail.com>
2023-12-01 15:25:52 -0700

    parties controller create refactor

--- app/controllers/users/discover/results/parties_controller.rb
+++ app/controllers/users/discover/results/parties_controller.rb
@@ -7,30 +7,22 @@ class Users::Discover::Results::PartiesController < ApplicationController
     @movie = movies_search.genre_runtime(params[:id])
   end
 
-  # refactor, this is too fat
   def create
-    party = Party.new({
-      duration: params[:duration],
-      date: params[:date],
-      start_time: params[:start_time],
-      movie_id: params[:movie_id]
-    })
     host = User.find(params[:user_id])
     movies_search = MoviesSearchService.new
     movie = movies_search.genre_runtime(params[:id])
-    
-    if movie.runtime > party.duration
-      redirect_to "/users/#{host.id}/movies/#{movie.movie_id}/parties/new"
-      flash[:alert] = 'invalid duration'
-    else
-      party.save
+    success, party = Party.create_with_checks(params, host, movie)
+    if success
       user_party = UserParty.create!(user: host, party: party, host: true)
       User.all.each do |user|
-        if user != host && params[user.name.to_s] == 1
+        if user != host && params[user.name.to_s] == '1'
           UserParty.create!(user: user, party: party, host: false)
         end
       end
       redirect_to user_path(host)
+    else
+      redirect_to "/users/#{host.id}/movies/#{movie.movie_id}/parties/new"
+      flash[:alert] = 'invalid duration'
     end
   end
 
--- app/models/party.rb
+++ app/models/party.rb
@@ -3,4 +3,20 @@ class Party < ApplicationRecord
   has_many :users, through: :user_parties
   
   validates_presence_of :duration, :date, :start_time, :movie_id
+
+  def self.create_with_checks(params, host, movie)
+    party = new({
+      duration: params[:duration],
+      date: params[:date],
+      start_time: params[:start_time],
+      movie_id: params[:movie_id]
+    })
+
+    if movie.runtime > party.duration
+      return false, party
+    end
+
+    party.save
+    [true, party]
+  end
 end
