6248a9cbd48335a63c11321507082624818a606c
Cory Powell <coryrpowell@gmail.com>
2023-12-01 12:45:57 -0500

    added to the README and refactored the show spec test for movie_results show_spec to have a within block.

--- spec/features/movie_results/index_spec.rb
+++ spec/features/movie_results/index_spec.rb
@@ -20,40 +20,33 @@ RSpec.describe "Movie Results Page", type: :feature do
         click_button "Search by Movie Title"
         expect(current_path).to eq("/users/#{@user1.id}/movies")
       end
-      
-    end
-  end 
 
-  describe "When I visit '/users/:id/movies'" do
+      it "I see Titles as links to their movie details page I see the 'Vote Average of the movies'" do
+        visit "/users/#{@user1.id}/movies"
+        within("#movie-238") do
+          expect(page).to have_link("The Godfather")
+          expect(page).to have_content("Vote Average: 8.7")
+        end
+        within("#movie-278") do
+          expect(page).to have_link("The Shawshank Redemption")
+          expect(page).to have_content("Vote Average: 8.705")
+        end
 
-    it "I see the Titles as links to their movie detail page" do
-      visit "/users/#{@user1.id}/movies"
-      
-      expect(page).to have_link("The Godfather")
-      expect(page).to have_link("The Shawshank Redemption")
-      expect(page).to have_link("The Godfather Part II")
-      
-      expect("The Godfather").to appear_before("The Shawshank Redemption")
-      
-    end
+        within("#movie-240") do
+          expect(page).to have_link("The Godfather Part II")
+          expect(page).to have_content("Vote Average: 8.589")
+        end
+      end
 
-    it "I see the 'Vote Average of the movies'" do
-      visit "/users/#{@user1.id}/movies"
+      it "should have a button to return to the Discover Page" do
+        visit "/users/#{@user1.id}/movies"
 
-      expect(page).to have_content("Vote Average: 8.7")
-      expect(page).to have_content("Vote Average: 8.705")
-      expect(page).to have_content("Vote Average: 8.6")
-      
-    end
+        expect(page).to have_button("Discover Page")
 
-    it "should have a button to return to the Discover Page" do
-      visit "/users/#{@user1.id}/movies"
+        click_button("Discover Page")
 
-      expect(page).to have_button("Discover Page")
-
-      click_button("Discover Page")
-
-      expect(current_path).to eq("/users/#{@user1.id}/discover")
+        expect(current_path).to eq("/users/#{@user1.id}/discover")
+      end
     end
   end
-end 
+end
