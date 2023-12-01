832b7a7913cbd3bcb4227cf381b0381aca76b41f
pcbennett108 <pcbennett42@gmail.com>
2023-12-01 13:19:04 -0700

    Add call to user facade in SHOW

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @facade = UserFacade.new(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def create
