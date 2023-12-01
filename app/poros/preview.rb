d1876b5a4b0d0b28ea74976f202839373b9e6cc2
Xander Hendry <vhendry.coding@gmail.com>
2023-11-30 22:16:26 -0500

    change preview poro variables for name accuracy

--- app/poros/preview.rb
+++ app/poros/preview.rb
@@ -2,8 +2,8 @@ class Preview
   attr_reader :original_title,
               :vote_average
 
-  def initialize(movie_data)
-    @title = movie_data[:original_title]
-    @vote_average = movie_data[:vote_average]
+  def initialize(preview_data)
+    @title = preview_data[:original_title]
+    @vote_average = preview_data[:vote_average]
   end
 end
