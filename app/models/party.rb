98e91c420c8a05ef02577142ba6347510568eac5
Anthea Yur <acyur6@gmail.com>
2023-12-01 13:47:20 -0600

    Functioning date and time selection, with pretty_time method

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,6 +3,10 @@ class Party < ApplicationRecord
   has_many :users, through: :user_parties
 
   def pretty_day
-    day.strftime("%B %d, %Y")
+    day.strftime("%B %e, %Y")
+  end
+
+  def pretty_time
+    start_time.strftime("%l:%M %p")
   end
 end
