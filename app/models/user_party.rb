303534639338f63b74ccffaec808f6fb2f1de464
Captainlearyo <clayoleary@gmail.com>
2023-12-01 16:04:56 -0600

    story 11 complete

--- app/models/user_party.rb
+++ app/models/user_party.rb
@@ -1,5 +1,10 @@
 class UserParty < ApplicationRecord
   belongs_to :user
   belongs_to :party
-  
+
+
+  def get_user_name
+  user = User.find_by_id(self.user_id)
+  user.present? ? user.name : "Unknown User"
+end
 end
