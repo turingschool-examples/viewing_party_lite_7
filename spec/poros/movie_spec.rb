c879e858609c57675e666f8d91e7c592105275f3
Scott DeVoss <scottdevoss@yahoo.com>
2023-11-30 17:19:35 -0700

    completed poros testing

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe Movie do
         original_title: "Star Wars",
         vote_average: 8.6,
         runtime: 121,
-        genres: "Space War",
+        genres: [
+          {
+            "name": "Space War"
+          },
+          {
+            "name": "Thriller"
+          }
+        ],
         overview: "A movie about wars in the stars"
       }
     @movie = Movie.new(@movie_data)
@@ -18,10 +25,21 @@ RSpec.describe Movie do
       expect(@movie.title).to eq('Star Wars')
       expect(@movie.average).to eq(8.6)
       expect(@movie.runtime).to eq(121)
-      expect(@movie.genres).to eq("Space War")
       expect(@movie.summary).to eq("A movie about wars in the stars")
       # expect(movie.genres).to be_a(Array)
       # expect(movie.genres[0]).to be_a(Hash)
+    end
+  end
+
+  describe "#convert_time" do
+    it "should convert the time to hours and minutes" do
+      expect(@movie.convert_time).to eq("2h 1min")
+    end
+  end
+
+  describe "#format_genre" do
+    it "should format the genre" do
+      expect(@movie.format_genre).to eq("Space War, Thriller")
     end
   end
 end
