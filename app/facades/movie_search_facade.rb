84e7cf28e4e1573b274220c528d6ea178e7c48e9
pcbennett108 <pcbennett42@gmail.com>
2023-12-01 13:38:12 -0700

    bulk commit

---
+++ app/facades/movie_search_facade.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Facade for movie search functionality
+
+class MovieSearchFacade
+  attr_reader :search_term, :movies
+
+  def initialize(search_term)
+    @search_term = search_term
+    @movies = search_results
+  end
+  
+  def search_results
+    service = MovieSearchService.new
+    json = service.movie_search(@search_term)
+    json[:results].take(20).map do |movie|
+      MovieSearch.new(movie)
+    end
+  end
+
+# require 'pry'; binding.pry
+end
