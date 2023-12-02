d6a18a6fdae2c7ef92df33a1cd9248fc5d1a23d0
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-02 13:32:09 -0700

    Add password secure validation in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   has_many :parties, through: :user_parties
   validates :name, presence: true
   validates :email, format: URI::MailTo::EMAIL_REGEXP, presence: true, uniqueness: true
-
+  has_secure_password
+  
   def self.other_users(exception)
     User.where.not(id: exception.id).all
   end
