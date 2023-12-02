483559570f81f985d6790ee13d966ff39f46b916
Xander Hendry <vhendry.coding@gmail.com>
2023-12-02 14:55:59 -0500

    add potential guests query method to User model, and add test to spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_many :viewing_parties
   has_many :party_guests
   has_many :viewing_parties, through: :party_guests
+
+  def self.potential_guests(host_id)
+    User.where("id != #{host_id}")
+  end
 end
