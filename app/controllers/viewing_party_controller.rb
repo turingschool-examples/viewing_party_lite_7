146317aa29b8cd13b0d43a1456cb3d22e01c56b2
Sooyung Kim <kimx1279@gmail.com>
2023-12-02 09:59:35 -0700

    Refactor and update vcr

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -10,11 +10,11 @@ class ViewingPartyController < ApplicationController
     party = Party.new(party_params)
 
     if party.save
-      host(host, party)
-      attendees(party)
+      host_of_party(host, party)
+      attendees_of_party(party)
       redirect_to user_path(host)
     else
-      redirect_to new_user_movie_viewing_party(host, params[:movie_id])
+      redirect_to new_user_movie_viewing_party_path(host, params[:movie_id])
       flash[:error] = 'Please fill out time and date of the party'
     end
   end
@@ -25,14 +25,13 @@ class ViewingPartyController < ApplicationController
     params.permit(:movie_id, :duration_of_party, :party_date, :start_time)
   end
 
-  def host(host, party)
-    PartyUser.create(user_id: host.id, party_id: party.id, is_host: true)
-    attendees(party)
+  def host_of_party(host, party)
+    PartyUser.create!(user_id: host.id, party_id: party.id, is_host: true)
   end
 
-  def attendees(party)
+  def attendees_of_party(party)
     params[:invites].map do |invite|
-      PartyUser.create(user_id: invite, party_id: party.id)
+      PartyUser.create!(user_id: invite, party_id: party.id)
     end
   end
 end
--- spec/features/viewing_party/show_spec.rb
+++ spec/features/viewing_party/show_spec.rb
@@ -8,37 +8,13 @@ RSpec.describe 'Viewing Party Page', type: :feature do
 
     describe 'when I visit /users/:user_id/movies/:movies_id/viewing_party/new' do
       it 'shows the title of the movie' do
-        json_response = File.read('spec/fixtures/spirited_away.json')
-        parsed = JSON.parse(json_response, symbolize_names: true)
-        movie_id = parsed[:id]
-        stub_request(:get, "https://api.themoviedb.org/3/movie/129?api_key=#{Rails.application.credentials.tmdb[:key]}&language=en-US").
-          with(
-            headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent'=>'Faraday v2.7.12'
-            }).
-          to_return(status: 200, body: json_response, headers: {})
-
-        visit new_user_movie_viewing_party_path(@user, movie_id)
+        visit new_user_movie_viewing_party_path(@user, 129)
 
         expect(page).to have_content('Create a Movie Party for Spirited Away')
       end
 
       it 'shows viewing party details, including movie title, duration of party, day and start time' do
-        json_response = File.read('spec/fixtures/spirited_away.json')
-        parsed = JSON.parse(json_response, symbolize_names: true)
-        movie_id = parsed[:id]
-        stub_request(:get, "https://api.themoviedb.org/3/movie/129?api_key=#{Rails.application.credentials.tmdb[:key]}&language=en-US").
-          with(
-            headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent'=>'Faraday v2.7.12'
-            }).
-          to_return(status: 200, body: json_response, headers: {})
-
-        visit new_user_movie_viewing_party_path(@user, movie_id)
+        visit new_user_movie_viewing_party_path(@user, 129)
 
         expect(page).to have_content('Viewing Party Details')
         expect(page).to have_content('Movie Title')
@@ -52,19 +28,8 @@ RSpec.describe 'Viewing Party Page', type: :feature do
       it 'creates a new viewing party' do
         user1 = User.create!(name: 'Sam', email: 'sam@email.com')
         user2 = User.create!(name: 'Susan', email: 'susan@email.com')
-        json_response = File.read('spec/fixtures/spirited_away.json')
-        parsed = JSON.parse(json_response, symbolize_names: true)
-        movie_id = parsed[:id]
-        stub_request(:get, "https://api.themoviedb.org/3/movie/129?api_key=#{Rails.application.credentials.tmdb[:key]}&language=en-US").
-          with(
-            headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'User-Agent'=>'Faraday v2.7.12'
-            }).
-          to_return(status: 200, body: json_response, headers: {})
 
-        visit new_user_movie_viewing_party_path(@user, movie_id)
+        visit new_user_movie_viewing_party_path(@user, 129)
 
         fill_in('party_date', with: '2023-12-01')
         fill_in('start_time', with: '07:00')
