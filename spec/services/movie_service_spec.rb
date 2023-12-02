6fb73b988fff89abc347416bb9791b6ca9a952e3
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-01 19:45:55 -0700

    Add test coverage for actor service

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -67,6 +67,21 @@ describe MovieService do
       end
     end
 
+    context "movie_cast" do
+      it "provides the cast for a movie" do
+        cast = MovieService.new.movie_cast(872585)
+        actor = cast[:cast].first
+        expect(cast).to be_a Hash
+
+        expect(actor).to have_key :name
+        expect(actor[:name]).to be_a String
+
+        expect(actor).to have_key :character
+        expect(actor[:character]).to be_a String
+
+      end
+    end
+
     context "get_url" do
       it "returns parsed json data" do
         url = "movie/popular"
