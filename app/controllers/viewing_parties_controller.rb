9529266039a5eb7da6ac0372f78ce8cc7f20e40b
Cory Powell <coryrpowell@gmail.com>
2023-12-02 22:01:34 -0500

    removed extra lines

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,24 +1,8 @@
 class ViewingPartiesController < ApplicationController
   before_action :find_movie, only: [:new]
-  before_action :find_user, only: %i[new create]
+  before_action :find_user, only: [:new]
   def new
     @users = User.where.not(id: @user.id)
-  end
-
-  def create
-    @viewing_party = ViewingParty.new(viewing_party_params)
-    if @viewing_party.save
-      UserViewingParty.create(user: @user.id, viewing_party: @viewing_party.id)
-
-      # User.all.each do |user|
-      #   UserViewingParty.create(user: user.id, viewing_party: viewing_party[:id])
-      # end
-      redirect_to "/users/#{@user.id}"
-    else
-      flash.notice = 'Please Fill in All Fields'
-      # require 'pry';binding.pry
-      redirect_to "/users/#{params[:id]}/movies/#{params[:movies_id]}/viewing_parties/new"
-    end
   end
 
   private
