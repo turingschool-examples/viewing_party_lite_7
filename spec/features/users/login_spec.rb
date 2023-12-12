1c9fb3cb63f02ace73b6749ae4705c93cea35ed7
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-12-12 15:48:58 -0700

    Write tests for logging in and out

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -34,4 +34,42 @@ RSpec.describe "New" do
     expect(page).to have_content("You have entered the incorrect credentials")
 
   end
+
+  it "stays logged in, and can logout" do
+    visit "/login"
+
+    fill_in :email, with: "Moogoo@gmail.com"
+    fill_in :password, with: "Hello123!"
+
+    expect(page).to_not have_button("Log out")
+    expect(page).to have_button("Login")
+
+    click_on "Log In"
+
+    expect(current_path).to eq("/users/#{@user3.id}")
+
+    expect(page).to_not have_button("Login")
+    expect(page).to have_button("Log out")
+
+    visit "/login"
+    expect(page).to_not have_button("Login")
+    expect(page).to have_button("Log out")
+
+    visit "/users/#{@user3.id}/discover"
+    expect(page).to_not have_button("Login")
+    expect(page).to have_button("Log out")
+
+    click_on "Log out"
+    expect(current_path).to eq("/")
+    expect(page).to_not have_button("Log out")
+    expect(page).to have_button("Login")
+
+    visit "/login"
+    expect(page).to_not have_button("Log out")
+    expect(page).to have_button("Login")
+
+    visit "/users/#{@user3.id}/discover"
+    expect(page).to_not have_button("Log out")
+    expect(page).to have_button("Login")
+  end
 end
