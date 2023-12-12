a62a8df730557cb65ac1811996418753dbc4d78a
Brendan Bondurant <139706925+brendan-bondurant@users.noreply.github.com>
2023-12-11 17:01:15 -0700

    Revert "Authentication Assignment"

--- db/migrate/20231211204353_add_username_and_password_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernameAndPasswordToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :username, :string
-    add_column :users, :password_digest, :string
-  end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,22 +4,20 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many(:party_users) }
     it { should have_many(:parties) }
-    it { should have_secure_password }
   end
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
-    it { should validate_presence_of(:password_digest)}
   end
 
   describe 'class methods' do
     describe '#all_excluding_id' do
       it 'returns all users excluding the ID provided' do
-        @user_1 = User.create!(name: 'Kiwi', email: 'kiwibird@gmail.com', password: "12345", password_confirmation: "12345")
-        @user_2 = User.create!(name: 'Coco', email: 'cocobird@gmail.com', password: "12345", password_confirmation: "12345")
-        @user_3 = User.create!(name: 'Chicken', email: 'chickenbird@gmail.com', password: "12345", password_confirmation: "12345")
-        @user_4 = User.create!(name: 'Hiccup', email: 'hiccupbird@gmail.com', password: "12345", password_confirmation: "12345")
+        @user_1 = User.create!(name: 'Kiwi', email: 'kiwibird@gmail.com')
+        @user_2 = User.create!(name: 'Coco', email: 'cocobird@gmail.com')
+        @user_3 = User.create!(name: 'Chicken', email: 'chickenbird@gmail.com')
+        @user_4 = User.create!(name: 'Hiccup', email: 'hiccupbird@gmail.com')
 
         expect(User.all_excluding_id(@user_1.id)).not_to eq([@user_1])
         expect(User.all_excluding_id(@user_3.id)).not_to eq([@user_3])
@@ -29,8 +27,8 @@ RSpec.describe User, type: :model do
 
   describe 'instance methods' do
     before(:each) do
-      @user_1 = User.create!(name: 'Kiwi', email: 'kiwibird@gmail.com', password: "12345", password_confirmation: "12345")
-      @user_2 = User.create!(name: 'Chicken', email: 'chickenbird@gmail.com', password: "12345", password_confirmation: "12345")
+      @user_1 = User.create!(name: 'Kiwi', email: 'kiwibird@gmail.com')
+      @user_2 = User.create!(name: 'Chicken', email: 'chickenbird@gmail.com')
       @party_1 = Party.create!(movie_id: 926_393, duration_of_party: 109, party_date: '2024-10-10', start_time: '07:23')
       @party_2 = Party.create!(movie_id: 926_393, duration_of_party: 109, party_date: '2024-10-11', start_time: '09:23')
       @party_3 = Party.create!(movie_id: 926_393, duration_of_party: 109, party_date: '2024-10-12', start_time: '13:09')
