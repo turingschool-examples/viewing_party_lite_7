1d3423c7c5e0a4afbc9d2b9b3c543ba60304c729
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-12-12 15:07:24 -0700

    feat: Complete Task 3: User Story 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,27 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
-
+  
   def new
   end
-
+  
   def create
-    user = user_params
-    user[:email] = user[:email].downcase
-    new_user = User.new(user_params)
-    if new_user.save
-      session[:new_user_id] = new_user.id
+    params[:email] = params[:email].downcase
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
       flash[:success] = 'Successfully Added New User'
-      redirect_to user_path(new_user)
+      redirect_to user_path(user)
     else
-      flash[:error] = "#{error_message(new_user.errors)}"
+      flash[:error] = "#{error_message(user.errors)}"
       redirect_to register_user_path
+    end
+  end
+
+  def show
+    if session[:user_id]
+      @user = User.find(params[:id])
+    else
+      flash[:error] = "You must log in to view the dashboard"
+      redirect_to root_path
     end
   end
 
@@ -24,6 +29,7 @@ class UsersController < ApplicationController
   end
 
   def login
+    params[:email] = params[:email].downcase
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
@@ -38,7 +44,7 @@ class UsersController < ApplicationController
   def log_out
     session[:user_id] = nil
     flash[:success] = "You've been logged out!"
-    redirect_to "/"
+    redirect_to root_path
   end
 
   private
